e9ce090c2361dcd49400112f8d2e3d29386602d7
oetyng <oetyng@gmail.com>
2023-04-21 19:00:56 +0200

    chore: move missed domain logic
    - This should have been moved from protocol earlier but was missed.

--- safenode/src/protocol/mod.rs
+++ safenode/src/protocol/mod.rs
@@ -6,10 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-/// Messages types
-pub mod messages;
-
-/// Dbc genesis creation.
-pub mod dbc_genesis;
 /// Errors.
 pub mod error;
+/// Messages types
+pub mod messages;
