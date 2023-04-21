4fffe68553fc7d240c208fa6b2cebe42e00ae82c
Josh Wilson <joshuef@gmail.com>
2023-04-21 10:22:28 +0900

    fix: add missing tracing macro to client

--- safenode/src/bin/safenode/main.rs
+++ safenode/src/bin/safenode/main.rs
@@ -5,7 +5,6 @@
 // under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
-
 mod rpc;
 
 use safenode::{
