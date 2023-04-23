3d6e8e6b7e1774bb1759ef3aff247026be050286
Josh Wilson <joshuef@gmail.com>
2023-04-24 08:49:15 +0900

    feat: initial setup of KademliaConfig for provider usage

--- safenode/src/network/error.rs
+++ safenode/src/network/error.rs
@@ -26,6 +26,8 @@ pub(super) type Result<T, E = Error> = std::result::Result<T, E>;
 #[derive(Debug, Error)]
 #[allow(missing_docs)]
 pub enum Error {
+    #[error("Close group size must be a non-zero usize")]
+    InvalidCloseGroupSize,
     #[error("Internal messaging channel was dropped")]
     InternalMsgChannelDropped,
 
