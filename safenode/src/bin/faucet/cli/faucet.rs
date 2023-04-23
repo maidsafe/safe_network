71bfa2da84007ce3d1d1d4cc1fa4bc773108f8c9
oetyng <oetyng@gmail.com>
2023-04-23 19:53:22 +0200

    feat: add testnet faucet to cli

---
+++ safenode/src/bin/faucet/cli/faucet.rs
@@ -0,0 +1,116 @@
+// Copyright 2023 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
+use safenode::{
+    client::{Client, WalletClient},
+    domain::{
+        dbc_genesis::create_genesis,
+        wallet::{parse_public_address, DepositWallet, LocalWallet, Wallet},
+    },
+};
+
+use sn_dbc::Token;
+
+use clap::Parser;
+use eyre::Result;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+pub enum FaucetCmds {
+    /// Tries to load any hex encoded `Dbc`s from the `received_dbcs`
+    /// path in the wallet dir, and deposit it to the wallet.
+    Genesis,
+    Send {
+        /// This shall be the number of nanos to send.
+        /// Necessary if the `send_to` argument has been given.
+        #[clap(name = "amount")]
+        amount: String,
+        /// This must be a hex-encoded `PublicAddress`.
+        #[clap(name = "to")]
+        to: String,
+    },
+}
+
+pub(crate) async fn faucet_cmds(cmds: FaucetCmds, client: &Client) -> Result<()> {
+    match cmds {
+        FaucetCmds::Genesis => genesis().await?,
+        FaucetCmds::Send { amount, to } => send(amount, to, client).await?,
+    }
+    Ok(())
+}
+
+async fn genesis() -> Result<()> {
+    let genesis = create_genesis()?;
+    let root_dir = get_faucet_dir().await?;
+    let mut wallet = LocalWallet::load_from(&root_dir).await?;
+
+    let previous_balance = wallet.balance();
+
+    wallet.deposit(vec![genesis]);
+
+    let new_balance = wallet.balance();
+    let deposited = previous_balance.as_nano() - new_balance.as_nano();
+
+    if deposited > 0 {
+        if let Err(err) = wallet.store().await {
+            println!("Failed to store deposited amount: {:?}", err);
+        } else {
+            println!("Deposited {:?}.", sn_dbc::Token::from_nano(deposited));
+        }
+    } else {
+        println!("Nothing deposited.");
+    }
+
+    Ok(())
+}
+
+async fn send(amount: String, to: String, client: &Client) -> Result<()> {
+    let address = parse_public_address(to)?;
+
+    use std::str::FromStr;
+    let amount = Token::from_str(&amount)?;
+    if amount.as_nano() == 0 {
+        println!("Invalid format or zero amount passed in. Nothing sent.");
+        return Ok(());
+    }
+
+    let root_dir = get_faucet_dir().await?;
+    let wallet = LocalWallet::load_from(&root_dir).await?;
+
+    let mut wallet_client = WalletClient::new(client.clone(), wallet);
+
+    match wallet_client.send(amount, address).await {
+        Ok(new_dbc) => {
+            println!("Sent {amount:?} to {address:?}");
+            let mut wallet = wallet_client.into_wallet();
+            let new_balance = wallet.balance();
+
+            if let Err(err) = wallet.store().await {
+                println!("Failed to store wallet: {err:?}");
+            } else {
+                println!("Successfully stored wallet with new balance {new_balance:?}.");
+            }
+
+            wallet.store_created_dbc(new_dbc).await?;
+            println!("Successfully stored new dbc to wallet dir. It can now be sent to the recipient, using any channel of choice.");
+        }
+        Err(err) => {
+            println!("Failed to send {amount:?} to {address:?} due to {err:?}.");
+        }
+    }
+
+    Ok(())
+}
+
+async fn get_faucet_dir() -> Result<PathBuf> {
+    let mut home_dirs = dirs_next::home_dir().expect("A homedir to exist.");
+    home_dirs.push(".safe");
+    home_dirs.push("faucet");
+    tokio::fs::create_dir_all(home_dirs.as_path()).await?;
+    Ok(home_dirs)
+}
---
+++ safenode/src/bin/faucet/cli/register.rs
@@ -0,0 +1,120 @@
+// Copyright 2023 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
+use safenode::client::{Client, Error as ClientError};
+
+use clap::Subcommand;
+use eyre::Result;
+use xor_name::XorName;
+
+#[derive(Subcommand, Debug)]
+pub enum RegisterCmds {
+    /// Create a new register with the given pet name.
+    Create {
+        /// The name of the register to create.
+        #[clap(name = "name")]
+        name: String,
+    },
+    Edit {
+        /// The name of the register to edit.
+        #[clap(name = "name")]
+        name: String,
+        /// The entry to add to the register.
+        #[clap(name = "entry")]
+        entry: String,
+    },
+    Get {
+        /// The register pet names to get.
+        #[clap(name = "names")]
+        names: Vec<String>,
+    },
+}
+
+pub(crate) async fn register_cmds(cmds: RegisterCmds, client: &Client) -> Result<()> {
+    match cmds {
+        RegisterCmds::Create { name } => create_register(name, client).await?,
+        RegisterCmds::Edit { name, entry } => edit_register(name, entry, client).await?,
+        RegisterCmds::Get { names } => get_registers(names, client).await?,
+    }
+    Ok(())
+}
+
+async fn create_register(name: String, client: &Client) -> Result<()> {
+    let tag = 3006;
+    let xorname = XorName::from_content(name.as_bytes());
+    println!("Creating Register with '{name}' at xorname: {xorname:x} and tag {tag}");
+
+    let _register = match client.create_register(xorname, tag).await {
+        Ok(register) => {
+            println!("Successfully created register '{name}' at {xorname:?}, {tag}!");
+            register
+        }
+        Err(error) => {
+            panic!("Did not create register '{name}' on all nodes in the close group! {error}")
+        }
+    };
+    Ok(())
+}
+
+async fn edit_register(name: String, entry: String, client: &Client) -> Result<()> {
+    let tag = 3006;
+    let xorname = XorName::from_content(name.as_bytes());
+    println!("Trying to retrieve Register from {xorname:?}, {tag}");
+
+    match client.get_register(xorname, tag).await {
+        Ok(mut register) => {
+            println!(
+                "Successfully retrieved Register '{name}' from {}, {}!",
+                register.name(),
+                register.tag()
+            );
+            println!("Editing Register '{name}' with: {entry}");
+            match register.write(entry.as_bytes()).await {
+                Ok(()) => {}
+                Err(ref err @ ClientError::ContentBranchDetected(ref branches)) => {
+                    println!(
+                        "We need to merge {} branches in Register entries: {err}",
+                        branches.len()
+                    );
+                    register.write_merging_branches(entry.as_bytes()).await?;
+                }
+                Err(err) => return Err(err.into()),
+            }
+        }
+        Err(error) => {
+            panic!("Did not retrieve Register '{name}' from all nodes in the close group! {error}")
+        }
+    }
+
+    Ok(())
+}
+
+async fn get_registers(names: Vec<String>, client: &Client) -> Result<()> {
+    let tag = 3006;
+    for name in names {
+        println!("Register name passed in via `register get` is '{name}'...");
+        let xorname = XorName::from_content(name.as_bytes());
+
+        println!("Trying to retrieve Register from {xorname:?}, {tag}");
+
+        match client.get_register(xorname, tag).await {
+            Ok(register) => println!(
+                "Successfully retrieved Register '{name}' from {}, {}!",
+                register.name(),
+                register.tag()
+            ),
+            Err(error) => {
+                panic!(
+                    "Did not retrieve Register '{name}' from all nodes in the close group! {error}"
+                )
+            }
+        }
+    }
+
+    Ok(())
+}
