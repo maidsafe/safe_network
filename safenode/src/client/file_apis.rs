7dcb9fd216acfd8b247015b56e0a43aed445c293
Josh Wilson <joshuef@gmail.com>
2023-04-24 12:17:15 +0900

    fix: do not verify small chunks before attempting to upload

--- safenode/src/client/file_apis.rs
+++ safenode/src/client/file_apis.rs
@@ -128,11 +128,7 @@ impl Files {
     async fn upload_small(&self, small: SmallFile, verify: bool) -> Result<ChunkAddress> {
         let chunk = package_small(small)?;
         let address = *chunk.address();
-
-        if self.verify_chunk_is_stored(address).await.is_ok() {
-            return Ok(address);
-        }
-
+        
         self.client.store_chunk(chunk).await?;
 
         if verify {
