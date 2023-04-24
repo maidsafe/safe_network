f0c661f1b66777619f360c85b47f80e0f4bfd888
oetyng <oetyng@gmail.com>
2023-04-24 14:33:35 +0200

    chore: update incorrect cli cmd docs

--- safenode/src/bin/faucet/cli/faucet.rs
+++ safenode/src/bin/faucet/cli/faucet.rs
@@ -34,7 +34,7 @@ pub enum FaucetCmds {
     Genesis,
     Send {
         /// This shall be the number of nanos to send.
-        /// Necessary if the `send_to` argument has been given.
+        /// Necessary if the `to` argument has been given.
         #[clap(name = "amount")]
         amount: String,
         /// This must be a hex-encoded `PublicAddress`.
