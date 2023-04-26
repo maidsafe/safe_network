9b979c78b41359cbaaef411805d21484dfc7e872
oetyng <oetyng@gmail.com>
2023-04-26 12:44:58 +0200

    feat: impl early return for majority spend ok

--- safenode/src/client/wallet/send_client.rs
+++ safenode/src/client/wallet/send_client.rs
@@ -8,15 +8,9 @@
 
 use super::Client;
 
-use crate::{
-    domain::{
-        client_transfers::{
-            create_online_transfer, Outputs as TransferDetails, SpendRequestParams,
-        },
-        wallet::{Error, Result, SendClient},
-    },
-    network::close_group_majority,
-    protocol::messages::{Cmd, CmdResponse, Request, Response},
+use crate::domain::{
+    client_transfers::{create_online_transfer, Outputs as TransferDetails},
+    wallet::{Error, Result, SendClient},
 };
 
 use sn_dbc::{Dbc, DbcIdSource, DerivedKey, PublicAddress, Token};
@@ -31,48 +25,10 @@ impl SendClient for Client {
     ) -> Result<TransferDetails> {
         let transfer = create_online_transfer(dbcs, to, change_to, self).await?;
 
-        for spend_request_params in transfer.all_spend_request_params.clone() {
-            let SpendRequestParams {
-                signed_spend,
-                parent_tx,
-                fee_ciphers,
-            } = spend_request_params;
-
-            let cmd = Cmd::SpendDbc {
-                signed_spend: Box::new(signed_spend),
-                parent_tx: Box::new(parent_tx),
-                fee_ciphers,
-            };
-
-            let responses = self
-                .send_to_closest(Request::Cmd(cmd))
+        for spend_request_param in transfer.all_spend_request_params.clone() {
+            self.expect_closest_majority_response(spend_request_param)
                 .await
                 .map_err(|err| Error::CouldNotSendTokens(err.to_string()))?;
-
-            // Get all Ok results of the expected response type `Spend`.
-            let ok_responses: Vec<_> = responses
-                .iter()
-                .flatten()
-                .flat_map(|resp| {
-                    if let Response::Cmd(CmdResponse::Spend(Ok(()))) = resp {
-                        Some(())
-                    } else {
-                        println!("Spend error {resp:?}.");
-                        None
-                    }
-                })
-                .collect();
-
-            // We require a majority of the close group to respond with Ok.
-            if ok_responses.len() >= close_group_majority() {
-                continue;
-            } else {
-                return Err(Error::CouldNotVerifyTransfer(format!(
-                    "Not enough close group nodes accepted the spend. Got {}, required: {}.",
-                    ok_responses.len(),
-                    close_group_majority()
-                )));
-            }
         }
 
         Ok(transfer)
