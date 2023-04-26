6ae8eaa0203617b6b6723a8d885e7e9051051250
oetyng <oetyng@gmail.com>
2023-04-26 20:37:07 +0200

    chore!: simplify faucet cli

--- safenode/src/bin/faucet/mod.rs
+++ safenode/src/bin/faucet/mod.rs
@@ -6,16 +6,17 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-mod cli;
+use safenode::{
+    client::{Client, ClientEvent},
+    domain::{
+        dbc_genesis::{get_tokens_from_faucet, load_faucet_wallet},
+        wallet::parse_public_address,
+    },
+};
 
-use self::cli::{faucet_cmds, Opt};
-
-use crate::cli::SubCmd;
-
-use safenode::client::{Client, ClientEvent};
-
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use eyre::Result;
+use sn_dbc::Token;
 use tracing::info;
 
 #[tokio::main]
@@ -36,9 +37,53 @@ async fn main() -> Result<()> {
         }
     }
 
-    match opt.cmd {
-        SubCmd::Faucet(cmds) => faucet_cmds(cmds, &client).await?,
-    };
+    faucet_cmds(opt.cmd, &client).await?;
 
+    Ok(())
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Opt {
+    /// Available sub commands.
+    #[clap(subcommand)]
+    pub cmd: SubCmd,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCmd {
+    /// Claim the amount in the genesis DBC and deposit it to the faucet local wallet.
+    /// This needs to be run before a testnet is opened to the public, as to not have
+    /// the genesis claimed by someone else (the key and dbc are public for audit).
+    ClaimGenesis,
+    Send {
+        /// This shall be the number of nanos to send.
+        #[clap(name = "amount")]
+        amount: String,
+        /// This must be a hex-encoded `PublicAddress`.
+        #[clap(name = "to")]
+        to: String,
+    },
+}
+
+async fn faucet_cmds(cmds: SubCmd, client: &Client) -> Result<()> {
+    match cmds {
+        SubCmd::ClaimGenesis => {
+            let _wallet = load_faucet_wallet(client).await;
+        }
+        SubCmd::Send { amount, to } => {
+            let to = parse_public_address(to)?;
+            use std::str::FromStr;
+            let amount = Token::from_str(&amount)?;
+            if amount.as_nano() == 0 {
+                println!("Invalid format or zero amount passed in. Nothing sent.");
+                return Ok(());
+            }
+
+            let dbc = get_tokens_from_faucet(amount, to, client).await;
+            let dbc_hex = dbc.to_hex()?;
+            println!("{dbc_hex}");
+        }
+    }
     Ok(())
 }
