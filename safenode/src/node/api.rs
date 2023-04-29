ebfad8848bfd5dbd55c0b6526c8a78565af2c841
oetyng <oetyng@gmail.com>
2023-04-29 11:28:43 +0200

    fix: remove more mut access in node msg handling
    - This should decrease the cases of sequential access or deadlocks.

--- safenode/src/node/api.rs
+++ safenode/src/node/api.rs
@@ -92,7 +92,7 @@ impl Node {
             .await
             .map_err(|e| Error::CouldNotLoadWallet(e.to_string()))?;
 
-        let mut node = Self {
+        let node = Self {
             network: network.clone(),
             registers: RegisterStorage::new(root_dir),
             transfers: Transfers::new(root_dir, node_id, node_wallet),
@@ -122,9 +122,7 @@ impl Node {
         })
     }
 
-    // **** Private helpers *****
-
-    async fn handle_network_event(&mut self, event: NetworkEvent) -> Result<()> {
+    async fn handle_network_event(&self, event: NetworkEvent) -> Result<()> {
         match event {
             NetworkEvent::RequestReceived { req, channel } => {
                 self.handle_request(req, channel).await?
@@ -160,7 +158,7 @@ impl Node {
     }
 
     async fn handle_request(
-        &mut self,
+        &self,
         request: Request,
         response_channel: MsgResponder,
     ) -> Result<()> {
@@ -198,7 +196,7 @@ impl Node {
         Ok(())
     }
 
-    async fn handle_query(&mut self, query: Query) -> QueryResponse {
+    async fn handle_query(&self, query: Query) -> QueryResponse {
         match query {
             Query::Register(query) => self.registers.read(&query, User::Anyone).await,
             Query::GetChunk(address) => {
