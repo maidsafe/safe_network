dc0fd3454cb8a7377f71859e74a03143d780d4c4
oetyng <oetyng@gmail.com>
2023-05-01 10:11:43 +0200

    chore: add manual rust churn test

--- safenode/src/tests_e2e/mod.rs
+++ safenode/src/tests_e2e/mod.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use std::path::Path;
+use std::{path::Path, time::Duration};
 
 use crate::{
     client::Client,
@@ -20,6 +20,20 @@ use sn_dbc::Token;
 
 use assert_fs::TempDir;
 use eyre::Result;
+use std::process::{Command, Stdio};
+use tokio::time::sleep;
+
+#[ignore = "Manual use only."]
+#[tokio::test(flavor = "multi_thread")]
+async fn upload_churn_download() {
+    start_network();
+    sleep(Duration::from_secs(10)).await;
+    upload_files();
+    sleep(Duration::from_secs(10)).await;
+    churn().await;
+    sleep(Duration::from_secs(10)).await;
+    download_files();
+}
 
 #[ignore = "Not yet finished."]
 #[tokio::test(flavor = "multi_thread")]
@@ -79,4 +93,77 @@ async fn get_wallet(root_dir: &Path) -> LocalWallet {
     LocalWallet::load_from(root_dir)
         .await
         .expect("Wallet shall be successfully created.")
+}
+
+// Start a network.
+fn start_network() {
+    let node_bin_path = Path::new("../target/release/testnet");
+    let args = vec![
+        "--interval",
+        "1",
+        "--node-path",
+        "../target/release/safenode",
+    ];
+    let _ = Command::new(node_bin_path)
+        .args(args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Cmd should run successfully.");
+}
+
+// Upload files.
+fn upload_files() {
+    let node_bin_path = Path::new("../target/release/safe");
+    let args = vec!["files", "upload", "--", "./resources"];
+    let _ = Command::new(node_bin_path)
+        .args(args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Cmd should run successfully.");
+}
+
+// Download files.
+fn download_files() {
+    let node_bin_path = Path::new("../target/release/safe");
+    let args = vec!["files", "download"];
+    let _ = Command::new(node_bin_path)
+        .args(args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Cmd should run successfully.");
+}
+
+async fn churn() {
+    let base_port = 12000;
+    let rpc_path = Path::new("../target/release/examples/safenode_rpc_client");
+    for i in 1..26 {
+        let address = format!("127.0.0.1:{}", base_port + i);
+        let args = vec![address.as_str(), "stop", "1"];
+        let _ = Command::new(rpc_path)
+            .args(args)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .expect("safenode_rpc_client cmd should run successfully.");
+
+        let node_bin_path = Path::new("../target/release/testnet");
+        let args = vec![
+            "--join",
+            "--node-count",
+            "1",
+            "--node-path",
+            "../target/release/safenode",
+        ];
+        let _ = Command::new(node_bin_path)
+            .args(args)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .expect("Cmd should run successfully.");
+
+        sleep(Duration::from_secs(5)).await;
+    }
 }
