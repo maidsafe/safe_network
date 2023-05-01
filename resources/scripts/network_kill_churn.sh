b3809abd676a7424b0c1dd6c8676c2a9c2bebdd8
oetyng <oetyng@gmail.com>
2023-05-01 10:14:39 +0200

    chore: run kill churn on PR

---
+++ resources/scripts/network_kill_churn.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if ! command -v rg &> /dev/null; then
+  echo "ripgrep could not be found and is required"
+  exit 1
+fi
+
+log_dir=~/.safe/node/local-test-network
+
+nodes_count=$(ls $log_dir | wc -l)
+
+echo
+echo "Number of existing nodes: $nodes_count"
+
+sleep 5
+
+count=0
+while (( $count != $nodes_count ))
+do
+    ((count++))
+    target_port=$((12000 + $count))
+
+    echo Iteration $count
+    echo Stopping node on port $target_port
+    cargo run --release --example safenode_rpc_client -- "127.0.0.1:$target_port" stop 1
+    cargo run --release --bin testnet -- --join --node-count 1 --node-path ./target/release/safenode
+	sleep 5
+done
