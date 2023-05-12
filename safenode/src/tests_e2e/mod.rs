60ea3569bf285c854f1e41d415361ccc40626f4e
bochaco <gabrielviganotti@gmail.com>
2023-05-12 17:40:29 -0300

    test(register): new test to verify Registers data integrity during nodes churn

--- safenode/src/tests_e2e/mod.rs
+++ safenode/src/tests_e2e/mod.rs
@@ -6,6 +6,8 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
+mod registers_with_churn;
+
 use std::path::Path;
 
 use crate::{
