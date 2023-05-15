41dabe2cdeb86ec835ef15e8d5506e70d0361c8a
bochaco <gabrielviganotti@gmail.com>
2023-05-15 14:55:17 -0300

    test(register): minor refactor to allow extending it to test with Chunks

--- safenode/src/tests_e2e/data_with_churn.rs
+++ safenode/src/tests_e2e/data_with_churn.rs
@@ -11,7 +11,7 @@ use super::get_client;
 use crate::{
     client::{Client, Error},
     network::Error as NetworkError,
-    protocol::storage::RegisterAddress,
+    protocol::{storage::RegisterAddress, NetworkAddress},
 };
 
 use safenode_proto::{safe_node_client::SafeNodeClient, NodeInfoRequest, RestartRequest};
@@ -39,34 +39,34 @@ mod safenode_proto {
 const NODE_COUNT: u16 = 25;
 const CHURN_PERIOD_MILLIS: u64 = 30_000;
 const REGISTER_CREATION_RATIO_TO_CHURN: u64 = 10;
-const REGISTER_QUERY_RATIO_TO_CHURN: u64 = 12;
-const MAX_NUM_OF_QUERY_ATTEMPTS: u8 = 10;
+const CONTENT_QUERY_RATIO_TO_CHURN: u64 = 12;
+const MAX_NUM_OF_QUERY_ATTEMPTS: u8 = 5;
 
 // Total amount of time we run the checks for before reporting the outcome
 const TOTAL_TIME_OF_TEST: Duration = Duration::from_secs(60 * 60); // 1hr
 
-type RegistersList = Arc<RwLock<VecDeque<RegisterAddress>>>;
+type ContentList = Arc<RwLock<VecDeque<NetworkAddress>>>;
 
-struct RegError {
-    addr: RegisterAddress,
+struct ContentError {
+    net_addr: NetworkAddress,
     attempts: u8,
     last_err: Error,
 }
 
-impl fmt::Debug for RegError {
+impl fmt::Debug for ContentError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
             "{:?}, attempts: {}, last error: {:?}",
-            self.addr, self.attempts, self.last_err
+            self.net_addr, self.attempts, self.last_err
         )
     }
 }
 
-type RegistersErredList = Arc<RwLock<BTreeMap<RegisterAddress, RegError>>>;
+type ContentErredList = Arc<RwLock<BTreeMap<NetworkAddress, ContentError>>>;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn register_data_during_churn() -> Result<()> {
+async fn data_availability_during_churn() -> Result<()> {
     let mut rng = rand::thread_rng();
     println!("Creating a client...");
     let client = get_client().await;
@@ -75,23 +75,23 @@ async fn register_data_during_churn() -> Result<()> {
     // Spawn a task to churn nodes
     churn_nodes_task();
 
-    // Shared bucket where we keep track of Registers created
-    let registers = RegistersList::default();
+    // Shared bucket where we keep track of content created/stored on the network
+    let content = ContentList::default();
 
-    // Shared bucket where we keep track of those Registers which erred when creating/fetching them.
+    // Shared bucket where we keep track of the content which erred when creating/storing/fetching.
     // We remove them from this bucket if we are then able to query/fetch them successfully.
     // We only try to query them 'MAX_NUM_OF_QUERY_ATTEMPTS' times, then report them effectivelly as failures.
-    let regs_erred = RegistersErredList::default();
+    let content_erred = ContentErredList::default();
 
-    // Shared bucket where we keep track of those Registers we failed to fetch for 'MAX_NUM_OF_QUERY_ATTEMPTS' times.
-    let failures = RegistersErredList::default();
+    // Shared bucket where we keep track of the content we failed to fetch for 'MAX_NUM_OF_QUERY_ATTEMPTS' times.
+    let failures = ContentErredList::default();
 
-    // Spawn a task to randomly query/fetch the Registers we create
-    query_registers_task(client.clone(), registers.clone(), regs_erred.clone());
+    // Spawn a task to randomly query/fetch the content we create/store
+    query_content_task(client.clone(), content.clone(), content_erred.clone());
 
-    // Spawn a task to retry querying the Registers that failed, up to 'MAX_NUM_OF_QUERY_ATTEMPTS' times,
+    // Spawn a task to retry querying the content that failed, up to 'MAX_NUM_OF_QUERY_ATTEMPTS' times,
     // and mark them as failures if they effectivelly cannot be retrieved.
-    retry_query_registers_task(client.clone(), regs_erred.clone(), failures.clone());
+    retry_query_content_task(client.clone(), content_erred.clone(), failures.clone());
 
     let start_time = Instant::now();
 
@@ -105,23 +105,24 @@ async fn register_data_during_churn() -> Result<()> {
         println!("Creating Register at {addr:?} in {delay:?}");
         sleep(delay).await;
 
+        let net_addr = NetworkAddress::RegisterAddress(addr);
         match client.create_register(xorname, tag).await {
             Err(err @ Error::Network(NetworkError::OutboundError(_))) => {
                 println!("Error (recoverable) when creating Register at {addr:?}: {err:?}");
-                let _ = regs_erred.write().await.insert(
-                    addr,
-                    RegError {
-                        addr,
+                let _ = content_erred.write().await.insert(
+                    net_addr.clone(),
+                    ContentError {
+                        net_addr: net_addr.clone(),
                         attempts: 0,
                         last_err: err,
                     },
                 );
-                registers.write().await.push_back(addr);
+                content.write().await.push_back(net_addr);
             }
             Err(err) => {
                 println!("Discarding Register due to error when creating it {addr:?}: {err:?}")
             }
-            Ok(_) => registers.write().await.push_back(addr),
+            Ok(_) => content.write().await.push_back(net_addr),
         }
 
         let failed = failures.read().await;
@@ -146,39 +147,41 @@ async fn register_data_during_churn() -> Result<()> {
     Ok(())
 }
 
-// Spawns a task which periodically queries a Register by randomly choosing it from the list
-// of registers created by another task.
-fn query_registers_task(client: Client, registers: RegistersList, regs_erred: RegistersErredList) {
+// Spawns a task which periodically queries a content by randomly choosing it from the list
+// of content created by another task.
+fn query_content_task(client: Client, content: ContentList, content_erred: ContentErredList) {
     let _handle = tokio::spawn(async move {
-        let delay = Duration::from_millis(CHURN_PERIOD_MILLIS / REGISTER_QUERY_RATIO_TO_CHURN);
+        let delay = Duration::from_millis(CHURN_PERIOD_MILLIS / CONTENT_QUERY_RATIO_TO_CHURN);
         loop {
-            let len = registers.read().await.len();
+            let len = content.read().await.len();
             if len == 0 {
-                println!("No Registers created just yet, let's try in {delay:?} ...");
+                println!("No content created/stored just yet, let's try in {delay:?} ...");
                 sleep(delay).await;
                 continue;
             }
 
-            // let's choose a random Register to query, picking it from the list of created
+            // let's choose a random content to query, picking it from the list of created
             let index = rand::thread_rng().gen_range(0..len);
-            let addr = registers.read().await[index];
-            println!("Querying Register (bucket index: {index}) at {addr:?} in {delay:?}");
+            let net_addr = content.read().await[index].clone();
+            println!("Querying content (bucket index: {index}) at {net_addr:?} in {delay:?}");
             sleep(delay).await;
 
-            match client.get_register(*addr.name(), addr.tag()).await {
+            match query_content(&client, &net_addr).await {
                 Ok(_) => {
-                    let _ = regs_erred.write().await.remove(&addr);
+                    let _ = content_erred.write().await.remove(&net_addr);
                 }
                 Err(last_err) => {
-                    println!("Failed to query Register (index: {index}) at {addr:?}: {last_err:?}");
+                    println!(
+                        "Failed to query content (index: {index}) at {net_addr:?}: {last_err:?}"
+                    );
                     // mark it to try 'MAX_NUM_OF_QUERY_ATTEMPTS' times.
-                    let _ = regs_erred
+                    let _ = content_erred
                         .write()
                         .await
-                        .entry(addr)
+                        .entry(net_addr.clone())
                         .and_modify(|curr| curr.attempts += 1)
-                        .or_insert(RegError {
-                            addr,
+                        .or_insert(ContentError {
+                            net_addr,
                             attempts: 1,
                             last_err,
                         });
@@ -208,12 +211,12 @@ fn churn_nodes_task() {
     });
 }
 
-// Checks (periodically) for any registers that an error was reported either at the moment of its creation or
+// Checks (periodically) for any content that an error was reported either at the moment of its creation or
 // in a later query attempt.
-fn retry_query_registers_task(
+fn retry_query_content_task(
     client: Client,
-    regs_erred: RegistersErredList,
-    failures: RegistersErredList,
+    content_erred: ContentErredList,
+    failures: ContentErredList,
 ) {
     let _handle = tokio::spawn(async move {
         let delay = Duration::from_millis(2 * CHURN_PERIOD_MILLIS);
@@ -221,22 +224,23 @@ fn retry_query_registers_task(
             sleep(delay).await;
 
             // let's try to query from the bucket of those that erred upon creation/query
-            let erred = regs_erred.write().await.pop_first();
+            let erred = content_erred.write().await.pop_first();
 
-            if let Some((addr, mut reg_error)) = erred {
-                let attempts = reg_error.attempts + 1;
-                println!("Querying erred Register at {addr:?}, attempt: #{attempts} ...");
-                if let Err(last_err) = client.get_register(*addr.name(), addr.tag()).await {
-                    println!("Erred Register is still not retrievable at {addr:?} after {attempts} attempts: {last_err:?}");
+            if let Some((net_addr, mut content_error)) = erred {
+                let attempts = content_error.attempts + 1;
+
+                println!("Querying erred content at {net_addr:?}, attempt: #{attempts} ...");
+                if let Err(last_err) = query_content(&client, &net_addr).await {
+                    println!("Erred content is still not retrievable at {net_addr:?} after {attempts} attempts: {last_err:?}");
                     // We only keep it to retry 'MAX_NUM_OF_QUERY_ATTEMPTS' times,
                     // otherwise report it effectivelly as failure.
-                    reg_error.attempts = attempts;
-                    reg_error.last_err = last_err;
+                    content_error.attempts = attempts;
+                    content_error.last_err = last_err;
 
                     if attempts == MAX_NUM_OF_QUERY_ATTEMPTS {
-                        let _ = failures.write().await.insert(addr, reg_error);
+                        let _ = failures.write().await.insert(net_addr, content_error);
                     } else {
-                        let _ = regs_erred.write().await.insert(addr, reg_error);
+                        let _ = content_erred.write().await.insert(net_addr, content_error);
                     }
                 }
             }
@@ -262,4 +266,15 @@ async fn node_restart(addr: SocketAddr) -> Result<()> {
     );
 
     Ok(())
+}
+
+async fn query_content(client: &Client, net_addr: &NetworkAddress) -> Result<(), Error> {
+    // TODO: support querying Chunks
+    match net_addr {
+        NetworkAddress::RegisterAddress(addr) => {
+            let _ = client.get_register(*addr.name(), addr.tag()).await?;
+            Ok(())
+        }
+        _other => Ok(()),
+    }
 }
