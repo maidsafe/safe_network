2eac7f336c10f9aeb645b9822a50e40191c1d311
bochaco <gabrielviganotti@gmail.com>
2023-05-15 14:55:17 -0300

    test(register): minor refactor to allow extending it to test with Chunks

--- safenode/src/tests_e2e/mod.rs
+++ safenode/src/tests_e2e/mod.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-mod registers_with_churn;
+mod data_with_churn;
 
 use std::path::Path;
 
