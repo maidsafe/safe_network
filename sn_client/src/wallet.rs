02eba57f0653d83259245f68c7f04aace9074345
bochaco <gabrielviganotti@gmail.com>
2023-05-29 14:49:26 -0300

    feat: Chunk storage payment and building payment proofs

--- sn_client/src/wallet.rs
+++ sn_client/src/wallet.rs
@@ -8,12 +8,15 @@
 
 use super::Client;
 
+use sn_protocol::NetworkAddress;
 use sn_transfers::{
     client_transfers::TransferOutputs,
     wallet::{Error, LocalWallet, Result},
 };
 
+use bls::SecretKey;
 use futures::future::join_all;
+use merkletree::{merkle::MerkleTree, store::VecStore};
 use sn_dbc::{Dbc, PublicAddress, Token};
 
 /// A wallet client can be used to send and
@@ -45,7 +48,7 @@ impl WalletClient {
         self.resend_pending_txs().await;
 
         // offline transfer
-        let transfer = self.wallet.local_send(vec![(amount, to)]).await?;
+        let transfer = self.wallet.local_send(vec![(amount, to)], None).await?;
         let dbcs = transfer.created_dbcs.clone();
 
         // send to network
@@ -57,6 +60,40 @@ impl WalletClient {
 
         // return created DBCs even if network part failed???
         match &dbcs[..] {
+            [info, ..] => Ok(info.dbc.clone()),
+            [] => Err(Error::CouldNotSendTokens(
+                "No DBCs were returned from the wallet.".into(),
+            )),
+        }
+    }
+
+    /// Send tokens to nodes closest to the data we want to store and pay for.
+    pub async fn pay_for_storage(
+        &mut self,
+        chunks: impl Iterator<Item = &NetworkAddress>,
+    ) -> Result<Dbc> {
+        // FIXME: calculate the amount to pay to each node, perhaps just 1 nano to begin with.
+        let amount = Token::from_nano(1);
+
+        // Let's build the Merkle-tree to obtain the reason-hash
+        let tree_leaves: Vec<MerkleTreeItem> = chunks
+            .map(|c| MerkleTreeItem::from_slice(&c.as_bytes()))
+            .collect();
+        let tree = MerkleTree::<_, _, VecStore<MerkleTreeItem>>::from_data(tree_leaves)
+            .map_err(|err| Error::StoragePaymentReason(err.to_string()))?;
+
+        // The reason hash is set to be the root of the merkle-tree of chunks to pay for
+        let reason_hash = tree.root().0.into();
+
+        // FIXME: calculate closest nodes to pay for storage
+        let to = PublicAddress::new(SecretKey::random().public_key());
+
+        let transfer = self
+            .wallet
+            .local_send(vec![(amount, to)], Some(reason_hash))
+            .await?;
+
+        match &transfer.created_dbcs[..] {
             [info, ..] => Ok(info.dbc.clone()),
             [] => Err(Error::CouldNotSendTokens(
                 "No DBCs were returned from the wallet.".into(),
@@ -149,4 +186,98 @@ pub async fn send(from: LocalWallet, amount: Token, to: PublicAddress, client: &
         .expect("Created dbc shall be successfully stored.");
 
     new_dbc
+}
+
+use merkletree::{
+    hash::{Algorithm, Hashable},
+    merkle::Element,
+};
+use std::fmt;
+use tiny_keccak::{Hasher, Sha3};
+
+const SIZE: usize = 32;
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Debug, Default)]
+struct MerkleTreeItem([u8; SIZE]);
+
+impl AsRef<[u8]> for MerkleTreeItem {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl Element for MerkleTreeItem {
+    fn byte_len() -> usize {
+        SIZE
+    }
+
+    fn from_slice(bytes: &[u8]) -> Self {
+        assert_eq!(bytes.len(), Self::byte_len());
+        let mut el = [0u8; SIZE];
+        el[..].copy_from_slice(bytes);
+        MerkleTreeItem(el)
+    }
+
+    fn copy_to_slice(&self, bytes: &mut [u8]) {
+        bytes.copy_from_slice(&self.0);
+    }
+}
+
+struct TestSha256Hasher {
+    engine: Sha3,
+}
+
+impl TestSha256Hasher {
+    fn new() -> TestSha256Hasher {
+        TestSha256Hasher {
+            engine: Sha3::v256(),
+        }
+    }
+}
+
+impl fmt::Debug for TestSha256Hasher {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Sha256Hasher")
+    }
+}
+
+impl Default for TestSha256Hasher {
+    fn default() -> Self {
+        TestSha256Hasher::new()
+    }
+}
+
+impl std::hash::Hasher for TestSha256Hasher {
+    fn finish(&self) -> u64 {
+        unimplemented!()
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.engine.update(bytes)
+    }
+}
+
+impl Hashable<TestSha256Hasher> for MerkleTreeItem {
+    // Required method
+    fn hash(&self, state: &mut TestSha256Hasher) {}
+}
+
+impl Algorithm<MerkleTreeItem> for TestSha256Hasher {
+    fn hash(&mut self) -> MerkleTreeItem {
+        let mut result = MerkleTreeItem::default();
+        let item_size = result.0.len();
+
+        let sha3 = self.engine.clone();
+        let mut hash = [0u8; 32];
+        sha3.finalize(&mut hash);
+        let hash_output = hash.to_vec();
+
+        if item_size < hash_output.len() {
+            result
+                .0
+                .copy_from_slice(&hash_output.as_slice()[0..item_size]);
+        } else {
+            result.0.copy_from_slice(hash_output.as_slice())
+        }
+        result
+    }
 }
