2b31bf8187272d733d32cbcdfc10c593c4bff12c
Josh Wilson <joshuef@gmail.com>
2023-05-29 09:15:46 +0900

    feat: add build_info crate

--- sn_node/build.rs
+++ sn_node/build.rs
@@ -5,20 +5,7 @@
 // under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
-use std::process::Command;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("./src/protocol/safenode_proto/safenode.proto")?;
-
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .expect("Failed to execute git command");
-
-    let git_hash = String::from_utf8(output.stdout).unwrap().trim().to_string();
-
-    // Set the Git hash as an environment variable
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
-
     Ok(())
 }
