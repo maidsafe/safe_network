4c89af5d6b9b3844ce6ea7d66c1df458fe22935d
Josh Wilson <joshuef@gmail.com>
2023-05-30 11:45:14 +0900

    fix(node): increase replication range to 5.
    e2e churn tests were failing at 3

--- sn_networking/src/event.rs
+++ sn_networking/src/event.rs
@@ -48,7 +48,7 @@ const DEAD_PEER_DETECTION_THRESHOLD: usize = 3;
 // Defines how close that a node will trigger repliation.
 // That is, the node has to be among the REPLICATION_RANGE closest to data,
 // to carry out the replication.
-const REPLICATION_RANGE: usize = 3;
+const REPLICATION_RANGE: usize = 5;
 
 #[derive(NetworkBehaviour)]
 #[behaviour(out_event = "NodeEvent")]
