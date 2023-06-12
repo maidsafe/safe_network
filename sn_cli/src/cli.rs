7ea45a34ae4a5d8b3e4733cef0ab6bc92d75bc9f
Benno Zeeman <bzeeman@live.nl>
2023-06-12 12:48:02 +0200

    refactor: use clap env and parse multiaddr

--- sn_cli/src/cli.rs
+++ sn_cli/src/cli.rs
@@ -6,31 +6,20 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use crate::subcommands::SubCmd;
 use clap::Parser;
 use color_eyre::Result;
-use libp2p::Multiaddr;
 use std::time::Duration;
+
+use crate::subcommands::SubCmd;
+use sn_peers_acquisition::PeersArgs;
+
 // Please do not remove the blank lines in these doc comments.
 // They are used for inserting line breaks when the help menu is rendered in the UI.
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct Opt {
-    /// Provide a peer to connect to a public network, using the MultiAddr format.
-    ///
-    /// An example MultiAddr:
-    ///
-    /// /ip4/13.40.152.226/udp/12000/quic-v1/p2p/12D3KooWRi6wF7yxWLuPSNskXc6kQ5cJ6eaymeMbCRdTnMesPgFx
-    ///
-    /// Noteworthy are the second, fourth, and last parts.
-    ///
-    /// Those are the IP address and UDP port the peer is listening on, and its peer ID, respectively.
-    ///
-    /// Many peers can be provided by using the argument multiple times.
-    ///
-    /// If none are provided, a connection will be attempted to a local network.
-    #[clap(long = "peer", global = true)]
-    pub peers: Vec<Multiaddr>,
+    #[command(flatten)]
+    pub(crate) peers: PeersArgs,
 
     /// Available sub commands.
     #[clap(subcommand)]
