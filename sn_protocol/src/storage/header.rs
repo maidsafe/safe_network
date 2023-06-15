91aeea37512749825234e9f2bdec5a16e90b4dd5
RolandSherwin <RolandSherwin@protonmail.com>
2023-06-15 22:05:15 +0530

    fix(network): use `rmp_serde` for `RecordHeader` ser/de

--- sn_protocol/src/storage/header.rs
+++ sn_protocol/src/storage/header.rs
@@ -30,30 +30,37 @@ impl Display for RecordKind {
 }
 
 impl RecordHeader {
-    // Bincode serializes enums with unit variants as a u32, not a u8, so it would take up 4 bytes.
-    pub const SIZE: usize = 4;
+    pub const SIZE: usize = 7;
+
+    pub fn try_serialize(self) -> Result<Vec<u8>> {
+        rmp_serde::to_vec(&self).map_err(|_| Error::RecordHeaderParsingFailed)
+    }
+
+    pub fn try_deserialize(bytes: &[u8]) -> Result<Self> {
+        rmp_serde::from_slice(bytes).map_err(|_| Error::RecordHeaderParsingFailed)
+    }
 
     pub fn from_record(record: &Record) -> Result<Self> {
-        bincode::deserialize(&record.value[..RecordHeader::SIZE + 1])
+        Self::try_deserialize(&record.value[..RecordHeader::SIZE + 1])
             .map_err(|_| Error::RecordHeaderParsingFailed)
     }
 }
 
+/// Utility to deserialize a `KAD::Record` into any type.
+/// Use `RecordHeader::from_record` if you want the `RecordHeader` instead.
 pub fn try_deserialize_record<T: serde::de::DeserializeOwned>(record: &Record) -> Result<T> {
     let bytes = &record.value[RecordHeader::SIZE..];
-    bincode::deserialize(bytes).map_err(|_| Error::RecordParsingFailed)
+    rmp_serde::from_slice(bytes).map_err(|_| Error::RecordParsingFailed)
 }
 
+/// Utility to serialize the provided data along with the RecordKind to be stored as Record::value
 pub fn try_serialize_record<T: serde::Serialize>(
     data: &T,
     record_kind: RecordKind,
 ) -> Result<Vec<u8>> {
-    let payload = bincode::serialize(data).map_err(|_| Error::RecordParsingFailed)?;
-    let record_header = RecordHeader { kind: record_kind };
-
-    let mut record_value =
-        bincode::serialize(&record_header).map_err(|_| Error::RecordParsingFailed)?;
-
+    let payload = rmp_serde::to_vec(data).map_err(|_| Error::RecordParsingFailed)?;
+    let mut record_value = RecordHeader { kind: record_kind }.try_serialize()?;
     record_value.extend(payload);
+
     Ok(record_value)
 }
