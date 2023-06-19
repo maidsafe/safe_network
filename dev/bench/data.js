045cf3544a6994794a198fceb263a276acb15186
github-action-benchmark <github@users.noreply.github.com>
2023-06-19 13:30:35 +0000

    add Client memory during upload (customSmallerIsBetter) benchmark result for d6223ee3453a70c26e74fbdb948d1fb6beb65fa4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687181432550,
+  "lastUpdate": 1687181435822,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "Safe `files` benchmarks": [
@@ -321,6 +321,36 @@ window.BENCHMARK_DATA = {
         ]
       }
     ],
-    "Client memory during upload": []
+    "Client memory during upload": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
+          "message": "fix(CI): correct client mem usage in the graph",
+          "timestamp": "2023-06-19T22:10:48+09:00",
+          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
+          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
+        },
+        "date": 1687181434945,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "safe-cli-memory-usage",
+            "value": 166.31,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
   }
 }
