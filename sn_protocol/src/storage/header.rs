a456983dee359662fa3da3f29fa10abc7def4a4f
RolandSherwin <RolandSherwin@protonmail.com>
2023-06-19 15:22:47 +0530

    fix(record_header): encode unit enum as u32 - rmp_serde serializes unit variant as it pleases, thus the encode size   is not fixed.

--- sn_protocol/src/storage/header.rs
+++ sn_protocol/src/storage/header.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use crate::error::{Error, Result};
+use crate::error::Error;
 use libp2p::kad::Record;
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
@@ -16,13 +16,42 @@ pub struct RecordHeader {
     pub kind: RecordKind,
 }
 
-#[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum RecordKind {
     Chunk,
     DbcSpend,
     Register,
 }
 
+impl Serialize for RecordKind {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match *self {
+            Self::Chunk => serializer.serialize_u32(0),
+            Self::DbcSpend => serializer.serialize_u32(1),
+            Self::Register => serializer.serialize_u32(2),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for RecordKind {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let num = u32::deserialize(deserializer)?;
+        match num {
+            0 => Ok(Self::Chunk),
+            1 => Ok(Self::DbcSpend),
+            2 => Ok(Self::Register),
+            _ => Err(serde::de::Error::custom(
+                "Unexpected integer for RecordKind variant",
+            )),
+        }
+    }
+}
 impl Display for RecordKind {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "RecordKind({self:?})")
@@ -30,17 +59,17 @@ impl Display for RecordKind {
 }
 
 impl RecordHeader {
-    pub const SIZE: usize = 7;
+    pub const SIZE: usize = 2;
 
-    pub fn try_serialize(self) -> Result<Vec<u8>> {
+    pub fn try_serialize(self) -> Result<Vec<u8>, Error> {
         rmp_serde::to_vec(&self).map_err(|_| Error::RecordHeaderParsingFailed)
     }
 
-    pub fn try_deserialize(bytes: &[u8]) -> Result<Self> {
+    pub fn try_deserialize(bytes: &[u8]) -> Result<Self, Error> {
         rmp_serde::from_slice(bytes).map_err(|_| Error::RecordHeaderParsingFailed)
     }
 
-    pub fn from_record(record: &Record) -> Result<Self> {
+    pub fn from_record(record: &Record) -> Result<Self, Error> {
         Self::try_deserialize(&record.value[..RecordHeader::SIZE + 1])
             .map_err(|_| Error::RecordHeaderParsingFailed)
     }
@@ -48,7 +77,7 @@ impl RecordHeader {
 
 /// Utility to deserialize a `KAD::Record` into any type.
 /// Use `RecordHeader::from_record` if you want the `RecordHeader` instead.
-pub fn try_deserialize_record<T: serde::de::DeserializeOwned>(record: &Record) -> Result<T> {
+pub fn try_deserialize_record<T: serde::de::DeserializeOwned>(record: &Record) -> Result<T, Error> {
     let bytes = &record.value[RecordHeader::SIZE..];
     rmp_serde::from_slice(bytes).map_err(|_| Error::RecordParsingFailed)
 }
@@ -57,10 +86,39 @@ pub fn try_deserialize_record<T: serde::de::DeserializeOwned>(record: &Record) -
 pub fn try_serialize_record<T: serde::Serialize>(
     data: &T,
     record_kind: RecordKind,
-) -> Result<Vec<u8>> {
+) -> Result<Vec<u8>, Error> {
     let payload = rmp_serde::to_vec(data).map_err(|_| Error::RecordParsingFailed)?;
     let mut record_value = RecordHeader { kind: record_kind }.try_serialize()?;
     record_value.extend(payload);
 
     Ok(record_value)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{RecordHeader, RecordKind};
+    use crate::error::Result;
+
+    #[test]
+    fn verify_record_header_encoded_size() -> Result<()> {
+        let chunk = RecordHeader {
+            kind: RecordKind::Chunk,
+        }
+        .try_serialize()?;
+        assert_eq!(chunk.len(), RecordHeader::SIZE);
+
+        let spend = RecordHeader {
+            kind: RecordKind::DbcSpend,
+        }
+        .try_serialize()?;
+        assert_eq!(spend.len(), RecordHeader::SIZE);
+
+        let register = RecordHeader {
+            kind: RecordKind::Register,
+        }
+        .try_serialize()?;
+        assert_eq!(register.len(), RecordHeader::SIZE);
+
+        Ok(())
+    }
 }
