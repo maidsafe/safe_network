53e213402da221c5db5ad795ffa1d5e4a4785fda
Josh Wilson <joshuef@gmail.com>
2023-06-20 12:50:19 +0900

    merge 'safe files' benches

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -2,212 +2,6 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1687232775622,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
-    "Safe `files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "31c4e717fa22d96a1956fc48e83aa990739ce6c8",
-          "message": "ci: fix escaping on benchmark prs",
-          "timestamp": "2023-06-19T13:54:13+09:00",
-          "tree_id": "47aacf3288cc89b4a6160c58a6c3aedb556ae590",
-          "url": "https://github.com/maidsafe/safe_network/commit/31c4e717fa22d96a1956fc48e83aa990739ce6c8"
-        },
-        "date": 1687151490105,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5436252522185475,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 2.88777892701517,
-            "unit": "MiB/s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "ccc4cea83798d4db399c89e70c3880ad8653aa12",
-          "message": "fix: ci chart generation using non-linux cmd",
-          "timestamp": "2023-06-19T14:28:54+09:00",
-          "tree_id": "88bef75649cc4dd225bfbcc42ffc1fa70d1f0127",
-          "url": "https://github.com/maidsafe/safe_network/commit/ccc4cea83798d4db399c89e70c3880ad8653aa12"
-        },
-        "date": 1687153483927,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5929951380245495,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 3.1350951604772725,
-            "unit": "MiB/s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "93f9b3e4019f6acd31ca096c672f72a9f08c3ee6",
-          "message": "fix: unit for client chart",
-          "timestamp": "2023-06-19T15:19:57+09:00",
-          "tree_id": "aec68c8f6a2176adf213840249ac5c6726d1e61e",
-          "url": "https://github.com/maidsafe/safe_network/commit/93f9b3e4019f6acd31ca096c672f72a9f08c3ee6"
-        },
-        "date": 1687156543530,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5834937720422131,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 3.078326451029672,
-            "unit": "MiB/s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "f81acd200128e520dbe643d051da39211b32dc22",
-          "message": "ci: fix missing mb->kb change for client mem measurements",
-          "timestamp": "2023-06-19T20:48:01+09:00",
-          "tree_id": "40192d2d04a315192f3f1bc21ac6b5b431a49c59",
-          "url": "https://github.com/maidsafe/safe_network/commit/f81acd200128e520dbe643d051da39211b32dc22"
-        },
-        "date": 1687176589501,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5829100360585217,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 3.1337843815001385,
-            "unit": "MiB/s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
-          "message": "fix(CI): correct client mem usage in the graph",
-          "timestamp": "2023-06-19T22:10:48+09:00",
-          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
-          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
-        },
-        "date": 1687181326348,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5418745092670142,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 2.9514069795635898,
-            "unit": "MiB/s"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "8260565a50e20365d0718492282f4196299724a7",
-          "message": "feat(CI): check client average memory usage during upload large file",
-          "timestamp": "2023-06-20T09:11:32+09:00",
-          "tree_id": "ef5d96d98b1e3cf36e327c5d44f1e066e974746a",
-          "url": "https://github.com/maidsafe/safe_network/commit/8260565a50e20365d0718492282f4196299724a7"
-        },
-        "date": 1687220848476,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5459711400724478,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 3.0499356648393197,
-            "unit": "MiB/s"
-          }
-        ]
-      }
-    ],
     "Node memory usage": [
       {
         "commit": {
@@ -416,6 +210,210 @@ window.BENCHMARK_DATA = {
       }
     ],
     "`safe files` benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "31c4e717fa22d96a1956fc48e83aa990739ce6c8",
+          "message": "ci: fix escaping on benchmark prs",
+          "timestamp": "2023-06-19T13:54:13+09:00",
+          "tree_id": "47aacf3288cc89b4a6160c58a6c3aedb556ae590",
+          "url": "https://github.com/maidsafe/safe_network/commit/31c4e717fa22d96a1956fc48e83aa990739ce6c8"
+        },
+        "date": 1687151490105,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.5436252522185475,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 2.88777892701517,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "ccc4cea83798d4db399c89e70c3880ad8653aa12",
+          "message": "fix: ci chart generation using non-linux cmd",
+          "timestamp": "2023-06-19T14:28:54+09:00",
+          "tree_id": "88bef75649cc4dd225bfbcc42ffc1fa70d1f0127",
+          "url": "https://github.com/maidsafe/safe_network/commit/ccc4cea83798d4db399c89e70c3880ad8653aa12"
+        },
+        "date": 1687153483927,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.5929951380245495,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 3.1350951604772725,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "93f9b3e4019f6acd31ca096c672f72a9f08c3ee6",
+          "message": "fix: unit for client chart",
+          "timestamp": "2023-06-19T15:19:57+09:00",
+          "tree_id": "aec68c8f6a2176adf213840249ac5c6726d1e61e",
+          "url": "https://github.com/maidsafe/safe_network/commit/93f9b3e4019f6acd31ca096c672f72a9f08c3ee6"
+        },
+        "date": 1687156543530,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.5834937720422131,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 3.078326451029672,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "f81acd200128e520dbe643d051da39211b32dc22",
+          "message": "ci: fix missing mb->kb change for client mem measurements",
+          "timestamp": "2023-06-19T20:48:01+09:00",
+          "tree_id": "40192d2d04a315192f3f1bc21ac6b5b431a49c59",
+          "url": "https://github.com/maidsafe/safe_network/commit/f81acd200128e520dbe643d051da39211b32dc22"
+        },
+        "date": 1687176589501,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.5829100360585217,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 3.1337843815001385,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
+          "message": "fix(CI): correct client mem usage in the graph",
+          "timestamp": "2023-06-19T22:10:48+09:00",
+          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
+          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
+        },
+        "date": 1687181326348,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.5418745092670142,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 2.9514069795635898,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "8260565a50e20365d0718492282f4196299724a7",
+          "message": "feat(CI): check client average memory usage during upload large file",
+          "timestamp": "2023-06-20T09:11:32+09:00",
+          "tree_id": "ef5d96d98b1e3cf36e327c5d44f1e066e974746a",
+          "url": "https://github.com/maidsafe/safe_network/commit/8260565a50e20365d0718492282f4196299724a7"
+        },
+        "date": 1687220848476,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.5459711400724478,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 3.0499356648393197,
+            "unit": "MiB/s"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
