e3c7b00dcea5f12469afae4efbc250d8dbbe2d5d
Josh Wilson <joshuef@gmail.com>
2023-06-20 12:52:44 +0900

    merge benches

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -2,213 +2,6 @@ window.BENCHMARK_DATA = {
   "lastUpdate": 1687232775622,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
-    "Node memory usage": [
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "31c4e717fa22d96a1956fc48e83aa990739ce6c8",
-          "message": "ci: fix escaping on benchmark prs",
-          "timestamp": "2023-06-19T13:54:13+09:00",
-          "tree_id": "47aacf3288cc89b4a6160c58a6c3aedb556ae590",
-          "url": "https://github.com/maidsafe/safe_network/commit/31c4e717fa22d96a1956fc48e83aa990739ce6c8"
-        },
-        "date": 1687151578044,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "node-memory-usage-through-safe-benchmark",
-            "value": 10.16,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "ccc4cea83798d4db399c89e70c3880ad8653aa12",
-          "message": "fix: ci chart generation using non-linux cmd",
-          "timestamp": "2023-06-19T14:28:54+09:00",
-          "tree_id": "88bef75649cc4dd225bfbcc42ffc1fa70d1f0127",
-          "url": "https://github.com/maidsafe/safe_network/commit/ccc4cea83798d4db399c89e70c3880ad8653aa12"
-        },
-        "date": 1687153573359,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "node-memory-usage-through-safe-benchmark",
-            "value": 11.38,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "93f9b3e4019f6acd31ca096c672f72a9f08c3ee6",
-          "message": "fix: unit for client chart",
-          "timestamp": "2023-06-19T15:19:57+09:00",
-          "tree_id": "aec68c8f6a2176adf213840249ac5c6726d1e61e",
-          "url": "https://github.com/maidsafe/safe_network/commit/93f9b3e4019f6acd31ca096c672f72a9f08c3ee6"
-        },
-        "date": 1687156631175,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "node-memory-usage-through-safe-benchmark",
-            "value": 10.13,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "f81acd200128e520dbe643d051da39211b32dc22",
-          "message": "ci: fix missing mb->kb change for client mem measurements",
-          "timestamp": "2023-06-19T20:48:01+09:00",
-          "tree_id": "40192d2d04a315192f3f1bc21ac6b5b431a49c59",
-          "url": "https://github.com/maidsafe/safe_network/commit/f81acd200128e520dbe643d051da39211b32dc22"
-        },
-        "date": 1687176675200,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "node-memory-usage-through-safe-benchmark",
-            "value": 10.13,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
-          "message": "fix(CI): correct client mem usage in the graph",
-          "timestamp": "2023-06-19T22:10:48+09:00",
-          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
-          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
-        },
-        "date": 1687181431764,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "node-memory-usage-through-safe-benchmark",
-            "value": 11.54,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "8260565a50e20365d0718492282f4196299724a7",
-          "message": "feat(CI): check client average memory usage during upload large file",
-          "timestamp": "2023-06-20T09:11:32+09:00",
-          "tree_id": "ef5d96d98b1e3cf36e327c5d44f1e066e974746a",
-          "url": "https://github.com/maidsafe/safe_network/commit/8260565a50e20365d0718492282f4196299724a7"
-        },
-        "date": 1687220936505,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "node-memory-usage-through-safe-benchmark",
-            "value": 11.24,
-            "unit": "MB"
-          }
-        ]
-      }
-    ],
-    "Client memory during upload": [
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
-          "message": "fix(CI): correct client mem usage in the graph",
-          "timestamp": "2023-06-19T22:10:48+09:00",
-          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
-          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
-        },
-        "date": 1687181434945,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "safe-cli-memory-usage",
-            "value": 166.31,
-            "unit": "MB"
-          }
-        ]
-      }
-    ],
     "`safe files` benchmarks": [
       {
         "commit": {
@@ -565,6 +358,180 @@ window.BENCHMARK_DATA = {
             "username": "joshuef"
           },
           "distinct": true,
+          "id": "31c4e717fa22d96a1956fc48e83aa990739ce6c8",
+          "message": "ci: fix escaping on benchmark prs",
+          "timestamp": "2023-06-19T13:54:13+09:00",
+          "tree_id": "47aacf3288cc89b4a6160c58a6c3aedb556ae590",
+          "url": "https://github.com/maidsafe/safe_network/commit/31c4e717fa22d96a1956fc48e83aa990739ce6c8"
+        },
+        "date": 1687151578044,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node-memory-usage-through-safe-benchmark",
+            "value": 10.16,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "ccc4cea83798d4db399c89e70c3880ad8653aa12",
+          "message": "fix: ci chart generation using non-linux cmd",
+          "timestamp": "2023-06-19T14:28:54+09:00",
+          "tree_id": "88bef75649cc4dd225bfbcc42ffc1fa70d1f0127",
+          "url": "https://github.com/maidsafe/safe_network/commit/ccc4cea83798d4db399c89e70c3880ad8653aa12"
+        },
+        "date": 1687153573359,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node-memory-usage-through-safe-benchmark",
+            "value": 11.38,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "93f9b3e4019f6acd31ca096c672f72a9f08c3ee6",
+          "message": "fix: unit for client chart",
+          "timestamp": "2023-06-19T15:19:57+09:00",
+          "tree_id": "aec68c8f6a2176adf213840249ac5c6726d1e61e",
+          "url": "https://github.com/maidsafe/safe_network/commit/93f9b3e4019f6acd31ca096c672f72a9f08c3ee6"
+        },
+        "date": 1687156631175,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node-memory-usage-through-safe-benchmark",
+            "value": 10.13,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "f81acd200128e520dbe643d051da39211b32dc22",
+          "message": "ci: fix missing mb->kb change for client mem measurements",
+          "timestamp": "2023-06-19T20:48:01+09:00",
+          "tree_id": "40192d2d04a315192f3f1bc21ac6b5b431a49c59",
+          "url": "https://github.com/maidsafe/safe_network/commit/f81acd200128e520dbe643d051da39211b32dc22"
+        },
+        "date": 1687176675200,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node-memory-usage-through-safe-benchmark",
+            "value": 10.13,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
+          "message": "fix(CI): correct client mem usage in the graph",
+          "timestamp": "2023-06-19T22:10:48+09:00",
+          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
+          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
+        },
+        "date": 1687181431764,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node-memory-usage-through-safe-benchmark",
+            "value": 11.54,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "8260565a50e20365d0718492282f4196299724a7",
+          "message": "feat(CI): check client average memory usage during upload large file",
+          "timestamp": "2023-06-20T09:11:32+09:00",
+          "tree_id": "ef5d96d98b1e3cf36e327c5d44f1e066e974746a",
+          "url": "https://github.com/maidsafe/safe_network/commit/8260565a50e20365d0718492282f4196299724a7"
+        },
+        "date": 1687220936505,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node-memory-usage-through-safe-benchmark",
+            "value": 11.24,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
           "id": "f356c09f241fee248d154e3573d84382a71e5f61",
           "message": "fix: missing quote",
           "timestamp": "2023-06-20T11:29:33+09:00",
@@ -641,6 +608,35 @@ window.BENCHMARK_DATA = {
       }
     ],
     "Client memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "d6223ee3453a70c26e74fbdb948d1fb6beb65fa4",
+          "message": "fix(CI): correct client mem usage in the graph",
+          "timestamp": "2023-06-19T22:10:48+09:00",
+          "tree_id": "d889ce0fd14dee415c3dd3ca279e96c67a5e9803",
+          "url": "https://github.com/maidsafe/safe_network/commit/d6223ee3453a70c26e74fbdb948d1fb6beb65fa4"
+        },
+        "date": 1687181434945,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "safe-cli-memory-usage",
+            "value": 166.31,
+            "unit": "MB"
+          }
+        ]
+      },
       {
         "commit": {
           "author": {
