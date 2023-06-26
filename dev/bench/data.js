7886912cde406d99ae2e17b334d9a3dbf0b86d02
github-action-benchmark <github@users.noreply.github.com>
2023-06-26 16:51:11 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 2c465f63b62c3b23d73dd1b70cbf45ffdda131af

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687771125021,
+  "lastUpdate": 1687798271469,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -1053,6 +1053,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files upload 10mb",
             "value": 6.514438627976119,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "2c465f63b62c3b23d73dd1b70cbf45ffdda131af",
+          "message": "fix: get_closest_local shall only return CLOSE_GROUP_SIZE peers",
+          "timestamp": "2023-06-26T15:48:51Z",
+          "tree_id": "db66777a517d3f5a3a27ea1b6861f2d635fb2386",
+          "url": "https://github.com/maidsafe/safe_network/commit/2c465f63b62c3b23d73dd1b70cbf45ffdda131af"
+        },
+        "date": 1687798269024,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.6601985742259732,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 6.425993559736622,
             "unit": "MiB/s"
           }
         ]
