abfdd57544cccfcdae2141d3eedca471596a0d6e
github-action-benchmark <github@users.noreply.github.com>
2023-06-27 18:17:55 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for f49082f6a0862436416fb1bdac6b5ff4847a5a60

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687874754167,
+  "lastUpdate": 1687889875497,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -1165,6 +1165,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 23.94907227349557,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "distinct": true,
+          "id": "f49082f6a0862436416fb1bdac6b5ff4847a5a60",
+          "message": "feat: append peer id to node's default root dir\n\nBREAKING CHANGE: The default root directory for the node will now have the node's peer ID appended\nto its path.\n\nUsing the peer ID facilitates running multiple node processes on a single machine, without the need\nto explicitly use the `--root-dir` argument for each node to specify its own root directory.\nPreviously, if they didn't do that, all the nodes would share the same record store.",
+          "timestamp": "2023-06-27T17:14:04Z",
+          "tree_id": "8fadc9f32127cb75d9c0de4cafd90b095c0fdc5f",
+          "url": "https://github.com/maidsafe/safe_network/commit/f49082f6a0862436416fb1bdac6b5ff4847a5a60"
+        },
+        "date": 1687889874472,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.6660606549365365,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 6.511569997852373,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 23.98908131557219,
             "unit": "MiB/s"
           }
         ]
