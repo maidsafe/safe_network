dadf048fc2bc7f9114c083efe8c7d8f77681381e
github-action-benchmark <github@users.noreply.github.com>
2023-06-28 12:09:50 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 43b062a0f43c765398f5d2ede773e4c52bea3280

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687954187786,
+  "lastUpdate": 1687954190509,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -3112,6 +3112,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ ~450mb upload",
             "value": 471,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "grumbach",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "43b062a0f43c765398f5d2ede773e4c52bea3280",
+          "message": "fix: rename UserRights to UserPermissions",
+          "timestamp": "2023-06-28T19:55:42+09:00",
+          "tree_id": "a4ca7508f40b62b2e0d55e443c9f9537d71b557f",
+          "url": "https://github.com/maidsafe/safe_network/commit/43b062a0f43c765398f5d2ede773e4c52bea3280"
+        },
+        "date": 1687954190080,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ ~450mb upload",
+            "value": 918.98,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ ~450mb upload",
+            "value": 438,
             "unit": "MB"
           }
         ]
