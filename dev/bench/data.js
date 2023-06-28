b3973bf17a29c48431104faee580e66174a30ffb
github-action-benchmark <github@users.noreply.github.com>
2023-06-28 05:05:01 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for dbf94c7ffc226bb900ee217875264dedf970bac9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1687919358317,
+  "lastUpdate": 1687928701409,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -1243,6 +1243,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 13.536414554205619,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "dbf94c7ffc226bb900ee217875264dedf970bac9",
+          "message": "ci: check local-discovery is not a default feature in any crate",
+          "timestamp": "2023-06-28T12:52:29+09:00",
+          "tree_id": "16302ca77f4653b7ef6a4d0f6dcb95f4adfa1144",
+          "url": "https://github.com/maidsafe/safe_network/commit/dbf94c7ffc226bb900ee217875264dedf970bac9"
+        },
+        "date": 1687928700217,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.06335063422959822,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 0.6374162750915088,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 19.080125302666755,
             "unit": "MiB/s"
           }
         ]
