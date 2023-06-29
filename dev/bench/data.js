5397d64f73d2c1a92b434b2168af831ba2ac5401
github-action-benchmark <github@users.noreply.github.com>
2023-06-29 02:11:48 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 0916891426253ce05266a6538e5fcc4f74a280e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688004545212,
+  "lastUpdate": 1688004708098,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -2503,6 +2503,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 17.43,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "0916891426253ce05266a6538e5fcc4f74a280e3",
+          "message": "fix(memcheck): add missing local-discovery feats",
+          "timestamp": "2023-06-29T10:00:38+09:00",
+          "tree_id": "6b8dbfa294fa29a65bb5b4d90a8c00af9e3cc19e",
+          "url": "https://github.com/maidsafe/safe_network/commit/0916891426253ce05266a6538e5fcc4f74a280e3"
+        },
+        "date": 1688004707160,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 19,
             "unit": "MB"
           }
         ]
