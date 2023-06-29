6f39a1457d329d93e9b8eb9b52f3793b1565947a
grumbach <anselmega@gmail.com>
2023-06-29 19:30:29 +0900

    refactor: demystify permissions

--- sn_registers/src/permissions.rs
+++ sn_registers/src/permissions.rs
@@ -6,37 +6,8 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use super::Action;
-
 use serde::{Deserialize, Serialize};
-use std::{collections::BTreeMap, hash::Hash};
-
-/// The permissions of an user
-#[derive(Copy, Clone, Serialize, Deserialize, PartialEq, PartialOrd, Ord, Eq, Hash, Debug)]
-pub struct UserPermissions {
-    /// `Some(true)` if the user can write.
-    /// `Some(false)` explicitly denies writes (even if `Anyone` has the right to write).
-    /// `None` use default: the permissions for `Anyone`
-    write: Option<bool>,
-}
-
-impl UserPermissions {
-    /// Constructs a new set of user permissions.
-    pub fn new(write: impl Into<Option<bool>>) -> Self {
-        Self {
-            write: write.into(),
-        }
-    }
-
-    /// Returns `Some(true)` if `action` is allowed and `Some(false)` if it's not permitted.
-    /// `None` means that default permissions should be applied.
-    pub fn is_allowed(self, action: Action) -> Option<bool> {
-        match action {
-            Action::Read => Some(true), // It's public data, so it's always allowed to read it.
-            Action::Write => self.write,
-        }
-    }
-}
+use std::{collections::BTreeSet, hash::Hash};
 
 /// User that can access a Register.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize, Debug)]
@@ -48,5 +19,44 @@ pub enum User {
 }
 
 /// Register permissions
-/// Map of users to their public permission set.
-pub type Permissions = BTreeMap<User, UserPermissions>;
+/// Everyone can read a Register, all data is public on safe network.
+/// The Default value is nobody can write.
+#[derive(Clone, Serialize, Deserialize, PartialEq, PartialOrd, Ord, Eq, Hash, Debug, Default)]
+pub struct Permissions {
+    /// Anyone can read a Register since all data is public
+    /// The owner of a register can always write to it
+    /// This is the list of users that the owner has allowed to write to this Register
+    pub writers: BTreeSet<User>,
+}
+
+impl Permissions {
+    /// Constructs a new set of permissions with a list of users allowed to write
+    /// Empty list means nobody (appart from the owner) can write
+    pub fn new_with(writers: impl IntoIterator<Item = User>) -> Self {
+        Self {
+            writers: writers.into_iter().collect(),
+        }
+    }
+
+    /// Constructs a new set of permissions where everyone can write
+    pub fn new_anyone_can_write() -> Self {
+        Self {
+            writers: vec![User::Anyone].into_iter().collect(),
+        }
+    }
+
+    /// Constructs a new set of permissions where only the owner can write
+    pub fn new_owner_only() -> Self {
+        Default::default()
+    }
+
+    /// Checks is everyone can write to this Register
+    pub fn everyone_can_write(&self) -> bool {
+        self.writers.contains(&User::Anyone)
+    }
+
+    /// Returns true if the given user can write to this Register
+    pub fn can_write(&self, user: &User) -> bool {
+        self.everyone_can_write() || self.writers.contains(user)
+    }
+}
