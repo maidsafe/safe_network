d4e88bfec5e40fdf8b8b596061dec6fd4081b407
qima <qi.ma@maidsafe.net>
2023-06-29 20:28:27 +0800

    chore: various tidy up

--- sn_peers_acquisition/src/lib.rs
+++ sn_peers_acquisition/src/lib.rs
@@ -21,6 +21,14 @@ pub struct PeersArgs {
     /// where `1.2.3.4` is the IP, `1200` is the port and the (optional) last part is the peer ID.
     ///
     /// This argument can be provided multiple times to connect to multiple peers.
+    ///
+    /// Note: `SAFE_PEERS` env_var only got picked up
+    ///       when `--peers` argument is undefined via the safe/safenode executable.
+    ///       OR, during the restart of a node, `SAFE_PEERS` contains new peers not presented
+    ///       within the original initial_peers passed via `--peers` argument
+    ///       BUT, the value of `SAFE_PEERS` env_var is at the time when safenode started,
+    ///       i.e. if `SAFE_PEERS` env_var got updated after a safenode got started,
+    ///            it will still be the old value got picked up during node restarting.
     #[clap(long = "peer", value_name = "multiaddr", env = SAFE_PEERS_ENV, value_delimiter = ',', value_parser = parse_peer_addr)]
     pub peers: Vec<Multiaddr>,
 }
