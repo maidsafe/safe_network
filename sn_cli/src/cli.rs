7efad07a4b628e62130a1177a894a07037c7480a
Chris O'Neil <chriso83@protonmail.com>
2023-06-29 20:54:57 +0100

    feat: provide `--log-output-dest` arg for `safe`
    The behaviour for this is mostly the same as `safenode`, except if the argument is not
    specified, no logging will take place (this does not affect println output).

--- sn_cli/src/cli.rs
+++ sn_cli/src/cli.rs
@@ -7,17 +7,48 @@
 // permissions and limitations relating to use of the SAFE Network Software.
 
 use clap::Parser;
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
+use std::path::PathBuf;
 use std::time::Duration;
 
 use crate::subcommands::SubCmd;
+use sn_logging::LogOutputDest;
 use sn_peers_acquisition::PeersArgs;
+
+pub fn parse_logs_output(val: &str) -> Result<LogOutputDest> {
+    match val {
+        "stdout" => Ok(LogOutputDest::Stdout),
+        "default" => {
+            let dir = dirs_next::data_dir()
+                .ok_or_else(|| eyre!("could not obtain root directory path".to_string()))?
+                .join("safe")
+                .join("client")
+                .join("logs");
+            Ok(LogOutputDest::Path(dir))
+        }
+        // The path should be a directory, but we can't use something like `is_dir` to check
+        // because the path doesn't need to exist. We can create it for the user.
+        value => Ok(LogOutputDest::Path(PathBuf::from(value))),
+    }
+}
 
 // Please do not remove the blank lines in these doc comments.
 // They are used for inserting line breaks when the help menu is rendered in the UI.
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct Opt {
+    /// Specify the logging output destination.
+    ///
+    /// Valid values are "stdout", "default", or a custom path.
+    ///
+    /// The default location is platform specific:
+    ///  - Linux: $HOME/.local/share/safe/client/logs
+    ///  - macOS: $HOME/Library/Application Support/safe/client/logs
+    ///  - Windows: C:\Users\<username>\AppData\Roaming\safe\client\logs
+    #[allow(rustdoc::invalid_html_tags)]
+    #[clap(long, value_parser = parse_logs_output, verbatim_doc_comment)]
+    pub log_output_dest: Option<LogOutputDest>,
+
     #[command(flatten)]
     pub(crate) peers: PeersArgs,
 
