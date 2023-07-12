1cbe399bb072e1fa633ce96d2bd997582be81274
RolandSherwin <RolandSherwin@protonmail.com>
2023-07-12 14:23:14 +0530

    chore(metrics): remove network stats - Currently the `NetworkMetrics` provides interface/system wide stats   and thus can dirtied easily. A process level mechanism has to be   implemented to have valid usable network stat.

--- sn_logging/src/metrics.rs
+++ sn_logging/src/metrics.rs
@@ -8,7 +8,7 @@
 
 use serde::Serialize;
 use std::time::Duration;
-use sysinfo::{self, CpuExt, NetworkExt, Pid, PidExt, ProcessExt, System, SystemExt};
+use sysinfo::{self, CpuExt, Pid, PidExt, ProcessExt, System, SystemExt};
 use tracing::{debug, error};
 
 const UPDATE_INTERVAL: Duration = Duration::from_secs(5);
@@ -28,25 +28,8 @@ struct Metrics {
     system_memory_used_mb: f32,
     // Percentage of RAM used
     system_memory_usage_percent: f32,
-    // Network metrics is None if the default network interface cannot be found
-    network: Option<NetworkMetrics>,
     // Process metrics is None if the Pid for the process is invalid
     process: Option<ProcessMetrics>,
-}
-
-#[derive(Debug, Serialize)]
-#[allow(dead_code)]
-struct NetworkMetrics {
-    // The default network interface
-    interface_name: String,
-    // Bytes received during UPDATE_INTERVAL
-    bytes_received: u64,
-    // Bytes transmitted during UPDATE_INTERVAL
-    bytes_transmitted: u64,
-    // The total MBytes received through the interface
-    total_mb_received: f32,
-    // The total MBytes transmitted through the interface
-    total_mb_transmitted: f32,
 }
 
 #[derive(Debug, Serialize)]
@@ -71,7 +54,6 @@ struct ProcessMetrics {
 pub async fn init_metrics(pid: u32) {
     let mut sys = System::new_all();
     let pid = Pid::from_u32(pid);
-    let default_interface = default_net::get_default_interface();
 
     loop {
         refresh_metrics(&mut sys, pid);
@@ -95,29 +77,6 @@ pub async fn init_metrics(pid: u32) {
             }
         };
 
-        let network = if let Ok(default_interface) = &default_interface {
-            if let Some((interface_name, network_stat)) = sys
-                .networks()
-                .into_iter()
-                .find(|&(interface, _)| interface == &default_interface.name)
-            {
-                let network = NetworkMetrics {
-                    interface_name: interface_name.clone(),
-                    bytes_received: network_stat.received(),
-                    bytes_transmitted: network_stat.transmitted(),
-                    total_mb_received: network_stat.total_received() as f32 / TO_MB,
-                    total_mb_transmitted: network_stat.total_transmitted() as f32 / TO_MB,
-                };
-                Some(network)
-            } else {
-                // Could not get stats for the default interface
-                None
-            }
-        } else {
-            // Could not obtain the default network interface");
-            None
-        };
-
         let cpu_stat = sys.global_cpu_info();
         let metrics = Metrics {
             physical_cpu_threads: sys.cpus().len(),
@@ -126,7 +85,6 @@ pub async fn init_metrics(pid: u32) {
             system_memory_used_mb: sys.used_memory() as f32 / TO_MB,
             system_memory_usage_percent: (sys.used_memory() as f32 / sys.total_memory() as f32)
                 * 100.0,
-            network,
             process,
         };
         match serde_json::to_string(&metrics) {
