e0439f525c875b636ab363e788219623c1bb8527
github-action-benchmark <github@users.noreply.github.com>
2023-07-17 15:08:10 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 434b73a3b25650e4104e589c3c956fb9b18580e9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689606460304,
+  "lastUpdate": 1689606490421,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -4951,6 +4951,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 34.17,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "434b73a3b25650e4104e589c3c956fb9b18580e9",
+          "message": "ci: remove publish checks as throwing false positives",
+          "timestamp": "2023-07-17T16:45:27+02:00",
+          "tree_id": "85f3e45b72d6bff81c2a7f077eefa5312698a365",
+          "url": "https://github.com/maidsafe/safe_network/commit/434b73a3b25650e4104e589c3c956fb9b18580e9"
+        },
+        "date": 1689606490005,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 21.85,
             "unit": "MB"
           }
         ]
