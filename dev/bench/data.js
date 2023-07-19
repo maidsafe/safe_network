b6a767ad35ab33ba1d4f0f5847c2dc2344bbffdb
github-action-benchmark <github@users.noreply.github.com>
2023-07-19 18:57:33 +0000

    add Client memory (customSmallerIsBetter) benchmark result for a5a728910a9c8acf24922c904a6f9c9e4f5f9916

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689793049557,
+  "lastUpdate": 1689793053649,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -7464,6 +7464,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 193,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "distinct": true,
+          "id": "a5a728910a9c8acf24922c904a6f9c9e4f5f9916",
+          "message": "feat(testnet): enable the use of `CARGO_TARGET_DIR`",
+          "timestamp": "2023-07-19T17:48:51Z",
+          "tree_id": "85d8c65dc060c56233d7da6d041d90fb744aeb71",
+          "url": "https://github.com/maidsafe/safe_network/commit/a5a728910a9c8acf24922c904a6f9c9e4f5f9916"
+        },
+        "date": 1689793052829,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 230.6,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 191,
             "unit": "MB"
           }
         ]
