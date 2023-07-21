758e4f5eb26a4eb241807d3eb30dabb87fc7631e
RolandSherwin <RolandSherwin@protonmail.com>
2023-07-21 19:54:28 +0530

    feat(log): add new log markers

--- sn_node/src/log_markers.rs
+++ sn_node/src/log_markers.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use libp2p::PeerId;
+use libp2p::{kad::RecordKey, PeerId};
 use sn_protocol::messages::{Cmd, CmdResponse};
 use std::time::Duration;
 // this gets us to_string easily enough
@@ -39,16 +39,19 @@ pub enum Marker<'a> {
     LostRecordDetected(&'a Vec<PeerId>),
 
     /// Replication trigger was fired
-    ReplicationTriggered(&'a Vec<PeerId>),
+    ReplicationTriggered,
+
+    /// The new members added to our close group
+    CloseGroupUpdated(&'a Vec<PeerId>),
 
     /// Keys of Records we are fetching to replicate locally
     FetchingKeysForReplication {
         /// fetching_keys_len: number of keys we are fetching
         fetching_keys_len: usize,
-        /// provided_keys_len: number of keys we were provided (the difference would be the number of keys we already have)
-        provided_keys_len: usize,
-        /// peer_id: the peer we are fetching from
-        peer_id: PeerId,
+        /// direct_fetch_count: number of keys we are directly fetching from a provided peer
+        direct_fetch_count: usize,
+        /// network_fetch_count: number of keys we are fetching from the network
+        network_fetch_count: usize,
     },
 }
 
@@ -58,5 +61,22 @@ impl<'a> Marker<'a> {
         // Down the line, if some logs are noisier than others, we can
         // match the type and log a different level.
         info!("{self:?}");
+    }
+
+    /// Helper to log the FetchingKeysForReplication variant
+    pub fn fetching_keys_for_replication(keys: &Vec<(RecordKey, Option<PeerId>)>) -> Self {
+        let direct_fetch_count = keys
+            .iter()
+            .filter(|(_, maybe_direct)| maybe_direct.is_some())
+            .count();
+        let network_fetch_count = keys
+            .iter()
+            .filter(|(_, maybe_direct)| maybe_direct.is_none())
+            .count();
+        Marker::FetchingKeysForReplication {
+            fetching_keys_len: keys.len(),
+            direct_fetch_count,
+            network_fetch_count,
+        }
     }
 }
