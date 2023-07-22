403a50235b5a29451037e88393bf6ec513cb5aa8
github-action-benchmark <github@users.noreply.github.com>
2023-07-22 06:52:36 +0000

    add Node memory (customSmallerIsBetter) benchmark result for beb15f9ba34f85e93d97815a52d9be110bf43113

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690008718530,
+  "lastUpdate": 1690008756424,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -5767,6 +5767,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 35.94,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "beb15f9ba34f85e93d97815a52d9be110bf43113",
+          "message": "fix(testnet): clean the whole safe dir\n\nAlso more test wallet creation in to that same safedir",
+          "timestamp": "2023-07-22T05:53:06Z",
+          "tree_id": "5feac39ecfce972533099201d761b380e7bf44c6",
+          "url": "https://github.com/maidsafe/safe_network/commit/beb15f9ba34f85e93d97815a52d9be110bf43113"
+        },
+        "date": 1690008756062,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 28.11,
             "unit": "MB"
           }
         ]
