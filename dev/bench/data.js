aaa2254b31b862111881c874ce9f1eb053c523ec
github-action-benchmark <github@users.noreply.github.com>
2023-07-25 19:38:14 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for cc7cca03bf7c2a7405db91aaf7c35a540958561d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690297441520,
+  "lastUpdate": 1690313894584,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -3583,6 +3583,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 24.910010596410622,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "cc7cca03bf7c2a7405db91aaf7c35a540958561d",
+          "message": "fix(replication): send out keys for replication if not empty",
+          "timestamp": "2023-07-25T18:36:10Z",
+          "tree_id": "86efdec182faa4c1cabdfb42c8549d3309c24abd",
+          "url": "https://github.com/maidsafe/safe_network/commit/cc7cca03bf7c2a7405db91aaf7c35a540958561d"
+        },
+        "date": 1690313893408,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.8188391950811127,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 0.9405861176276823,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 25.5743061030106,
             "unit": "MiB/s"
           }
         ]
