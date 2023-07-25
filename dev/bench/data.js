72db3b46dc5084de353471ac6ae2eae689ef1602
github-action-benchmark <github@users.noreply.github.com>
2023-07-25 08:04:54 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 57227080465794a41451f1cb81c4139a9647cff6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690272290888,
+  "lastUpdate": 1690272294736,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -8382,6 +8382,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 240,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gabrielviganotti@gmail.com",
+            "name": "bochaco",
+            "username": "bochaco"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "57227080465794a41451f1cb81c4139a9647cff6",
+          "message": "feat(node): fetch all spends for a payment proof concurrently to verify them",
+          "timestamp": "2023-07-25T06:47:50Z",
+          "tree_id": "870daca36465dbf7626199ecc3861ff4dde873e4",
+          "url": "https://github.com/maidsafe/safe_network/commit/57227080465794a41451f1cb81c4139a9647cff6"
+        },
+        "date": 1690272293719,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 235.05,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 242,
             "unit": "MB"
           }
         ]
