1abec15ee35509ad36ce9921cbc60584f5363eac
github-action-benchmark <github@users.noreply.github.com>
2023-07-28 15:10:45 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 1595c9b5061caae7ff0ddd7ec807108f12ad5064

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690556905475,
+  "lastUpdate": 1690557045010,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -6787,6 +6787,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 29.39,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "1595c9b5061caae7ff0ddd7ec807108f12ad5064",
+          "message": "chore(readme): include faucet arg while launching testnet",
+          "timestamp": "2023-07-28T09:57:20Z",
+          "tree_id": "466d9c032927ab67b45431ea4e45d58a73e069bd",
+          "url": "https://github.com/maidsafe/safe_network/commit/1595c9b5061caae7ff0ddd7ec807108f12ad5064"
+        },
+        "date": 1690557044676,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 30,
             "unit": "MB"
           }
         ]
