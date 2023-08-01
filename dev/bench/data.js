251dd11b39f4c242ee171b6f7c406d45c2d31313
github-action-benchmark <github@users.noreply.github.com>
2023-08-01 20:51:11 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 121cca741af89aede6a64db66aaa5a952b8dcf31

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690922961129,
+  "lastUpdate": 1690923071199,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -7399,6 +7399,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 27.92,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "121cca741af89aede6a64db66aaa5a952b8dcf31",
+          "message": "chore(networking): improve data pruning\n\nwarn if booting relevant data, clarify error docs",
+          "timestamp": "2023-08-01T17:19:23+02:00",
+          "tree_id": "4d68c66e2e014bec2df78738dd3ec70b30c9356a",
+          "url": "https://github.com/maidsafe/safe_network/commit/121cca741af89aede6a64db66aaa5a952b8dcf31"
+        },
+        "date": 1690923070783,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 24.14,
             "unit": "MB"
           }
         ]
