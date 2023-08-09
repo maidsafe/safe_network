d1ecf0d268d4c3160738b4dbd12f881e99e54f0b
qima <qi.ma@maidsafe.net>
2023-08-09 19:24:08 +0800

    fix: distance_range caculated from proper last entry of close_group

--- sn_node/src/replication.rs
+++ sn_node/src/replication.rs
@@ -105,8 +105,9 @@ impl Node {
         }
 
         // set the distance range used for pruning
+        // our_close_group only has CLOSE_GROUP_SIZE entries, so we just grab the last one
         let our_close_group = self.network.get_our_close_group().await?;
-        let distance_range = match our_close_group.get(CLOSE_GROUP_SIZE) {
+        let distance_range = match our_close_group.last() {
             Some(peer) => NetworkAddress::from_peer(*peer).distance(&our_address),
             None => {
                 warn!("Could not obtain distance_range");
