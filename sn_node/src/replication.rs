ad3e19768350e63e4a4d2cbf111f52ff4891a7de
qima <qi.ma@maidsafe.net>
2023-08-10 21:03:51 +0800

    chore: optimize replication algorithm

--- sn_node/src/replication.rs
+++ sn_node/src/replication.rs
@@ -8,8 +8,10 @@
 
 use crate::Node;
 use crate::{error::Result, log_markers::Marker};
-use libp2p::kad::RecordKey;
-use libp2p::PeerId;
+use libp2p::{
+    kad::{RecordKey, K_VALUE},
+    PeerId,
+};
 use sn_networking::{sort_peers_by_address, CLOSE_GROUP_SIZE};
 use sn_protocol::{
     messages::{Cmd, Query, Request},
@@ -22,52 +24,72 @@ use tokio::task::JoinHandle;
 const MAX_REPLICATION_KEYS_PER_REQUEST: usize = 500;
 
 impl Node {
-    /// Replication is triggered when is there is a change in our close group
-    pub(crate) async fn try_trigger_replication(&self) -> Result<()> {
+    /// When there is PeerAdded or PeerRemoved, trigger replication, and replication target to be:
+    /// 1, For PeerAdded(X), replicate any record that is now having X in its close_group
+    ///    (from our knowledge) to that X
+    /// 2, For PeerRemoved(X), replicate any record that previously having X in its close_group,
+    ///    to that record's new close_group's farthest peer
+    pub(crate) async fn try_trigger_replication(
+        &self,
+        peer_id: PeerId,
+        is_removal: bool,
+    ) -> Result<()> {
+        // Already contains self_peer_id
+        let mut all_peers = self.network.get_all_local_peers().await?;
+
+        // Do not carry out replication if not many peers present.
+        if all_peers.len() < K_VALUE.into() {
+            trace!(
+                "Not having enough peers to start replication: {:?}/{K_VALUE:?}",
+                all_peers.len()
+            );
+            return Ok(());
+        }
+
         Marker::ReplicationTriggered.log();
-        let our_close_group = self.network.get_our_close_group().await?;
         let our_peer_id = self.network.peer_id;
         let our_address = NetworkAddress::from_peer(our_peer_id);
 
-        let all_peers = self.network.get_all_local_peers().await?;
         let all_records = self.network.get_all_local_record_addresses().await?;
-        trace!("Replication triggred, all records: {all_records:?}");
+        trace!("Replication triggred, all records: {:?}", all_records.len());
 
-        // a key is sent to a peer if the peer is considered to be close to that key,
-        // and that peer is close enough to us as well.
         let mut replicate_to: BTreeMap<PeerId, Vec<NetworkAddress>> = Default::default();
-        let furthest_close_distance = if let Some(furthest_peer) = our_close_group.last() {
-            if let Some(dist_ilog2) = NetworkAddress::from_peer(*furthest_peer)
-                .distance(&our_address)
-                .ilog2()
-            {
-                dist_ilog2
-            } else {
-                warn!("Cannot calculate ilog2 for the distance");
-                return Ok(());
-            }
-        } else {
-            warn!("our_close_group doesn't have last entry");
-            return Ok(());
-        };
+
+        if is_removal {
+            all_peers.push(peer_id);
+        }
+
         for key in all_records {
             let sorted_based_on_key =
-                sort_peers_by_address(all_peers.clone(), &key, CLOSE_GROUP_SIZE)?;
+                sort_peers_by_address(all_peers.clone(), &key, CLOSE_GROUP_SIZE + 1)?;
             trace!("replication: close for {key:?} are: {sorted_based_on_key:?}");
 
-            for peer in sorted_based_on_key.iter().filter(|&p| p != &our_peer_id) {
-                if let Some(peer_dist_ilog2) = NetworkAddress::from_peer(*peer)
-                    .distance(&our_address)
-                    .ilog2()
-                {
-                    if peer_dist_ilog2 <= furthest_close_distance {
-                        let keys_to_replicate =
-                            replicate_to.entry(*peer).or_insert(Default::default());
-                        keys_to_replicate.push(key.clone());
+            if sorted_based_on_key.contains(&peer_id) {
+                let target_peer = if is_removal {
+                    // For dead peer, only replicate to farthest close_group peer,
+                    // when the dead peer was one of the close_group peers to the record.
+                    if let Some(farthest_peer) = sorted_based_on_key.last() {
+                        if *farthest_peer != peer_id {
+                            *farthest_peer
+                        } else {
+                            continue;
+                        }
+                    } else {
+                        continue;
                     }
                 } else {
-                    warn!("Cannot calculate ilog2 for the peer distance");
-                }
+                    // For new peer, always replicate to it when it is close_group of the record.
+                    if Some(&peer_id) != sorted_based_on_key.last() {
+                        peer_id
+                    } else {
+                        continue;
+                    }
+                };
+
+                let keys_to_replicate = replicate_to
+                    .entry(target_peer)
+                    .or_insert(Default::default());
+                keys_to_replicate.push(key.clone());
             }
         }
         trace!("replication list {replicate_to:?}");
@@ -83,6 +105,7 @@ impl Node {
         }
 
         // set the distance range used for pruning
+        let our_close_group = self.network.get_our_close_group().await?;
         let distance_range = match our_close_group.get(CLOSE_GROUP_SIZE) {
             Some(peer) => NetworkAddress::from_peer(*peer).distance(&our_address),
             None => {
