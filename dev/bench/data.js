70611fd8f6802032018e57a73057dc0e8cf47ad9
github-action-benchmark <github@users.noreply.github.com>
2023-08-24 17:24:15 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 7b863cd9e11ecaa51e6ec042f077b17bdbeb3f1e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692897851988,
+  "lastUpdate": 1692897855155,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -12864,6 +12864,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 83,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "7b863cd9e11ecaa51e6ec042f077b17bdbeb3f1e",
+          "message": "chore: rust 1.72.0 fixes",
+          "timestamp": "2023-08-24T16:14:55Z",
+          "tree_id": "c428a2e1a3c80db3915b47721af3d1a2ed8e1eb6",
+          "url": "https://github.com/maidsafe/safe_network/commit/7b863cd9e11ecaa51e6ec042f077b17bdbeb3f1e"
+        },
+        "date": 1692897854420,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 86.528,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 73,
             "unit": "MB"
           }
         ]
