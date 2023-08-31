07ae8b41e74377e35c2854f92a06b955c8cc0eb5
github-action-benchmark <github@users.noreply.github.com>
2023-08-31 10:53:20 +0000

    add Client memory (customSmallerIsBetter) benchmark result for b1c88b7746efabd86d7c3b09b3244a533b733abd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693479197833,
+  "lastUpdate": 1693479200117,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -13578,6 +13578,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 77,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "b1c88b7746efabd86d7c3b09b3244a533b733abd",
+          "message": "feat(cli): expose 'concurrency' flag",
+          "timestamp": "2023-08-31T08:49:53Z",
+          "tree_id": "e32c3a11ea51effa94a974584e26e34d2dd5686e",
+          "url": "https://github.com/maidsafe/safe_network/commit/b1c88b7746efabd86d7c3b09b3244a533b733abd"
+        },
+        "date": 1693479199286,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 1359.7778,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 1293,
             "unit": "MB"
           }
         ]
