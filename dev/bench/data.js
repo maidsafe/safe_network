5ca0d2c59ae65683003e404b055f6f66c4c96b9b
github-action-benchmark <github@users.noreply.github.com>
2023-09-20 19:56:43 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 2878e324e987d22ac33f7b5823aa0c4dfcd2531b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695239533431,
+  "lastUpdate": 1695239802972,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -14601,6 +14601,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 60.0064,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "2878e324e987d22ac33f7b5823aa0c4dfcd2531b",
+          "message": "chore: more iterations for verify reward update",
+          "timestamp": "2023-09-20T18:50:38Z",
+          "tree_id": "6966d4ab8e565e1e6d3ed1688ea8bf0eb7f2e91e",
+          "url": "https://github.com/maidsafe/safe_network/commit/2878e324e987d22ac33f7b5823aa0c4dfcd2531b"
+        },
+        "date": 1695239802618,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 57.13101,
             "unit": "MB"
           }
         ]
