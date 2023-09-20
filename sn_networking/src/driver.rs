9a534bce0fb06e953484bd87a8093a006d21eae6
grumbach <anselmega@gmail.com>
2023-09-20 15:58:01 +0900

    feat: rename utxo by CashNoteRedemption

--- sn_networking/src/driver.rs
+++ sn_networking/src/driver.rs
@@ -60,7 +60,7 @@ type PendingGetRecord = HashMap<QueryId, (oneshot::Sender<Result<Record>>, GetRe
 
 /// What is the largest packet to send over the network.
 /// Records larger than this will be rejected.
-// TODO: revisit once utxo is in
+// TODO: revisit once cashnote_redemption is in
 const MAX_PACKET_SIZE: usize = 1024 * 1024 * 5; // the chunk size is 1mb, so should be higher than that to prevent failures, 5mb here to allow for CashNote storage
 
 // Timeout for requests sent/received through the request_response behaviour.
