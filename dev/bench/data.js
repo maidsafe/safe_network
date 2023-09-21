fb39ce0a02118055b4cc2a36f764c78521428c7a
github-action-benchmark <github@users.noreply.github.com>
2023-09-21 07:41:32 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 3c1432cf780fa909aa76e4c822207a5cfbe2c180

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695282090008,
+  "lastUpdate": 1695282092847,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20752,6 +20752,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 158,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "3c1432cf780fa909aa76e4c822207a5cfbe2c180",
+          "message": "chore: store uploaded files list as text\n\nFor the uploaded files list, rather than serialize to binary, we use a simple text format. The user\ncan now see the addresses of all the files they've uploaded, and use this information to retrieve\nthem, if need be. We are also now just using one file, appending to it for each upload.\n\nNow that this file is human readable, the `files ls` command was deemed unnecessary and was\ntherefore removed.\n\nWith respect to the `files` commands, I've also taken the opportunity to make the language in the\ntext output more concise and matter of fact. For example, we don't really need to use things like\nexclamation marks in the text.",
+          "timestamp": "2023-09-21T06:25:42Z",
+          "tree_id": "ed96928a4b8b6c310136178e09363e69521999c2",
+          "url": "https://github.com/maidsafe/safe_network/commit/3c1432cf780fa909aa76e4c822207a5cfbe2c180"
+        },
+        "date": 1695282091968,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 243.47443,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 173,
             "unit": "MB"
           }
         ]
