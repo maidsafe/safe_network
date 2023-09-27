2518c08a28f0023e4ab7c945b019b4d431139407
github-action-benchmark <github@users.noreply.github.com>
2023-09-27 11:00:35 +0000

    add Node memory (customSmallerIsBetter) benchmark result for e1f251af977f4e464da7f26f080b73baed73f6de

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695812212734,
+  "lastUpdate": 1695812435399,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -15553,6 +15553,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 50.7904,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "grumbach",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "anselmega@gmail.com",
+            "name": "Anselme",
+            "username": "grumbach"
+          },
+          "distinct": false,
+          "id": "e1f251af977f4e464da7f26f080b73baed73f6de",
+          "message": "fix: benches",
+          "timestamp": "2023-09-27T09:59:28Z",
+          "tree_id": "b4628acc5023d9b2ebfc7a1853dca1975ac10a47",
+          "url": "https://github.com/maidsafe/safe_network/commit/e1f251af977f4e464da7f26f080b73baed73f6de"
+        },
+        "date": 1695812434948,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 51.744766,
             "unit": "MB"
           }
         ]
