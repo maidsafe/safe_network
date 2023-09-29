ea867d24d8561e28974bfbf850a99b70a0ead69b
github-action-benchmark <github@users.noreply.github.com>
2023-09-29 08:02:19 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 54c15d458d350a45da1d408930f175c457e4d3b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695888830081,
+  "lastUpdate": 1695974539460,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -9472,6 +9472,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 10.731184830103079,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "gabrielviganotti@gmail.com",
+            "name": "bochaco",
+            "username": "bochaco"
+          },
+          "distinct": true,
+          "id": "54c15d458d350a45da1d408930f175c457e4d3b5",
+          "message": "fix(client): just skip empty files",
+          "timestamp": "2023-09-29T07:06:03Z",
+          "tree_id": "d230814f921a5bca510fcf8a04cb44bab295e7c2",
+          "url": "https://github.com/maidsafe/safe_network/commit/54c15d458d350a45da1d408930f175c457e4d3b5"
+        },
+        "date": 1695974538132,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.11629545179860606,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 0.5882993159429702,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 11.04449005587532,
             "unit": "MiB/s"
           }
         ]
