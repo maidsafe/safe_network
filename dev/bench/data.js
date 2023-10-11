8587ec7fb9124daf9a24d83febaf907311e84b1d
github-action-benchmark <github@users.noreply.github.com>
2023-10-11 07:59:51 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 76dcdb889cad0a0f9547c54aa411e46159951705

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697010964162,
+  "lastUpdate": 1697011191361,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -18409,6 +18409,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 49,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "76dcdb889cad0a0f9547c54aa411e46159951705",
+          "message": "fix: handling GetClosestPeers query error branch",
+          "timestamp": "2023-10-11T09:33:41+02:00",
+          "tree_id": "a0233b099889d0a53baa4e1423f2ba6f1e3f3322",
+          "url": "https://github.com/maidsafe/safe_network/commit/76dcdb889cad0a0f9547c54aa411e46159951705"
+        },
+        "date": 1697011190453,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 50,
             "unit": "MB"
           }
         ]
