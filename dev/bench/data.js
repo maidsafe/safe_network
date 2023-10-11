47f9ea56f995115fed9421dd87885d739b77ecb3
github-action-benchmark <github@users.noreply.github.com>
2023-10-11 07:14:59 +0000

    add Client memory (customSmallerIsBetter) benchmark result for f5cfd0eb33b2cc6c95ec186954050c980cb270c2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697008498407,
+  "lastUpdate": 1697008499868,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -25954,6 +25954,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 38,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "f5cfd0eb33b2cc6c95ec186954050c980cb270c2",
+          "message": "feat: showing expected holders to CLI when required",
+          "timestamp": "2023-10-11T08:51:36+02:00",
+          "tree_id": "642f4b2c83a1287089d89482a8933d9dd5ab32cc",
+          "url": "https://github.com/maidsafe/safe_network/commit/f5cfd0eb33b2cc6c95ec186954050c980cb270c2"
+        },
+        "date": 1697008499487,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 165,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 37,
             "unit": "MB"
           }
         ]
