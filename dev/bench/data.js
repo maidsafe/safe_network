a2db1fafff9372ba2e5149ff3f04e6aef442c153
github-action-benchmark <github@users.noreply.github.com>
2023-10-16 09:56:07 +0000

    add Node memory (customSmallerIsBetter) benchmark result for aacb62f0ecb6c23ae3341a1589109cf430555784

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697449852973,
+  "lastUpdate": 1697450167186,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -19157,6 +19157,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 47,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "aacb62f0ecb6c23ae3341a1589109cf430555784",
+          "message": "fix(network): perfrom bootstrapping continuously to make it well connected",
+          "timestamp": "2023-10-16T08:50:45Z",
+          "tree_id": "0369b4eb1c4ed8b4cac383b3fa709c4d37c47b08",
+          "url": "https://github.com/maidsafe/safe_network/commit/aacb62f0ecb6c23ae3341a1589109cf430555784"
+        },
+        "date": 1697450166215,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 50,
             "unit": "MB"
           }
         ]
