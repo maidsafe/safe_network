87bd7fd0435242731f06379331bfec379714e943
Roland Sherwin <RolandSherwin@protonmail.com>
2023-10-17 14:44:35 +0530

    feat(client): stop futher bootstrapping if the client has K_VALUE peers

--- sn_networking/src/bootstrap.rs
+++ sn_networking/src/bootstrap.rs
@@ -36,16 +36,7 @@ impl SwarmDriver {
             .should_we_bootstrap(peers_in_rt, current_bootstrap_interval)
             .await;
         if should_bootstrap {
-            debug!("Trying to initiate bootstrap. Current bootstrap_interval {current_bootstrap_interval:?}");
-            match self.swarm.behaviour_mut().kademlia.bootstrap() {
-                Ok(query_id) => {
-                    debug!("Initiated kad bootstrap process with query id {query_id:?}");
-                    self.bootstrap.initiated();
-                }
-                Err(err) => {
-                    error!("Failed to initiate kad bootstrap with error: {err:?}");
-                }
-            };
+            self.initiate_bootstrap();
         }
         if let Some(new_interval) = &new_interval {
             debug!(
@@ -55,11 +46,26 @@ impl SwarmDriver {
         }
         new_interval
     }
+
+    /// Helper to initiate the Kademlia bootstrap process.
+    pub(crate) fn initiate_bootstrap(&mut self) {
+        match self.swarm.behaviour_mut().kademlia.bootstrap() {
+            Ok(query_id) => {
+                debug!("Initiated kad bootstrap process with query id {query_id:?}");
+                self.bootstrap.initiated();
+            }
+            Err(err) => {
+                error!("Failed to initiate kad bootstrap with error: {err:?}");
+            }
+        };
+    }
 }
 
 /// Tracks and helps with the continuous kad::bootstrapping process
 pub(crate) struct ContinuousBootstrap {
     is_ongoing: bool,
+    initial_bootstrap_done: bool,
+    stop_bootstrapping: bool,
     last_peer_added_instant: Instant,
 }
 
@@ -67,7 +73,9 @@ impl ContinuousBootstrap {
     pub(crate) fn new() -> Self {
         Self {
             is_ongoing: false,
+            initial_bootstrap_done: false,
             last_peer_added_instant: Instant::now(),
+            stop_bootstrapping: false,
         }
     }
 
@@ -77,13 +85,27 @@ impl ContinuousBootstrap {
     }
 
     /// Notify about a newly added peer to the RT. This will help with slowing down the bootstrap process.
-    pub(crate) fn notify_new_peer(&mut self) {
+    /// Returns `true` if we have to perform the initial bootstrapping.
+    pub(crate) fn notify_new_peer(&mut self) -> bool {
         self.last_peer_added_instant = Instant::now();
+        // true to kick off the initial bootstrapping. `run_bootstrap_continuously` might kick of so soon that we might
+        // not have a single peer in the RT and we'd not perform any bootstrapping for a while.
+        if !self.initial_bootstrap_done {
+            self.initial_bootstrap_done = true;
+            true
+        } else {
+            false
+        }
     }
 
     /// A previous Kademlia Bootstrap process has been completed. Now a new bootstrap process can start.
     pub(crate) fn completed(&mut self) {
         self.is_ongoing = false;
+    }
+
+    /// Set the flag to stop any further re-bootstrapping.
+    pub(crate) fn stop_bootstrapping(&mut self) {
+        self.stop_bootstrapping = true;
     }
 
     /// Returns `true` if we should carry out the Kademlia Bootstrap process immediately.
@@ -93,6 +115,14 @@ impl ContinuousBootstrap {
         peers_in_rt: u32,
         current_interval: Duration,
     ) -> (bool, Option<Interval>) {
+        // stop bootstrapping if flag is set
+        if self.stop_bootstrapping {
+            info!("stop_bootstrapping flag has been set to true. Disabling further bootstrapping");
+            let mut new_interval = tokio::time::interval(Duration::from_secs(86400));
+            new_interval.tick().await; // the first tick completes immediately
+            return (false, Some(new_interval));
+        }
+
         // kad bootstrap process needs at least one peer in the RT be carried out.
         let should_bootstrap = !self.is_ongoing && peers_in_rt >= 1;
 
@@ -101,8 +131,7 @@ impl ContinuousBootstrap {
         // Don't slow down if we haven't even added one peer to our RT.
         if self.last_peer_added_instant.elapsed() > LAST_PEER_ADDED_TIME_LIMIT && peers_in_rt != 0 {
             info!(
-                "It has been {LAST_PEER_ADDED_TIME_LIMIT:?} since we last added a peer to RT.
-                Slowing down the continuous bootstrapping process"
+                "It has been {LAST_PEER_ADDED_TIME_LIMIT:?} since we last added a peer to RT. Slowing down the continuous bootstrapping process"
             );
 
             let mut new_interval = tokio::time::interval(NO_PEER_ADDED_SLOWDOWN_INTERVAL);
