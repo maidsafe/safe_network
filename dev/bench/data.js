9b09fa4deda9204fd7f49ec8c6d80f0b174d3e89
github-action-benchmark <github@users.noreply.github.com>
2023-10-22 13:00:38 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 96b944d5e7a576b3c7740af39a1d18aa4df1398d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,42 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697898790693,
+  "lastUpdate": 1697979638950,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "bf5808d3a159a147a3819a1db6e99fb77455f968",
-          "message": "feat: use standarised directories for files/wallet commands\n\nBREAKING CHANGE: use `~/.local/share/safe/wallet` as the root directory for wallet storage and\n`~/.local/share/safe/client` for files related to downloads/uploads.\n\nThe previous directories were `~/.safe/wallet` and `~/.safe/client`, which we can phase out in\npreference of using directories in the XDG standard.\n\nUsage of a `RECEIVED_DBCS_PATH` environment variable was added for users who may feel the default\ndirectory a bit unwieldy for working with the `wallet deposit` command, especially on Windows, where\nthe `AppData` directory is marked as hidden and so will only be shown by Windows Explorer if users\nhave viewing hidden files enabled.\n\nAlso provided and/or clarified documentation for other commands.",
-          "timestamp": "2023-06-22T09:19:11+09:00",
-          "tree_id": "625485846f58bc29312ebcd36f868e0fb401fee7",
-          "url": "https://github.com/maidsafe/safe_network/commit/bf5808d3a159a147a3819a1db6e99fb77455f968"
-        },
-        "date": 1687394035154,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.8332028930492699,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 6.57560832646883,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11634,6 +11600,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 19.645199041932283,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "96b944d5e7a576b3c7740af39a1d18aa4df1398d",
+          "message": "chore(node): increase replication frequency",
+          "timestamp": "2023-10-22T14:29:41+02:00",
+          "tree_id": "c44b68130100a65cd50b363fdfcb32f56fbba752",
+          "url": "https://github.com/maidsafe/safe_network/commit/96b944d5e7a576b3c7740af39a1d18aa4df1398d"
+        },
+        "date": 1697979636873,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 0.0905036987051225,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 0.5497956937854701,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 19.5595460909879,
             "unit": "MiB/s"
           }
         ]
