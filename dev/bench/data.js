188a5571c5eaf211176818e95c7a5cbbda11f1b2
github-action-benchmark <github@users.noreply.github.com>
2023-11-01 16:41:20 +0000

    add Node memory (customSmallerIsBetter) benchmark result for d17034e7186a2ccecbd8dcab6e8ad6b9f229a701

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698856513066,
+  "lastUpdate": 1698856880699,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "21dd4dea96c458a6c48edbcb1727bab5bee9524f",
-          "message": "feat(node): expose log markers in public api\n\nStart using them for some more crucial log messages, which we'll treat\nas an API and have to version accordingly.",
-          "timestamp": "2023-06-22T15:47:56+09:00",
-          "tree_id": "7140bc560623b9cf1818913bf16bbf574824ae84",
-          "url": "https://github.com/maidsafe/safe_network/commit/21dd4dea96c458a6c48edbcb1727bab5bee9524f"
-        },
-        "date": 1687417371957,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 21.65,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 57,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": false,
+          "id": "d17034e7186a2ccecbd8dcab6e8ad6b9f229a701",
+          "message": "chore: log detailed intermediate errors",
+          "timestamp": "2023-11-01T14:59:03Z",
+          "tree_id": "58b1438adce3c6df1daaf4a787d0ecd03787c187",
+          "url": "https://github.com/maidsafe/safe_network/commit/d17034e7186a2ccecbd8dcab6e8ad6b9f229a701"
+        },
+        "date": 1698856879869,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 58,
             "unit": "MB"
           }
         ]
