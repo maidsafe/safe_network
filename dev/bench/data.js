e996065daf19f059a4f20eb6ea2034b851acbf97
github-action-benchmark <github@users.noreply.github.com>
2023-11-07 11:19:09 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 72b3f1ed382fd896a8b8550b21c0057e89b4df83

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699355947706,
+  "lastUpdate": 1699355949488,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": false,
-          "id": "9fd5908d809199826b40cbf6e776c899ff27bb3a",
-          "message": "feat: provide option for log output in json\n\nSome users who had been interacting with the InstallNet testnet requested output in this form. It\ndoesn't give us any more elaborate data, like say what comes from an Open Telemetry trace, but might\nfunction as a starting point.",
-          "timestamp": "2023-06-21T01:03:54Z",
-          "tree_id": "4a305c262b77e2cd29286cea444566e4e230d936",
-          "url": "https://github.com/maidsafe/safe_network/commit/9fd5908d809199826b40cbf6e776c899ff27bb3a"
-        },
-        "date": 1687313026993,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 931.73,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 471,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "RolandSherwin",
             "username": "RolandSherwin"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 37,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "72b3f1ed382fd896a8b8550b21c0057e89b4df83",
+          "message": "chore: rename test function and spell correction",
+          "timestamp": "2023-11-07T10:23:59Z",
+          "tree_id": "efc9af79abae81a77694b33acf456128949c887f",
+          "url": "https://github.com/maidsafe/safe_network/commit/72b3f1ed382fd896a8b8550b21c0057e89b4df83"
+        },
+        "date": 1699355948865,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 178,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 34,
             "unit": "MB"
           }
         ]
