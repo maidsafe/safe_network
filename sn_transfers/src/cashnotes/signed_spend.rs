507b80a27b511f771b54dc615f0d89d0d0e44fef
Josh Wilson <joshuef@gmail.com>
2023-11-19 14:55:17 +0100

    feat(networking): shortcircuit response sending for replication

--- sn_transfers/src/cashnotes/signed_spend.rs
+++ sn_transfers/src/cashnotes/signed_spend.rs
@@ -104,7 +104,7 @@ impl std::hash::Hash for SignedSpend {
 }
 
 /// Represents the data to be signed by the DerivedSecretKey of the CashNote being spent.
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(custom_debug::Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Spend {
     /// UniquePubkey of input CashNote that this SignedSpend is proving to be spent.
     pub unique_pubkey: UniquePubkey,
