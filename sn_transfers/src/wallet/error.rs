c08a24266d8163eb76390e9bd90e94ba910fc76b
bochaco <gabrielviganotti@gmail.com>
2023-11-27 17:07:23 -0300

    feat(transfers): serialise wallets and transfers data with MsgPack instead of bincode

--- sn_transfers/src/wallet/error.rs
+++ sn_transfers/src/wallet/error.rs
@@ -64,9 +64,12 @@ pub enum Error {
     /// Bls error
     #[error("Bls error: {0}")]
     Bls(#[from] bls::error::Error),
-    /// Bincode error
-    #[error("Bincode error:: {0}")]
-    Bincode(#[from] bincode::Error),
+    /// MsgPack serialisation error
+    #[error("MsgPack serialisation error:: {0}")]
+    Serialisation(#[from] rmp_serde::encode::Error),
+    /// MsgPack deserialisation error
+    #[error("MsgPack deserialisation error:: {0}")]
+    Deserialisation(#[from] rmp_serde::decode::Error),
     /// I/O error
     #[error("I/O error: {0}")]
     Io(#[from] std::io::Error),
