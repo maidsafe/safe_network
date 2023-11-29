0f470d8d35beab5cf32fff60c0bdf50b56654b4d
Chris O'Neil <chriso83@protonmail.com>
2023-11-29 11:23:49 +0000

    ci: release process and licensing
    Provide version bumping and release workflows. These are copied from the `safeup` repository; they
    should serve fine for what we need here.
    
    Also prepare the crate for publishing by adding a README and a license. The license verification
    tool has also been enabled for CI. All source files have had copyright statement applied.

--- sn-node-manager/src/service.rs
+++ sn-node-manager/src/service.rs
@@ -1,3 +1,11 @@
+// Copyright (C) 2023 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
 use crate::config::create_owned_dir;
 use color_eyre::Result;
 #[cfg(test)]
