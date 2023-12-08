31808116ba7fa72c54b2017fc70902dc2c7f81a0
github-action-benchmark <github@users.noreply.github.com>
2023-12-08 08:39:58 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 5990e3eb3bf29a48f2d026937c045732e223db57

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702024796833,
+  "lastUpdate": 1702024798642,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -32312,6 +32312,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 65975,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 15,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "5990e3eb3bf29a48f2d026937c045732e223db57",
+          "message": "chore(deps): bump wagoid/commitlint-github-action from 1.6.0 to 5.4.4\n\nBumps [wagoid/commitlint-github-action](https://github.com/wagoid/commitlint-github-action) from 1.6.0 to 5.4.4.\n- [Changelog](https://github.com/wagoid/commitlint-github-action/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/wagoid/commitlint-github-action/compare/f114310111fdbd07e99f47f9ca13d62b3ec98372...0d749a1a91d4770e983a7b8f83d4a3f0e7e0874e)\n\n---\nupdated-dependencies:\n- dependency-name: wagoid/commitlint-github-action\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2023-12-08T09:24:40+01:00",
+          "tree_id": "ad3148231d8e1b8bc7720be52fc4e08527b2cda6",
+          "url": "https://github.com/maidsafe/safe_network/commit/5990e3eb3bf29a48f2d026937c045732e223db57"
+        },
+        "date": 1702024798208,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 4528,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 71798,
             "unit": "ms"
           },
           {
