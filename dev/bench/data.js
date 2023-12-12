adefda3c4ac6fe0fcd0870ebf8ac0a2464ab79bc
github-action-benchmark <github@users.noreply.github.com>
2023-12-12 10:54:38 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 58c044fc8837c681f1a28ad3836be9a8d698e962

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702378299054,
+  "lastUpdate": 1702378478597,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11707,35 +11707,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "8dfe4b950ad0745a02bda2fd72afe34a8a9c49f2",
-          "message": "fix(ci): provide correct platform value",
-          "timestamp": "2023-08-02T08:14:10+02:00",
-          "tree_id": "4cdb1c6debd95343eddfe2533a5ee92139e5bd9f",
-          "url": "https://github.com/maidsafe/safe_network/commit/8dfe4b950ad0745a02bda2fd72afe34a8a9c49f2"
-        },
-        "date": 1690976980127,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 25.48,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -20399,6 +20370,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 92,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "58c044fc8837c681f1a28ad3836be9a8d698e962",
+          "message": "feat(ci): don't log raw data",
+          "timestamp": "2023-12-12T11:26:44+01:00",
+          "tree_id": "44dbef4d98ce1380ddf4589fc2d1ae6a6e60cc2d",
+          "url": "https://github.com/maidsafe/safe_network/commit/58c044fc8837c681f1a28ad3836be9a8d698e962"
+        },
+        "date": 1702378477684,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 84,
             "unit": "MB"
           }
         ]
