6e91a31137216e9103d2e20e7ae7ef4805ae7bc6
Josh Wilson <joshuef@gmail.com>
2023-12-13 16:42:27 +0100

    32

--- sn_client/src/file_apis.rs
+++ sn_client/src/file_apis.rs
@@ -43,7 +43,7 @@ pub struct Files {
 type ChunkFileResult = Result<(XorName, u64, Vec<(XorName, PathBuf)>)>;
 
 // Defines the size of batch for the parallel downloading of data.
-pub const BATCH_SIZE: usize = 16;
+pub const BATCH_SIZE: usize = 32;
 
 impl Files {
     /// Create file apis instance.
