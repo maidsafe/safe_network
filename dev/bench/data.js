799f72c4608aeda04f8228447612efb05100ab60
github-action-benchmark <github@users.noreply.github.com>
2023-12-19 15:04:26 +0000

    add Client memory (customSmallerIsBetter) benchmark result for cf0c80d09a56f7860071edb0d69617dd6a7e6f25

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702998264758,
+  "lastUpdate": 1702998266584,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20408,40 +20408,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": false,
-          "id": "c8983fadc3dfc98e073b93904ca8eed09b295be6",
-          "message": "fix: correct bench download calculation",
-          "timestamp": "2023-08-31T11:28:17Z",
-          "tree_id": "9f214c04fd4f60123aef0b7ae65a742d3ca93cb0",
-          "url": "https://github.com/maidsafe/safe_network/commit/c8983fadc3dfc98e073b93904ca8eed09b295be6"
-        },
-        "date": 1693488356942,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 1351.258,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 1256,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "RolandSherwin",
             "username": "RolandSherwin"
@@ -30600,6 +30566,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 112,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "cf0c80d09a56f7860071edb0d69617dd6a7e6f25",
+          "message": "chore: add data path field to node info\n\nSince we provide the logs path, it's useful for the node manager to also have access here to the\ndata path, which it can provide to the user.\n\nI also wanted to provide the port the node is running on, but it's not easy to get access to that if\nthe port is assigned by libp2p.",
+          "timestamp": "2023-12-19T14:25:09Z",
+          "tree_id": "e3fb3bdbdbe0ce998cfaad234c8db575b2368ab0",
+          "url": "https://github.com/maidsafe/safe_network/commit/cf0c80d09a56f7860071edb0d69617dd6a7e6f25"
+        },
+        "date": 1702998265995,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 666,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 117,
             "unit": "MB"
           }
         ]
