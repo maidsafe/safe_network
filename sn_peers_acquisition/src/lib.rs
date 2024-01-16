471148f1c349c21743b6a8f6ad81041a259d9cc2
Josh Wilson <joshuef@gmail.com>
2024-01-16 16:25:31 +0100

    chore: tidy up wasm32 as target arch rather than a feat

--- sn_peers_acquisition/src/lib.rs
+++ sn_peers_acquisition/src/lib.rs
@@ -124,17 +124,18 @@ async fn get_network_contacts(args: &PeersArgs) -> Result<Vec<Multiaddr>> {
 pub fn parse_peer_addr(addr: &str) -> Result<Multiaddr> {
     // Parse valid IPv4 socket address, e.g. `1.2.3.4:1234`.
     if let Ok(addr) = addr.parse::<std::net::SocketAddrV4>() {
-        #[cfg(not(feature = "quic, webtransport-websys"))]
+        let multiaddr = Multiaddr::from(*addr.ip());
+        #[cfg(feature = "tcp")]
         // Turn the address into a `/ip4/<ip>/tcp/<port>` multiaddr.
-        let multiaddr = Multiaddr::from(*addr.ip()).with(Protocol::Tcp(addr.port()));
+        let multiaddr = multiaddr.with(Protocol::Tcp(addr.port()));
         #[cfg(feature = "quic")]
         // Turn the address into a `/ip4/<ip>/udp/<port>/quic-v1` multiaddr.
-        let multiaddr = Multiaddr::from(*addr.ip())
+        let multiaddr = multiaddr
             .with(Protocol::Udp(addr.port()))
             .with(Protocol::QuicV1);
-        #[cfg(feature = "webtransport-websys")]
+        #[cfg(target_arch = "wasm32")]
         // Turn the address into a `/ip4/<ip>/udp/<port>/webtransport-websys-v1` multiaddr.
-        let multiaddr = Multiaddr::from(*addr.ip())
+        let multiaddr = multiaddr
             .with(Protocol::Udp(addr.port()))
             .with(Protocol::WebTransport);
         return Ok(multiaddr);
