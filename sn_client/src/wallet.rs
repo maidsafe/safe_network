44939878c8dad60ffb5d6cd07ce950c1e6ce7c1c
Jason Paul <digital@engineer.com>
2024-01-16 16:46:46 +0000

    docs: new wallet client example

--- sn_client/src/wallet.rs
+++ sn_client/src/wallet.rs
@@ -35,6 +35,26 @@ pub struct WalletClient {
 
 impl WalletClient {
     /// Create a new wallet client.
+    ///
+    /// # Arguments
+    /// * `client` - A instance of the struct [`sn_client::Client`](self::Client)
+    /// * `wallet` - An instance of the struct [`sn_transfers::LocalWallet`](self::LocalWallet)
+    ///
+    /// # Example
+    /// ```no_run
+    /// use sn_client::{Client, WalletClient, Error};
+    /// use tempfile::TempDir;
+    /// use bls::SecretKey;
+    /// use sn_transfers::{LocalWallet, MainSecretKey};
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(),Error>{
+    /// let client = Client::new(SecretKey::random(), None, false, None).await?;
+    /// let tmp_path = TempDir::new()?.path().to_owned();
+    /// let mut wallet = LocalWallet::load_from_path(&tmp_path,Some(MainSecretKey::new(SecretKey::random())))?;
+    /// let mut wallet_client = WalletClient::new(client, wallet);
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn new(client: Client, wallet: LocalWallet) -> Self {
         Self { client, wallet }
     }
