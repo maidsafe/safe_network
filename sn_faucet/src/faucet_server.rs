4868ebfb38f4b8dd63eab88b08020441beee707f
Ian Coleman <ian@iancoleman.io>
2024-01-19 11:09:04 +1100

    feat(faucet): load public keys for distribution

--- sn_faucet/src/faucet_server.rs
+++ sn_faucet/src/faucet_server.rs
@@ -13,11 +13,13 @@ use sn_client::Client;
 use sn_transfers::{LocalWallet, NanoTokens};
 use std::collections::HashMap;
 use std::path::{self, Path, PathBuf};
+use std::str::FromStr;
 use tiny_http::{Response, Server};
 use tracing::{debug, error, info, trace};
 
 const SNAPSHOT_FILENAME: &str = "snapshot.json";
 const SNAPSHOT_URL: &str = "https://api.omniexplorer.info/ask.aspx?api=getpropertybalances&prop=3";
+const PUBKEYS_URL: &str = "https://pastebin.com/raw/pUm6tVRN";
 
 // Parsed from json in SNAPSHOT_URL
 #[derive(Serialize, Deserialize)]
@@ -69,7 +71,8 @@ pub async fn restart_faucet_server(client: &Client) -> Result<()> {
 }
 
 async fn startup_server(client: &Client) -> Result<()> {
-    load_maid_snapshot()?;
+    let _balances = load_maid_snapshot()?;
+    let _keys = load_maid_pubkeys()?;
     let server =
         Server::http("0.0.0.0:8000").map_err(|err| eyre!("Failed to start server: {err}"))?;
 
@@ -129,6 +132,15 @@ fn get_snapshot_data_dir_path() -> Result<PathBuf> {
     let dir = dirs_next::data_dir()
         .ok_or_else(|| eyre!("could not obtain data directory path".to_string()))?
         .join("safe_snapshot");
+    std::fs::create_dir_all(dir.clone())?;
+    Ok(dir.to_path_buf())
+}
+
+fn get_pubkeys_data_dir_path() -> Result<PathBuf> {
+    let dir = dirs_next::data_dir()
+        .ok_or_else(|| eyre!("could not obtain data directory path".to_string()))?
+        .join("safe_snapshot")
+        .join("pubkeys");
     std::fs::create_dir_all(dir.clone())?;
     Ok(dir.to_path_buf())
 }
@@ -215,4 +227,101 @@ fn parse_snapshot(json_str: String) -> Result<HashMap<String, u32>> {
     // log the total number of balances that were parsed
     info!("Parsed {} maid balances from the snapshot", balances.len());
     Ok(balances_map)
+}
+
+fn load_maid_pubkeys() -> Result<HashMap<String, String>> {
+    info!("Loading public keys for distributions");
+    let mut pubkeys: HashMap<String, String> = HashMap::new();
+    // load from existing files
+    let pk_dir = get_pubkeys_data_dir_path()?;
+    let file_list = std::fs::read_dir(pk_dir)?;
+    for file in file_list {
+        // add to hashmap
+        let file = file?;
+        let pk_hex = std::fs::read_to_string(file.path())?;
+        let address = match file.file_name().into_string() {
+            Ok(s) => s,
+            Err(s) => {
+                let msg = format!("Error reading filename {s:?}");
+                return Err(eyre!(msg));
+            }
+        };
+        pubkeys.insert(address, pk_hex);
+    }
+    info!("{} pubkeys after reading existing files", pubkeys.len());
+    // load from blockchain list on internet
+    info!("Fetching pukeys from {PUBKEYS_URL}");
+    let response = minreq::get(PUBKEYS_URL).send()?;
+    // check the request is ok
+    if response.status_code != 200 {
+        let msg = format!(
+            "Pubkey request failed with http status {}",
+            response.status_code
+        );
+        return Err(eyre!(msg));
+    }
+    // parse the response as csv, each row has format:
+    // address,pkhex
+    let body = response.as_str()?;
+    let lines: Vec<&str> = body.trim().split('\n').collect();
+    info!("{} pubkey rows from {PUBKEYS_URL}", lines.len());
+    for line in lines {
+        let cells: Vec<&str> = line.split(',').collect();
+        if cells.len() != 2 {
+            continue;
+        }
+        let address = cells[0].trim().to_string();
+        let pk_hex = cells[1].trim().to_string();
+        // validate this pk corresponds to the address
+        if !maid_pk_matches_address(&address, &pk_hex) {
+            continue;
+        }
+        // save this pair to pk_dir
+        save_address_pk(&address, &pk_hex)?;
+        // add this pair to the hashmap
+        pubkeys.insert(address, pk_hex);
+    }
+    info!(
+        "{} pubkeys after reading from blockchain list",
+        pubkeys.len()
+    );
+    Ok(pubkeys)
+}
+
+fn maid_pk_matches_address(address: &str, pk_hex: &str) -> bool {
+    // parse the address
+    let addr = match bitcoin::Address::from_str(address) {
+        Ok(a) => a,
+        Err(_) => return false,
+    };
+    let btc_addr = match addr.clone().require_network(bitcoin::Network::Bitcoin) {
+        Ok(a) => a,
+        Err(_) => return false,
+    };
+    // parse the public key
+    let pk = match bitcoin::PublicKey::from_str(pk_hex) {
+        Ok(p) => p,
+        Err(_) => return false,
+    };
+    // The public key may be for a p2pkh address (starting with 1) or a p2wpkh
+    // address (starting with 3) so we need to check both.
+    let is_p2pkh = btc_addr.is_related_to_pubkey(&pk);
+    if is_p2pkh {
+        return true;
+    }
+    let p2wpkh_addr = match bitcoin::Address::p2shwpkh(&pk, bitcoin::Network::Bitcoin) {
+        Ok(a) => a,
+        Err(_) => return false,
+    };
+    let is_p2wpkh = p2wpkh_addr == addr;
+    if is_p2wpkh {
+        return true;
+    }
+    false
+}
+
+fn save_address_pk(address: &str, pk_hex: &str) -> Result<()> {
+    let addr_path = get_pubkeys_data_dir_path()?.join(address);
+    std::fs::write(addr_path, pk_hex)?;
+    Ok(())
 }
