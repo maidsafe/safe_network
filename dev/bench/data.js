3d30df8096f4965f5d72dd09f36d14816f633a91
github-action-benchmark <github@users.noreply.github.com>
2024-02-05 16:55:00 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 439522916dff685a674e991dd68e920da3987056

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707152099354,
+  "lastUpdate": 1707152100821,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -35784,6 +35784,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver average long handling time",
             "value": 5,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "439522916dff685a674e991dd68e920da3987056",
+          "message": "fix: avoid log raw bytes of key accidently",
+          "timestamp": "2024-02-05T15:07:08Z",
+          "tree_id": "9d02ef4c9063bdd742de1d1abb3778297c82ee8d",
+          "url": "https://github.com/maidsafe/safe_network/commit/439522916dff685a674e991dd68e920da3987056"
+        },
+        "date": 1707152100492,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 6869,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 41940,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 6,
             "unit": "ms"
           }
         ]
