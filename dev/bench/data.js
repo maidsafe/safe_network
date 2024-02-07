2b3d3c4abc3a334fd38f18cc17dfef8973874c3d
github-action-benchmark <github@users.noreply.github.com>
2024-02-07 15:46:15 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 0d60555638267e3607cf3203912cf0117c4f9e61

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707320774049,
+  "lastUpdate": 1707320775636,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -36013,6 +36013,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 40955,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 5,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "grumbach",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "anselmega@gmail.com",
+            "name": "Anselme",
+            "username": "grumbach"
+          },
+          "distinct": true,
+          "id": "0d60555638267e3607cf3203912cf0117c4f9e61",
+          "message": "feat: extendable local state DAG in cli",
+          "timestamp": "2024-02-07T14:50:56Z",
+          "tree_id": "93198813e754521e18ed6687ed58dd0d1d184bb7",
+          "url": "https://github.com/maidsafe/safe_network/commit/0d60555638267e3607cf3203912cf0117c4f9e61"
+        },
+        "date": 1707320775304,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 7339,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 43178,
             "unit": "ms"
           },
           {
