3d0d108cd07ef50c9836f24f599acd6026e60a67
github-action-benchmark <github@users.noreply.github.com>
2024-02-08 09:10:17 +0000

    add Client memory (customSmallerIsBetter) benchmark result for aadc8b4db09b26d0c23a76e9c5a8bd8c0772c5ea

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707383416489,
+  "lastUpdate": 1707383417947,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20406,40 +20406,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "anselmega@gmail.com",
-            "name": "Anselme",
-            "username": "grumbach"
-          },
-          "distinct": false,
-          "id": "22624ae9055a3b87b1d9227b99b3a77a8906eeb3",
-          "message": "chore(logging): reduce metric frequency and logged stats.\n\nSome of this is never used",
-          "timestamp": "2023-10-03T08:25:10Z",
-          "tree_id": "052b37f06bc10201372828b2a1b1174d7b30eb90",
-          "url": "https://github.com/maidsafe/safe_network/commit/22624ae9055a3b87b1d9227b99b3a77a8906eeb3"
-        },
-        "date": 1696325189529,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 146,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 35,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -30597,6 +30563,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 83,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "aadc8b4db09b26d0c23a76e9c5a8bd8c0772c5ea",
+          "message": "chore: improvements from dev feedback\n\nReduce the start up time for the local network by removing the wait in the `get_available_port`\nfunction. This function is now only being used to select ports for the RPC service, so it shouldn't\nbe subject to the same problems we had with the node when it switched over to Quic.\n\nAlso, the `--clean` argument on the run command has been extended to kill any existing local\nnetwork.",
+          "timestamp": "2024-02-08T08:11:51Z",
+          "tree_id": "d8b6bd786da80e90bde9735c0f651e0d64b1a2b5",
+          "url": "https://github.com/maidsafe/safe_network/commit/aadc8b4db09b26d0c23a76e9c5a8bd8c0772c5ea"
+        },
+        "date": 1707383417575,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 200,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 86,
             "unit": "MB"
           }
         ]
