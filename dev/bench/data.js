d53b851d12d62f8ea2456876dd0f807151aca5cd
github-action-benchmark <github@users.noreply.github.com>
2024-02-08 14:24:29 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 1a4ca918d65892dc1a840dba581ebb03232d40be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707402170356,
+  "lastUpdate": 1707402269315,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11706,35 +11706,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "anselmega@gmail.com",
-            "name": "grumbach",
-            "username": "grumbach"
-          },
-          "committer": {
-            "email": "anselmega@gmail.com",
-            "name": "Anselme",
-            "username": "grumbach"
-          },
-          "distinct": true,
-          "id": "d97e7873f9d3bdd7b072851a62120cbcb3afec33",
-          "message": "feat: faucet retry genesis claim on failure",
-          "timestamp": "2023-10-03T12:24:46Z",
-          "tree_id": "8a5f13cc7d9d2e318d53630ceb4a6b8c6c9f923b",
-          "url": "https://github.com/maidsafe/safe_network/commit/d97e7873f9d3bdd7b072851a62120cbcb3afec33"
-        },
-        "date": 1696339900390,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 49,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "bzeeman@live.nl",
             "name": "Benno Zeeman",
             "username": "b-zee"
@@ -20397,6 +20368,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 51,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno Zeeman",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "1a4ca918d65892dc1a840dba581ebb03232d40be",
+          "message": "fix(networking): solve large_enum_variant warning",
+          "timestamp": "2024-02-08T13:29:39Z",
+          "tree_id": "2993ba696149ecfdb9397b0c44d84105e91016ec",
+          "url": "https://github.com/maidsafe/safe_network/commit/1a4ca918d65892dc1a840dba581ebb03232d40be"
+        },
+        "date": 1707402268453,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 49,
             "unit": "MB"
           }
         ]
