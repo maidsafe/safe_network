208627891392384b2679c54328e4b4ea53262afe
qima <qi.ma@maidsafe.net>
2024-02-09 15:31:58 +0800

    chore(node): disable metrics record

--- sn_networking/src/driver.rs
+++ sn_networking/src/driver.rs
@@ -618,6 +618,7 @@ pub struct SwarmDriver {
     pub(crate) close_group: Vec<PeerId>,
     pub(crate) replication_fetcher: ReplicationFetcher,
     #[cfg(feature = "open-metrics")]
+    #[allow(unused)]
     pub(crate) network_metrics: NetworkMetrics,
 
     cmd_receiver: mpsc::Receiver<SwarmCmd>,
