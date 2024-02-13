ad7f867e7dd1207469f5556c212b24662a7203b5
Josh Wilson <joshuef@gmail.com>
2024-02-13 07:29:18 +0100

    ci: pass release channel into bump_version script

--- resources/scripts/bump_version.sh
+++ resources/scripts/bump_version.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+# optional release channel to pass in
+channel_prefix=""
+if [ ! -z "$1" ]; then
+  channel_prefix="$1-"
+fi
+
 release-plz update 2>&1 | tee bump_version_output
 
 crates_bumped=()
 while IFS= read -r line; do
   name=$(echo "$line" | awk -F"\`" '{print $2}')
   version=$(echo "$line" | awk -F"-> " '{print $2}')
-  crates_bumped+=("${name}-v${version}")
+  crates_bumped+=("${channel_prefix}${name}-v${version}")
 done < <(cat bump_version_output | grep "^\*")
 
 len=${#crates_bumped[@]}
