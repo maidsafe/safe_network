6f77b2b84446f7193570d0fad522a0b593360756
github-action-benchmark <github@users.noreply.github.com>
2024-02-15 15:43:03 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 8d72df3ea99b8a6bee2953013929b667d3b155b4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708011681883,
+  "lastUpdate": 1708011783434,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11706,35 +11706,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "115c9396bdd5e4c7af731699d97217e8e9c13f48",
-          "message": "feat(networking): return valid result if one found during a timeout",
-          "timestamp": "2023-10-11T15:46:31+02:00",
-          "tree_id": "d4a54ea20a527dc97dd0815288e0fd20883abfad",
-          "url": "https://github.com/maidsafe/safe_network/commit/115c9396bdd5e4c7af731699d97217e8e9c13f48"
-        },
-        "date": 1697033626818,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 51,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "anselmega@gmail.com",
             "name": "grumbach",
             "username": "grumbach"
@@ -20397,6 +20368,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 48,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gabrielviganotti@gmail.com",
+            "name": "Gabriel Viganotti",
+            "username": "bochaco"
+          },
+          "committer": {
+            "email": "gabrielviganotti@gmail.com",
+            "name": "bochaco",
+            "username": "bochaco"
+          },
+          "distinct": false,
+          "id": "8d72df3ea99b8a6bee2953013929b667d3b155b4",
+          "message": "chore: minor doc change based on peer review",
+          "timestamp": "2024-02-15T14:52:40Z",
+          "tree_id": "adfe6a93bbffae5e8cbc539e115b3337bb0ea232",
+          "url": "https://github.com/maidsafe/safe_network/commit/8d72df3ea99b8a6bee2953013929b667d3b155b4"
+        },
+        "date": 1708011782295,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 49,
             "unit": "MB"
           }
         ]
