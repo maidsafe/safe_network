73c3b047340bf536655ca5b8a8257618512bc613
Roland Sherwin <RolandSherwin@protonmail.com>
2024-02-22 13:25:29 +0530

    feat(daemon): retain peer_id while restarting a safenode service

--- sn_node_manager/src/daemon_control.rs
+++ sn_node_manager/src/daemon_control.rs
@@ -7,11 +7,11 @@
 // permissions and limitations relating to use of the SAFE Network Software.
 
 use crate::{node_control, service::ServiceControl, VerbosityLevel};
-use color_eyre::Result;
-use libp2p::Multiaddr;
+use color_eyre::{eyre::OptionExt, Result};
+use libp2p::PeerId;
 use service_manager::{ServiceInstallCtx, ServiceLabel};
 use sn_node_rpc_client::RpcActions;
-use sn_protocol::node_registry::Node;
+use sn_protocol::node_registry::{Node, NodeRegistry, NodeStatus};
 use std::{ffi::OsString, net::Ipv4Addr, path::PathBuf};
 
 pub fn run_daemon(
@@ -53,35 +53,121 @@ pub fn run_daemon(
 }
 
 pub async fn restart_node_service(
-    node: &mut Node,
+    node_registry: &mut NodeRegistry,
+    peer_id: PeerId,
+    retain_peer_id: bool,
     rpc_client: &dyn RpcActions,
     service_control: &dyn ServiceControl,
-    retain_peer_id: bool,
-    bootstrap_peers: Vec<Multiaddr>,
-    env_variables: Option<Vec<(String, String)>>,
 ) -> Result<()> {
-    node_control::stop(node, service_control).await?;
+    let nodes_len = node_registry.nodes.len();
+    let current_node = node_registry
+        .nodes
+        .iter_mut()
+        .find(|node| node.peer_id.is_some_and(|id| id == peer_id))
+        .ok_or_eyre(format!("Could not find the provided PeerId: {peer_id:?}"))?;
+    node_control::stop(current_node, service_control).await?;
 
-    // reuse the same port and root dir to retain peer id.
     if retain_peer_id {
-        service_control.uninstall(&node.service_name.clone())?;
+        // reuse the same port and root dir to retain peer id.
+        service_control.uninstall(&current_node.service_name.clone())?;
         let install_ctx = node_control::InstallNodeServiceCtxBuilder {
-            local: node.local,
-            data_dir_path: node.data_dir_path.clone(),
-            genesis: node.genesis,
-            name: node.service_name.clone(),
-            node_port: node.get_safenode_port(),
-            bootstrap_peers,
-            rpc_socket_addr: node.rpc_socket_addr,
-            log_dir_path: node.log_dir_path.clone(),
-            safenode_path: node.safenode_path.clone(),
-            service_user: node.user.clone(),
-            env_variables,
+            local: current_node.local,
+            data_dir_path: current_node.data_dir_path.clone(),
+            genesis: current_node.genesis,
+            name: current_node.service_name.clone(),
+            node_port: current_node.get_safenode_port(),
+            bootstrap_peers: node_registry.bootstrap_peers.clone(),
+            rpc_socket_addr: current_node.rpc_socket_addr,
+            log_dir_path: current_node.log_dir_path.clone(),
+            safenode_path: current_node.safenode_path.clone(),
+            service_user: current_node.user.clone(),
+            env_variables: node_registry.environment_variables.clone(),
         }
         .build()?;
         service_control.install(install_ctx)?;
-    }
+        node_control::start(
+            current_node,
+            service_control,
+            rpc_client,
+            VerbosityLevel::Normal,
+        )
+        .await?;
+    } else {
+        // else start a new node instance.
+        let new_node_number = nodes_len + 1;
+        let new_service_name = format!("safenode{new_node_number}");
 
-    node_control::start(node, service_control, rpc_client, VerbosityLevel::Normal).await?;
+        // modify the paths & copy safenode binary
+        let log_dir_path = {
+            let mut log_dir_path = current_node.log_dir_path.clone();
+            log_dir_path.pop();
+            log_dir_path.join(&new_service_name)
+        };
+        let data_dir_path = {
+            let mut data_dir_path = current_node.data_dir_path.clone();
+            data_dir_path.pop();
+            data_dir_path.join(&new_service_name)
+        };
+        let safenode_path = {
+            let mut safenode_path = current_node.safenode_path.clone();
+            let safenode_file_name = safenode_path
+                .file_name()
+                .ok_or_eyre("Could not get filename from the current node's safenode path")?
+                .to_string_lossy()
+                .to_string();
+            safenode_path.pop();
+            let safenode_path = safenode_path.join(safenode_file_name);
+
+            std::fs::copy(&current_node.safenode_path, &safenode_path)?;
+            safenode_path
+        };
+
+        let install_ctx = node_control::InstallNodeServiceCtxBuilder {
+            local: current_node.local,
+            genesis: current_node.genesis,
+            name: new_service_name.clone(),
+            // don't re-use port
+            node_port: None,
+            bootstrap_peers: node_registry.bootstrap_peers.clone(),
+            rpc_socket_addr: current_node.rpc_socket_addr,
+            // set new paths
+            data_dir_path: data_dir_path.clone(),
+            log_dir_path: log_dir_path.clone(),
+            safenode_path: safenode_path.clone(),
+            service_user: current_node.user.clone(),
+            env_variables: node_registry.environment_variables.clone(),
+        }
+        .execute()?;
+        service_control.install(install_ctx)?;
+
+        let mut node = Node {
+            genesis: current_node.genesis,
+            local: current_node.local,
+            service_name: new_service_name,
+            user: current_node.user.clone(),
+            number: new_node_number as u16,
+            rpc_socket_addr: current_node.rpc_socket_addr,
+            version: current_node.version.clone(),
+            status: NodeStatus::Added,
+            listen_addr: None,
+            pid: None,
+            peer_id: None,
+            log_dir_path,
+            data_dir_path,
+            safenode_path,
+            connected_peers: None,
+        };
+        node_control::start(
+            &mut node,
+            service_control,
+            rpc_client,
+            VerbosityLevel::Normal,
+        )
+        .await?;
+        node_registry.nodes.push(node);
+    };
+
+    node_registry.save()?;
+
     Ok(())
 }
