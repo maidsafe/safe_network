688e814a3fd5e3a22bcf6e7b4585951e591100c0
Chris O'Neil <chriso83@protonmail.com>
2024-03-13 15:45:07 +0000

    feat: provide `faucet upgrade` command
    Uses the generic service manager to provide a `faucet upgrade` command, which will be used during
    the general upgrade process to either upgrade or downgrade the faucet.
    
    Some code was common between the node and faucet upgrades, so that was refactored to be shared
    between the two.
    
    Also clarifies some of the language used on the command documentation.

--- sn_node_manager/src/cmd/faucet.rs
+++ sn_node_manager/src/cmd/faucet.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use super::is_running_as_root;
+use super::{download_and_get_upgrade_bin_path, is_running_as_root, print_upgrade_summary};
 use crate::{
     add_services::{add_faucet, config::AddFaucetServiceOptions},
     config,
@@ -14,11 +14,13 @@ use crate::{
     ServiceManager, VerbosityLevel,
 };
 use color_eyre::{eyre::eyre, Result};
+use colored::Colorize;
+use semver::Version;
 use sn_peers_acquisition::{get_peers_from_args, PeersArgs};
 use sn_releases::{ReleaseType, SafeReleaseRepositoryInterface};
 use sn_service_management::{
     control::{ServiceControl, ServiceController},
-    FaucetService, NodeRegistry,
+    FaucetService, NodeRegistry, UpgradeOptions,
 };
 use sn_transfers::get_faucet_data_dir;
 use std::path::PathBuf;
@@ -130,4 +132,71 @@ pub async fn stop(verbosity: VerbosityLevel) -> Result<()> {
     }
 
     Err(eyre!("The faucet service has not been added yet"))
+}
+
+pub async fn upgrade(
+    do_not_start: bool,
+    force: bool,
+    provided_env_variables: Option<Vec<(String, String)>>,
+    url: Option<String>,
+    version: Option<String>,
+    verbosity: VerbosityLevel,
+) -> Result<()> {
+    if !is_running_as_root() {
+        return Err(eyre!("The upgrade command must run as the root user"));
+    }
+
+    let mut node_registry = NodeRegistry::load(&config::get_node_registry_path()?)?;
+    if node_registry.faucet.is_none() {
+        println!("No faucet service has been created yet. No upgrade required.");
+        return Ok(());
+    }
+
+    if verbosity != VerbosityLevel::Minimal {
+        println!("=================================================");
+        println!("           Upgrade Faucet Service                ");
+        println!("=================================================");
+    }
+
+    let (upgrade_bin_path, target_version) =
+        download_and_get_upgrade_bin_path(ReleaseType::Faucet, url, version).await?;
+    let faucet = node_registry.faucet.clone().unwrap();
+
+    if !force {
+        let current_version = Version::parse(&faucet.version)?;
+        if target_version <= current_version {
+            println!(
+                "{} The faucet is already at the latest version",
+                "✓".green()
+            );
+            return Ok(());
+        }
+    }
+
+    let env_variables = if provided_env_variables.is_some() {
+        &provided_env_variables
+    } else {
+        &node_registry.environment_variables
+    };
+    let options = UpgradeOptions {
+        bootstrap_peers: node_registry.bootstrap_peers.clone(),
+        env_variables: env_variables.clone(),
+        force,
+        start_service: !do_not_start,
+        target_bin_path: upgrade_bin_path.clone(),
+        target_version: target_version.clone(),
+    };
+    let service = FaucetService::new(faucet.clone(), Box::new(ServiceController {}));
+    let mut service_manager =
+        ServiceManager::new(service, Box::new(ServiceController {}), verbosity);
+
+    match service_manager.upgrade(options).await {
+        Ok(upgrade_result) => {
+            node_registry.faucet = Some(service_manager.service.service_data);
+            print_upgrade_summary(vec![("faucet".to_string(), upgrade_result)]);
+            node_registry.save()?;
+            Ok(())
+        }
+        Err(e) => Err(eyre!("Upgrade failed: {e}")),
+    }
 }
