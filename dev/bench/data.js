ea40f0d8d66a23dba047842f1d59181493071a11
github-action-benchmark <github@users.noreply.github.com>
2024-03-14 15:19:22 +0000

    add Node memory (customSmallerIsBetter) benchmark result for fb9191c1d1b431e080890b4049a5e99b19308be1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710429456883,
+  "lastUpdate": 1710429562171,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11711,35 +11711,6 @@ window.BENCHMARK_DATA = {
             "username": "RolandSherwin"
           },
           "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "bc06c33ce7c6a86e5e78ceb71fa465f6671f3b9d",
-          "message": "feat: `bincode::serialize` into `Bytes` without intermediate allocation",
-          "timestamp": "2023-10-30T08:21:39Z",
-          "tree_id": "23b5df09bdcabd4cd8b240e0e17c91bae2c800ba",
-          "url": "https://github.com/maidsafe/safe_network/commit/bc06c33ce7c6a86e5e78ceb71fa465f6671f3b9d"
-        },
-        "date": 1698657535298,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 104,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
             "email": "qi.ma@maidsafe.net",
             "name": "maqi",
             "username": "maqi"
@@ -20397,6 +20368,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 50,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "gapspace@engineer.com",
+            "name": "Jason Paul",
+            "username": "JasonPaulGithub"
+          },
+          "distinct": true,
+          "id": "fb9191c1d1b431e080890b4049a5e99b19308be1",
+          "message": "fix(test): make tweaks to wan nightly tests",
+          "timestamp": "2024-03-14T14:29:06Z",
+          "tree_id": "e63a2351c243c4ff09fbe4766a38095bcc38056e",
+          "url": "https://github.com/maidsafe/safe_network/commit/fb9191c1d1b431e080890b4049a5e99b19308be1"
+        },
+        "date": 1710429561549,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 49,
             "unit": "MB"
           }
         ]
