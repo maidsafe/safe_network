c4fbac67380e3ba55563c3ff3db82acd9a1eaa20
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 10:46:53 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for fa158d5ea0112b5f2884913ca8914b97ae8ff239

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711579003260,
+  "lastUpdate": 1711622813791,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "gabrielviganotti@gmail.com",
-            "name": "bochaco",
-            "username": "bochaco"
-          },
-          "committer": {
-            "email": "anselmega@gmail.com",
-            "name": "Anselme",
-            "username": "grumbach"
-          },
-          "distinct": true,
-          "id": "3a729d92727a25743514e2815029ed662824343c",
-          "message": "feat(royalties): make royalties payment to be 15% of the total storage cost",
-          "timestamp": "2023-11-15T04:55:19Z",
-          "tree_id": "2215baeda5ae1aa6dd97edd3f2c6379d62a37556",
-          "url": "https://github.com/maidsafe/safe_network/commit/3a729d92727a25743514e2815029ed662824343c"
-        },
-        "date": 1700026610706,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 3.534760218709895,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 14.116197509015993,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 31.450439904203193,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11697,6 +11658,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 58.158375453204556,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "fa158d5ea0112b5f2884913ca8914b97ae8ff239",
+          "message": "fix(cli): read from cache during initial chunking process",
+          "timestamp": "2024-03-28T10:03:51Z",
+          "tree_id": "0aeb01e110c891dd526601bc34c8f0c9c1d1d2e5",
+          "url": "https://github.com/maidsafe/safe_network/commit/fa158d5ea0112b5f2884913ca8914b97ae8ff239"
+        },
+        "date": 1711622809827,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2540112367798657,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.8154200455115614,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 58.12667262857184,
             "unit": "MiB/s"
           }
         ]
