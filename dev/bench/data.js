bd6303cb6f4324423d57237eb617fedf88dacf9b
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 13:17:05 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for c78d10ab1e2f3731dd1122cacdd48279bf11db97

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711630511984,
+  "lastUpdate": 1711631825864,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": false,
-          "id": "a593dbd05d16115d35bd40c7cfbc8ca22259913d",
-          "message": "chore: include RPC endpoints field to DeploymentInventory",
-          "timestamp": "2023-11-15T08:45:28Z",
-          "tree_id": "c77f822bde3732cd2e84f4059a32c1aa653de7fc",
-          "url": "https://github.com/maidsafe/safe_network/commit/a593dbd05d16115d35bd40c7cfbc8ca22259913d"
-        },
-        "date": 1700040667320,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 4.2665442562124944,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 17.820593335735573,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 50.1629804354891,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11697,6 +11658,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 57.202607374719776,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "c78d10ab1e2f3731dd1122cacdd48279bf11db97",
+          "message": "chore: make sure cargo set-version is installed",
+          "timestamp": "2024-03-28T12:32:06Z",
+          "tree_id": "452539e5ac6e7f64e078e16b42a81cf243b9afbb",
+          "url": "https://github.com/maidsafe/safe_network/commit/c78d10ab1e2f3731dd1122cacdd48279bf11db97"
+        },
+        "date": 1711631822394,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2668819706224594,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.8174090258767663,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 57.1258088321113,
             "unit": "MiB/s"
           }
         ]
