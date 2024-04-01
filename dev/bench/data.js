705fe85f850cccab93a7cd9bfd6a25f736c70c88
github-action-benchmark <github@users.noreply.github.com>
2024-04-01 12:18:16 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 3d231c6b34af8f8e80006b8236a9ef0d2aa97068

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711973790732,
+  "lastUpdate": 1711973895950,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11706,35 +11706,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "ca49e1ec2559c9e5d14a4f054682ec0d5ae01f08",
-          "message": "fix(logs): enable faucet logs",
-          "timestamp": "2023-11-21T09:01:41+01:00",
-          "tree_id": "8e46ae14a49a2448ebbc685105ee95e48c9bcabc",
-          "url": "https://github.com/maidsafe/safe_network/commit/ca49e1ec2559c9e5d14a4f054682ec0d5ae01f08"
-        },
-        "date": 1700554561613,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 139,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -20397,6 +20368,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 48,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "grumbach",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "anselmega@gmail.com",
+            "name": "Anselme",
+            "username": "grumbach"
+          },
+          "distinct": true,
+          "id": "3d231c6b34af8f8e80006b8236a9ef0d2aa97068",
+          "message": "chore: make a constant",
+          "timestamp": "2024-04-01T11:35:28Z",
+          "tree_id": "32cbb25cdb6f4de5353f2a89128ee7fece2bbdbb",
+          "url": "https://github.com/maidsafe/safe_network/commit/3d231c6b34af8f8e80006b8236a9ef0d2aa97068"
+        },
+        "date": 1711973895536,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 49,
             "unit": "MB"
           }
         ]
