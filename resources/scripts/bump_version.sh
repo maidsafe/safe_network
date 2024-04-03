ca1675654f2425c3d8e80f42a5cf66f5d5517aee
Chris O'Neil <chriso83@protonmail.com>
2024-04-03 17:00:58 +0100

    chore: clarifications to version bumping script
    There was an issue with the script always applying 0 for the pre-release identifier. We got into a
    situation where it actually needed to be incremented by 1, because there was already a crate
    published at the 0 version.
    
    Added some text output to help clarify what the script is doing.
    
    Also tidied the style by consistently applying Bash syntax (as opposed to POSIX compatible) and a
    tab size of 2.

--- resources/scripts/bump_version.sh
+++ resources/scripts/bump_version.sh
@@ -2,27 +2,25 @@
 
 set -e
 
-
 # Suffix to append to the version. Passed as an argument to this script.
 SUFFIX="$1"
 
 # Ensure cargo set-version is installed
 if ! cargo set-version --help > /dev/null 2>&1; then
-    echo "cargo set-version command not found."
-    echo "Please install cargo-edit with the command: cargo install cargo-edit --features vendored-openssl"
-    exit 1
+  echo "cargo set-version command not found."
+  echo "Please install cargo-edit with the command: cargo install cargo-edit --features vendored-openssl"
+  exit 1
 fi
 
 # Ensure the suffix is either alpha or beta
-if [ -n "$SUFFIX" ]; then
-    if [[ "$SUFFIX" != "alpha" ]] && [[ "$SUFFIX" != "beta" ]]; then
-        echo "Invalid suffix. Suffix must be either 'alpha' or 'beta'."
-        exit 1
-    fi
+if [[ -n "$SUFFIX" ]]; then
+  if [[ "$SUFFIX" != "alpha" ]] && [[ "$SUFFIX" != "beta" ]]; then
+    echo "Invalid suffix. Suffix must be either 'alpha' or 'beta'."
+    exit 1
+  fi
 fi
 
 release-plz update 2>&1 | tee bump_version_output
-
 
 crates_bumped=()
 while IFS= read -r line; do
@@ -37,38 +35,40 @@ if [[ $len -eq 0 ]]; then
   exit 0
 fi
 
-# remove any performed changes if we're applying a suffix
-if [ -n "$SUFFIX" ]; then
-    git checkout -- .
+if [[ -n "$SUFFIX" ]]; then
+  echo "We are releasing to the $SUFFIX channel"
+  echo "Versions with $SUFFIX are not supported by release-plz"
+  echo "Reverting changes by release-plz"
+  git checkout -- .
 fi
 
 commit_message="chore(release): "
 for crate in "${crates_bumped[@]}"; do
-    # Extract the crate name and version in a cross-platform way
-    crate_name=$(echo "$crate" | sed -E 's/-v.*$//')
-    version=$(echo "$crate" | sed -E 's/^.*-v(.*)$/\1/')
-    new_version=$version
-    echo "the crate is: $crate_name"
-    # if we're changing the release channel...
-    if [ -n "$SUFFIX" ]; then
-        #if we're already in a realse channel, reapplying the suffix will reset things.
-        if [[ "$version" == *"-alpha."* || "$version" == *"-beta."* ]]; then
-          #remove any existing channel + version
-            base_version=$(echo "$version" | sed -E 's/(-alpha\.[0-9]+|-beta\.[0-9]+)$//')
-            new_version="${base_version}-${SUFFIX}.0"
-        else
-            new_version="${version}-${SUFFIX}.0"
-        fi
-    else
-        # For main release, strip any alpha or beta suffix from the version
-        new_version=$(echo "$version" | sed -E 's/(-alpha\.[0-9]+|-beta\.[0-9]+)$//')
-    fi
+  # Extract the crate name and version in a cross-platform way
+  crate_name=$(echo "$crate" | sed -E 's/-v.*$//')
+  version=$(echo "$crate" | sed -E 's/^.*-v(.*)$/\1/')
+  new_version=$version
 
-    # set the version
-    crate=$new_version
-    cargo set-version -p $crate_name $new_version
-    # update the commit msg
-    commit_message="${commit_message}${crate_name}-v$new_version/"
+  echo "----------------------------------------------------------"
+  echo "Processing $crate_name"
+  echo "----------------------------------------------------------"
+  if [[ -n "$SUFFIX" ]]; then
+    # if we're already in a release channel, reapplying the suffix will reset things.
+    if [[ "$version" == *"-alpha."* || "$version" == *"-beta."* ]]; then
+      base_version=$(echo "$version" | sed -E 's/(-alpha\.[0-9]+|-beta\.[0-9]+)$//')
+      pre_release_identifier=$(echo "$version" | sed -E 's/.*-(alpha|beta)\.([0-9]+)$/\2/')
+      new_version="${base_version}-${SUFFIX}.$pre_release_identifier"
+    else
+      new_version="${version}-${SUFFIX}.0"
+    fi
+  else
+    # For main release, strip any alpha or beta suffix from the version
+    new_version=$(echo "$version" | sed -E 's/(-alpha\.[0-9]+|-beta\.[0-9]+)$//')
+  fi
+
+  echo "Using set-version to apply $new_version to $crate_name"
+  cargo set-version -p $crate_name $new_version
+  commit_message="${commit_message}${crate_name}-v$new_version/" # append crate to commit message
 done
 commit_message=${commit_message%/} # strip off trailing '/' character
 
