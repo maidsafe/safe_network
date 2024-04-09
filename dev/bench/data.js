6b7eed3d503a5ba5667132ee13e2d333a1ba3a38
github-action-benchmark <github@users.noreply.github.com>
2024-04-09 15:58:53 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 17d292b2c2a2700a38b2a68ed69833c8ac81985a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712678225746,
+  "lastUpdate": 1712678333051,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11707,35 +11707,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "anselmega@gmail.com",
-            "name": "grumbach",
-            "username": "grumbach"
-          },
-          "committer": {
-            "email": "anselmega@gmail.com",
-            "name": "Anselme",
-            "username": "grumbach"
-          },
-          "distinct": true,
-          "id": "2c132d2bfde6688229de13b92697feaa8f7e2f23",
-          "message": "fix: protect against amounts tampering and incomplete spends attack",
-          "timestamp": "2023-12-05T13:20:48Z",
-          "tree_id": "1acf660c63ee560dffab3161a50a8444bc07c025",
-          "url": "https://github.com/maidsafe/safe_network/commit/2c132d2bfde6688229de13b92697feaa8f7e2f23"
-        },
-        "date": 1701785035095,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 91,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -20399,6 +20370,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 50,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "17d292b2c2a2700a38b2a68ed69833c8ac81985a",
+          "message": "feat(networking): shift to use ilog2 bucket distance for close data calcs\n\nilog2 is about magnitude rather than specifics as distance is already being estimated\nwe can use this to easily render a buffer zone of data we replicate\nvs a close bucket which we deem to be our responsibility",
+          "timestamp": "2024-04-09T15:15:02Z",
+          "tree_id": "10a720c91547f595bbeafb4754b815a84d54805e",
+          "url": "https://github.com/maidsafe/safe_network/commit/17d292b2c2a2700a38b2a68ed69833c8ac81985a"
+        },
+        "date": 1712678332446,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 54,
             "unit": "MB"
           }
         ]
