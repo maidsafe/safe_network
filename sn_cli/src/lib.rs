1d4761a731f2c8b440c69284c81a5e9e94aae77d
Roland Sherwin <RolandSherwin@protonmail.com>
2024-04-09 17:31:00 +0530

    feat(cli): implement FilesUploadStatusNotifier trait for lib code
    This would allow users of this struct to override the default stdout
    printing and instead provide thier own custom impl

--- sn_cli/src/lib.rs
+++ sn_cli/src/lib.rs
@@ -11,6 +11,6 @@ mod files;
 
 pub use acc_packet::AccountPacket;
 pub use files::{
-    download_file, download_files, ChunkManager, Estimator, FilesUploader, UploadedFile,
-    UPLOADED_FILES,
+    download_file, download_files, ChunkManager, Estimator, FilesUploadStatusNotifier,
+    FilesUploader, UploadedFile, UPLOADED_FILES,
 };
