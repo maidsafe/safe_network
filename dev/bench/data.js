051c8f27985e406c2da17a74956885a8687a9375
github-action-benchmark <github@users.noreply.github.com>
2024-04-09 13:49:12 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 1d4761a731f2c8b440c69284c81a5e9e94aae77d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712670550892,
+  "lastUpdate": 1712670552628,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20408,40 +20408,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "anselmega@gmail.com",
-            "name": "grumbach",
-            "username": "grumbach"
-          },
-          "committer": {
-            "email": "anselmega@gmail.com",
-            "name": "Anselme",
-            "username": "grumbach"
-          },
-          "distinct": true,
-          "id": "9e3b05a5e231397321257ad70067a0369cd41267",
-          "message": "fix: mark chunks as completed when no failures on retry",
-          "timestamp": "2023-12-05T11:44:29Z",
-          "tree_id": "15fbe1fd7aa1af48dcbce33a81cb0508e699e22a",
-          "url": "https://github.com/maidsafe/safe_network/commit/9e3b05a5e231397321257ad70067a0369cd41267"
-        },
-        "date": 1701779195274,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 647,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 150,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -30600,6 +30566,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 81,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "gabrielviganotti@gmail.com",
+            "name": "bochaco",
+            "username": "bochaco"
+          },
+          "distinct": true,
+          "id": "1d4761a731f2c8b440c69284c81a5e9e94aae77d",
+          "message": "feat(cli): implement FilesUploadStatusNotifier trait for lib code\n\nThis would allow users of this struct to override the default stdout\nprinting and instead provide thier own custom impl",
+          "timestamp": "2024-04-09T12:43:51Z",
+          "tree_id": "b47e8dcc662f27c88e3871759d0cd3a0718d1e02",
+          "url": "https://github.com/maidsafe/safe_network/commit/1d4761a731f2c8b440c69284c81a5e9e94aae77d"
+        },
+        "date": 1712670552057,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 191,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 82,
             "unit": "MB"
           }
         ]
