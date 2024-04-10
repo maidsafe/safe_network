847d98c5d9361ee505ce4d09ab1cc4a1d89a28f5
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 02:15:31 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 5cd1a19fefb649b7011f1c370ce81fb4a24f8787

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712715225685,
+  "lastUpdate": 1712715331150,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11707,35 +11707,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "gabrielviganotti@gmail.com",
-            "name": "bochaco",
-            "username": "bochaco"
-          },
-          "distinct": true,
-          "id": "32d264e31775de0b8be72bbea048889f656e38e9",
-          "message": "fix(node): get self spend should be aggregated even if it errors out",
-          "timestamp": "2023-12-05T18:27:58Z",
-          "tree_id": "2df25667d66006a7b35964d1437549686194a9cf",
-          "url": "https://github.com/maidsafe/safe_network/commit/32d264e31775de0b8be72bbea048889f656e38e9"
-        },
-        "date": 1701803450350,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 95,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "bzeeman@live.nl",
             "name": "Benno Zeeman",
             "username": "b-zee"
@@ -20399,6 +20370,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 47,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "5cd1a19fefb649b7011f1c370ce81fb4a24f8787",
+          "message": "feat(node): restore historic quoting metrics to allow restart",
+          "timestamp": "2024-04-10T01:22:48Z",
+          "tree_id": "ac4c36da9fd683b37676e7f8ff5720cd5d361c96",
+          "url": "https://github.com/maidsafe/safe_network/commit/5cd1a19fefb649b7011f1c370ce81fb4a24f8787"
+        },
+        "date": 1712715330764,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 61,
             "unit": "MB"
           }
         ]
