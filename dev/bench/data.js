5c7b2a62fa3de787d6be3a0ee707307c4122374d
github-action-benchmark <github@users.noreply.github.com>
2024-04-11 21:06:51 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 592ac510efa3741ea778ec6b994318ac7ef79470

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712850861142,
+  "lastUpdate": 1712869611367,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "maqi",
-            "username": "maqi"
-          },
-          "distinct": true,
-          "id": "45c0ed319a53cb5927afd9f8d7d1deee86b36555",
-          "message": "feat(cli): skip payment and upload for existing chunks",
-          "timestamp": "2023-12-12T16:02:42Z",
-          "tree_id": "9e1f5d9017dc0ba44abeb005ce60e2ee3c4d6463",
-          "url": "https://github.com/maidsafe/safe_network/commit/45c0ed319a53cb5927afd9f8d7d1deee86b36555"
-        },
-        "date": 1702399256312,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 1.3165942139198419,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.951172263254813,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 37.2288176171101,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11698,6 +11659,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 56.07948222377453,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "592ac510efa3741ea778ec6b994318ac7ef79470",
+          "message": "feat: distinguish failure to start during upgrade\n\nIt's possible for a service to be upgraded but then not subsequently start. In this case, it has\nstill been upgraded to a new version. It's worth making a distinction to the user between an actual\nerror in the upgrade process, or a failure to start, because in the latter case, they do actually\nhave an upgrade. They can then take action to try and start their services again.\n\nAs part of this change, the start process attempts to find whether the service process did indeed\nstart, because you don't always seem to get errors back from the service infrastructure. We also\nmake sure that we return an error if there was a failure with the upgrade process for any services.\nThis is necessary for visibility on our own deploy process.",
+          "timestamp": "2024-04-11T20:12:54Z",
+          "tree_id": "932c361826415206cab6aa3083f4a84edbbf7f03",
+          "url": "https://github.com/maidsafe/safe_network/commit/592ac510efa3741ea778ec6b994318ac7ef79470"
+        },
+        "date": 1712869606907,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2054149332607813,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.6058110794073848,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 56.86854190554197,
             "unit": "MiB/s"
           }
         ]
