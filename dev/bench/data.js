f2abd939adc4af4023ccf1763cae33bc0d5d6c1f
github-action-benchmark <github@users.noreply.github.com>
2024-04-15 16:53:52 +0000

    add Node memory (customSmallerIsBetter) benchmark result for cb4e0a36b0bd75133d89ab26d7062c18a55c617b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713199922091,
+  "lastUpdate": 1713200032297,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11707,35 +11707,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "git@happybeing.com",
-            "name": "Mark",
-            "username": "happybeing"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "10c55f1c1c81fab9eb19dd99989a3282d1046ae7",
-          "message": "fix: update README paths to use .local/share/safe",
-          "timestamp": "2023-12-13T15:00:00+01:00",
-          "tree_id": "bb6e2230bd361a41f884c68aa5d8810fe2687db7",
-          "url": "https://github.com/maidsafe/safe_network/commit/10c55f1c1c81fab9eb19dd99989a3282d1046ae7"
-        },
-        "date": 1702476825308,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 95,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -20399,6 +20370,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 49,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "cb4e0a36b0bd75133d89ab26d7062c18a55c617b",
+          "message": "test(CI): confirm there is no failed replication fetch",
+          "timestamp": "2024-04-15T16:01:21Z",
+          "tree_id": "0484f2c24f18d896e2f27518f041c517749d984f",
+          "url": "https://github.com/maidsafe/safe_network/commit/cb4e0a36b0bd75133d89ab26d7062c18a55c617b"
+        },
+        "date": 1713200031675,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 50,
             "unit": "MB"
           }
         ]
