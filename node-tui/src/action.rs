c39da28bd87a87ed610b8d98bf468c566c87f851
Josh Wilson <joshuef@gmail.com>
2024-04-17 11:23:24 +0900

    feat: init of nodeui

---
+++ node-tui/src/action.rs
@@ -0,0 +1,21 @@
+use std::{fmt, string::ToString};
+
+use serde::{
+    de::{self, Deserializer, Visitor},
+    Deserialize, Serialize,
+};
+use strum::Display;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Display, Deserialize)]
+pub enum Action {
+    Tick,
+    Render,
+    Resize(u16, u16),
+    Suspend,
+    Resume,
+    StartNode,
+    Quit,
+    Refresh,
+    Error(String),
+    Help,
+}
