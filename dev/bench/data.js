4351d1f7254103667bed575e254976a62f890d38
github-action-benchmark <github@users.noreply.github.com>
2024-04-22 18:24:48 +0000

    add Client memory (customSmallerIsBetter) benchmark result for c3b91c565fa8f9a879cfb2b5a84fdf1cd109e07e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713810286111,
+  "lastUpdate": 1713810288880,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20408,40 +20408,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "34c458e3e89d06cee3b2f5c2069436ea9b7cbc74",
-          "message": "chore: reduce default batch size",
-          "timestamp": "2023-12-20T09:04:58Z",
-          "tree_id": "b7c66be2755fa6c6b35c9f202d1c28bdaa368062",
-          "url": "https://github.com/maidsafe/safe_network/commit/34c458e3e89d06cee3b2f5c2069436ea9b7cbc74"
-        },
-        "date": 1703065328178,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 271,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 62,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "tomek@librem.one",
             "name": "trycatch",
             "username": "thomasmarkiewicz"
@@ -30595,6 +30561,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory usage w/ upload",
             "value": 186,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 83,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": false,
+          "id": "c3b91c565fa8f9a879cfb2b5a84fdf1cd109e07e",
+          "message": "chore(node): lower some log levels to reduce log size",
+          "timestamp": "2024-04-22T17:16:18Z",
+          "tree_id": "716782d7dee0e3a559e53274df7caf4130a9235b",
+          "url": "https://github.com/maidsafe/safe_network/commit/c3b91c565fa8f9a879cfb2b5a84fdf1cd109e07e"
+        },
+        "date": 1713810287727,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 198,
             "unit": "MB"
           },
           {
