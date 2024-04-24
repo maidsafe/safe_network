0872423487d2662c978c16bffb83acac23168185
github-action-benchmark <github@users.noreply.github.com>
2024-04-24 19:01:54 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for d501e3b4c12d44d0aa277b99a49f64a94def7f81

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713972632495,
+  "lastUpdate": 1713985314467,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "7d55a30c2338fbff17e9371950d11714c1d03536",
-          "message": "feat: use put_record_to during upload chunk",
-          "timestamp": "2023-12-29T07:00:36Z",
-          "tree_id": "831e0dc52e1891d913f384cc2c04295fac0b7de3",
-          "url": "https://github.com/maidsafe/safe_network/commit/7d55a30c2338fbff17e9371950d11714c1d03536"
-        },
-        "date": 1703836067238,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 0.5791611287494307,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.025068299260079,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 37.78177325917567,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11698,6 +11659,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 56.38497824904961,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "d501e3b4c12d44d0aa277b99a49f64a94def7f81",
+          "message": "fix: do not delete custom bin on `add` cmd\n\nWhen the `--path` argument is specified on the node manager's `add` command, a custom binary is\nsupplied, and now, the source of `--path` is not deleted.\n\nBy default, the `add` command downloads the latest version of a binary to a temporary location, and\nthat binary is then copied to the service location. Having finished with it, the node manager then\ncleans up the temporary binary. When the `--path` argument is used, it goes through the same code\npath, and hence the source of `--path` was being deleted as a side effect. Now, a flag is used to\nindicate whether the binary should be deleted, and it is set to `false` when the `--path` argument\nis supplied.\n\nThe `--path` argument is being used when users build their own `safenode`, so they are expecting\nthat the built binary will still exist.",
+          "timestamp": "2024-04-24T18:25:21Z",
+          "tree_id": "4ac758741f86cbe8bcaeab922677d4e2a2c5cb8a",
+          "url": "https://github.com/maidsafe/safe_network/commit/d501e3b4c12d44d0aa277b99a49f64a94def7f81"
+        },
+        "date": 1713985310463,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.1748612409850292,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.6013850200708355,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 56.93680557671058,
             "unit": "MiB/s"
           }
         ]
