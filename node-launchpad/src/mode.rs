95d45c2b4a46aea0d3b6eeeedc733476236c55d6
Roland Sherwin <RolandSherwin@protonmail.com>
2024-05-09 17:10:04 +0530

    feat(launchpad): allow users to input disk space to allocate

--- node-launchpad/src/mode.rs
+++ node-launchpad/src/mode.rs
@@ -14,6 +14,7 @@ pub enum Scene {
     Home,
     Options,
     DiscordUsernameInputBox,
+    ResourceAllocationInputBox,
 }
 
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
