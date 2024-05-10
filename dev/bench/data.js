e8fb757b011e59dc8ce1e8a831de8878f18eb321
github-action-benchmark <github@users.noreply.github.com>
2024-05-10 10:53:24 +0000

    add Client memory (customSmallerIsBetter) benchmark result for dfa136d8bdfbbe98aa4267a00474b33188c97260

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715338402147,
+  "lastUpdate": 1715338403974,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20415,40 +20415,6 @@ window.BENCHMARK_DATA = {
             "username": "maqi"
           },
           "committer": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "maqi",
-            "username": "maqi"
-          },
-          "distinct": true,
-          "id": "f0f48bff0f7f392ac60a92f2dbac45463425b338",
-          "message": "fix(client): avoid deadlock during upload in case of error",
-          "timestamp": "2024-01-15T18:28:09Z",
-          "tree_id": "eecb97d46a6447a27612fabe9aa08162b2433e81",
-          "url": "https://github.com/maidsafe/safe_network/commit/f0f48bff0f7f392ac60a92f2dbac45463425b338"
-        },
-        "date": 1705345586592,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 208,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 40,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
             "email": "RolandSherwin@protonmail.com",
             "name": "RolandSherwin",
             "username": "RolandSherwin"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 82,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "dfa136d8bdfbbe98aa4267a00474b33188c97260",
+          "message": "chore(node): use proper SpendReason enum",
+          "timestamp": "2024-05-10T09:55:58Z",
+          "tree_id": "0afdc5f3d42379cce3de9b98fd1e62bb4dcb9c19",
+          "url": "https://github.com/maidsafe/safe_network/commit/dfa136d8bdfbbe98aa4267a00474b33188c97260"
+        },
+        "date": 1715338403400,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 196,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 84,
             "unit": "MB"
           }
         ]
