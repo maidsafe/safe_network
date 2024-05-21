9bcd8c76a4f7505f20041fc381a94b9e785d34a0
github-action-benchmark <github@users.noreply.github.com>
2024-05-21 16:45:01 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 938d157dac251f60cfab03c5aede29e624316d52

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716309899179,
+  "lastUpdate": 1716309901257,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "b9aaadf803c6f2deec28f1e75316ff7f91c2d73a",
-          "message": "fix(ci): update references to point to the main repo",
-          "timestamp": "2024-02-02T16:40:10Z",
-          "tree_id": "e361de3387edc8ff097e8691c03345c410580317",
-          "url": "https://github.com/maidsafe/safe_network/commit/b9aaadf803c6f2deec28f1e75316ff7f91c2d73a"
-        },
-        "date": 1706895408314,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 192,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 83,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "chriso83@protonmail.com",
             "name": "Chris O'Neil",
             "username": "jacderida"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 85,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "938d157dac251f60cfab03c5aede29e624316d52",
+          "message": "fix(cli): avoid mis-estimation due to overflow",
+          "timestamp": "2024-05-21T15:45:24Z",
+          "tree_id": "cb0cf5464c8668d97c9b2efad6e0ddc835356c5a",
+          "url": "https://github.com/maidsafe/safe_network/commit/938d157dac251f60cfab03c5aede29e624316d52"
+        },
+        "date": 1716309900611,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 205,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 86,
             "unit": "MB"
           }
         ]
