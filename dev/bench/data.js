e5760df1b0655e24969c06e960ff9f69b913862f
github-action-benchmark <github@users.noreply.github.com>
2024-05-23 09:20:09 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 4eb51bce8adb80b9aa4cd475c8bf85d14d044620

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716455878344,
+  "lastUpdate": 1716456009928,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "aadc8b4db09b26d0c23a76e9c5a8bd8c0772c5ea",
-          "message": "chore: improvements from dev feedback\n\nReduce the start up time for the local network by removing the wait in the `get_available_port`\nfunction. This function is now only being used to select ports for the RPC service, so it shouldn't\nbe subject to the same problems we had with the node when it switched over to Quic.\n\nAlso, the `--clean` argument on the run command has been extended to kill any existing local\nnetwork.",
-          "timestamp": "2024-02-08T08:11:51Z",
-          "tree_id": "d8b6bd786da80e90bde9735c0f651e0d64b1a2b5",
-          "url": "https://github.com/maidsafe/safe_network/commit/aadc8b4db09b26d0c23a76e9c5a8bd8c0772c5ea"
-        },
-        "date": 1707383416166,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 54,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 52,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "4eb51bce8adb80b9aa4cd475c8bf85d14d044620",
+          "message": "fix(manager): download again if cached archive is corrupted",
+          "timestamp": "2024-05-23T08:27:23Z",
+          "tree_id": "c7959a323946c2660c6e82179cd29d44ac1f5054",
+          "url": "https://github.com/maidsafe/safe_network/commit/4eb51bce8adb80b9aa4cd475c8bf85d14d044620"
+        },
+        "date": 1716456009635,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 70,
             "unit": "MB"
           }
         ]
