8cea71036b0f5c3a978ccf7d0a23c1bd411f2062
github-action-benchmark <github@users.noreply.github.com>
2024-05-24 01:24:59 +0000

    add Client memory (customSmallerIsBetter) benchmark result for aabb9b5fb478ca9525adc262bf258a6c02ce69a5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716513897756,
+  "lastUpdate": 1716513899472,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "gapspace@engineer.com",
-            "name": "Jason Paul",
-            "username": "JasonPaulGithub"
-          },
-          "committer": {
-            "email": "gabrielviganotti@gmail.com",
-            "name": "bochaco",
-            "username": "bochaco"
-          },
-          "distinct": true,
-          "id": "c4953b2dc5a618f0ef137f6889d979bd849fd785",
-          "message": "docs: copyright update to current year",
-          "timestamp": "2024-02-08T18:14:03Z",
-          "tree_id": "786a54288d71668b6051c2567cdaa4f5b323eb2a",
-          "url": "https://github.com/maidsafe/safe_network/commit/c4953b2dc5a618f0ef137f6889d979bd849fd785"
-        },
-        "date": 1707419000479,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 214,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 84,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "chriso83@protonmail.com",
             "name": "Chris O'Neil",
             "username": "jacderida"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 85,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "aabb9b5fb478ca9525adc262bf258a6c02ce69a5",
+          "message": "feat(nat): rename nat-detection folder + disable release for now",
+          "timestamp": "2024-05-24T10:09:21+09:00",
+          "tree_id": "5a224e6e28d1ce4eec1ded3f593961c2cc9e0ddf",
+          "url": "https://github.com/maidsafe/safe_network/commit/aabb9b5fb478ca9525adc262bf258a6c02ce69a5"
+        },
+        "date": 1716513898884,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 194,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 88,
             "unit": "MB"
           }
         ]
