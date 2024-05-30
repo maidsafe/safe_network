8f66c81fc263017e20753c252b776a922378629b
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 11:47:38 +0000

    add Client memory (customSmallerIsBetter) benchmark result for e7d34b8b6c6e1057bb3f86b509938295bc05a7f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717069657095,
+  "lastUpdate": 1717069658563,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": false,
-          "id": "74afbbe1d3987118396a3da3d1def4d2ab7d614e",
-          "message": "Revert \"ci: re-add disk space debugging\"\n\nThis reverts commit dd05850e841efb7dd870828fb754b237a2fae731.\n\nWith the newer version of `release-plz` we have about 5GB to spare at the end of the publishing\nprocess:\n```\nFilesystem      Size  Used Avail Use% Mounted on\n/dev/root        73G   68G  5.0G  94% /\ntmpfs           7.9G  172K  7.9G   1% /dev/shm\ntmpfs           3.2G  1.1M  3.2G   1% /run\ntmpfs           5.0M     0  5.0M   0% /run/lock\n/dev/sda15      105M  6.1M   99M   6% /boot/efi\n/dev/sdb1        74G  4.1G   66G   6% /mnt\ntmpfs           1.6G   12K  1.6G   1% /run/user/1001\n```",
-          "timestamp": "2024-02-12T14:36:14Z",
-          "tree_id": "0a040ecc9e50f403f1412adce18306c33e5bca5c",
-          "url": "https://github.com/maidsafe/safe_network/commit/74afbbe1d3987118396a3da3d1def4d2ab7d614e"
-        },
-        "date": 1707752193915,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 196,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 84,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 83,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "e7d34b8b6c6e1057bb3f86b509938295bc05a7f6",
+          "message": "ci: allow for feature branch releases via stable- naming",
+          "timestamp": "2024-05-30T20:33:18+09:00",
+          "tree_id": "c46ea0b5918bac7c5e8444878fc6e3215abeca99",
+          "url": "https://github.com/maidsafe/safe_network/commit/e7d34b8b6c6e1057bb3f86b509938295bc05a7f6"
+        },
+        "date": 1717069658175,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 200,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 84,
             "unit": "MB"
           }
         ]
