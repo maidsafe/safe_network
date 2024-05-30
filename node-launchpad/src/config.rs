23c0181601bf90e271bebabe2f5f0300e51cc3b3
RolandSherwin <RolandSherwin@protonmail.com>
2024-05-31 01:29:59 +0530

    feat(launchpad): keep track of the nodes to start

--- node-launchpad/src/config.rs
+++ node-launchpad/src/config.rs
@@ -19,7 +19,7 @@ const CONFIG: &str = include_str!("../.config/config.json5");
 #[derive(Clone, Debug, Deserialize, Default, Serialize)]
 pub struct AppData {
     pub discord_username: String,
-    pub allocated_disk_space: usize,
+    pub nodes_to_start: usize,
 }
 
 impl AppData {
