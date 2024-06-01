f70cb3b82ea72f1863d8a38ddafb60da78b70d47
github-action-benchmark <github@users.noreply.github.com>
2024-06-01 13:57:36 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 4c93e23c190eb0ca978a177ae57e4279a8134d6c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717250254457,
+  "lastUpdate": 1717250256218,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -30612,45 +30612,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "e846314159a8613d9ecb4f8b3ed45d34d0be53a9",
-          "message": "feat: force and upgrade by url or version\n\nThree new arguments are added to the `upgrade` command: `--force`, `--url` and `--version`.\n\nThe `--url` and `--version` arguments provide two different sources for the upgrade, as opposed to\njust upgrading to the latest version. With `--url`, a custom binary can be provided, which will be\nused for the backwards compatibility test. The `--version` flag enables upgrading to a specific\nversion rather than the latest. Both of these can be used with the `--force` flag to downgrade to an\narbitrary version or to accept an upgrade from a binary with the same version, the latter of which\nwill again be used in the backwards compatibility test.\n\nIntegration tests provide coverage of these new features. Each of the tests really needs to run on\nits own machine, otherwise they interfere with each other, and tests can't make assumptions about\nhow many services there are. So we add twelve additional jobs to the merge workflow here, which is\nfor four tests on three operating systems. However, these tests should run pretty quickly.\n\nThe `stop` command was modified such that it will no longer return an error if the service is in the\n`ADDED` state, i.e., it has not been started before. This enables us to test the upgrade process\nwithout initially starting the service, which could introduce complications. The `get_safenode_port`\nfunction was also changed to return an `Option` rather than a `Result` for the same reason.",
-          "timestamp": "2024-02-15T08:12:15Z",
-          "tree_id": "14d5adebf1d7a6f7c43ccbe573078438d693bdec",
-          "url": "https://github.com/maidsafe/safe_network/commit/e846314159a8613d9ecb4f8b3ed45d34d0be53a9"
-        },
-        "date": 1707989098228,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "swarm_driver long handling times",
-            "value": 6275,
-            "unit": "hits"
-          },
-          {
-            "name": "swarm_driver long handling total_time",
-            "value": 37403,
-            "unit": "ms"
-          },
-          {
-            "name": "swarm_driver average long handling time",
-            "value": 5,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "bzeeman@live.nl",
             "name": "Benno Zeeman",
             "username": "b-zee"
@@ -42300,6 +42261,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 31554,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 4,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "4c93e23c190eb0ca978a177ae57e4279a8134d6c",
+          "message": "chore(launchpad): fixes for white terminal theme",
+          "timestamp": "2024-06-01T12:59:50Z",
+          "tree_id": "86f91a3bda3f22f50bfe618696fa39c51880a181",
+          "url": "https://github.com/maidsafe/safe_network/commit/4c93e23c190eb0ca978a177ae57e4279a8134d6c"
+        },
+        "date": 1717250255828,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 7310,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 33235,
             "unit": "ms"
           },
           {
