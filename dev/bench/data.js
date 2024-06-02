fef3b9cee60cc139797493aeab876869aa862a67
github-action-benchmark <github@users.noreply.github.com>
2024-06-02 10:18:42 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 769914472ad5f7ef7bc435d16a904e5b0a4b8fd7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717323521213,
+  "lastUpdate": 1717323522942,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "9de1b840b4dacc1440d933b0c9767f6752c130d5",
-          "message": "test: token_distribution against network",
-          "timestamp": "2024-02-19T12:17:04+01:00",
-          "tree_id": "371767604e4c2b5c8a1f7fe04b29b284c0b3b297",
-          "url": "https://github.com/maidsafe/safe_network/commit/9de1b840b4dacc1440d933b0c9767f6752c130d5"
-        },
-        "date": 1708342434740,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 204,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 85,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "gabrielviganotti@gmail.com",
             "name": "Gabriel Viganotti",
             "username": "bochaco"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 86,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "769914472ad5f7ef7bc435d16a904e5b0a4b8fd7",
+          "message": "feat(keys): enable compile or runtime override of keys",
+          "timestamp": "2024-06-02T09:29:11Z",
+          "tree_id": "78338e3e38ae47c84b17a03b59c9b9cd42f92095",
+          "url": "https://github.com/maidsafe/safe_network/commit/769914472ad5f7ef7bc435d16a904e5b0a4b8fd7"
+        },
+        "date": 1717323522381,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 194,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 82,
             "unit": "MB"
           }
         ]
