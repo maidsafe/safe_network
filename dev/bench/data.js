5aa9159b62e9ebf5658ce962db5aa19ed6b2e7a4
github-action-benchmark <github@users.noreply.github.com>
2024-06-04 14:03:18 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 2b5759182091f16cd4f5d61700241988a7f63329

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717509586600,
+  "lastUpdate": 1717509798786,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "23d1122f64520a08e149da81ae8d47683564e3ea",
-          "message": "chore: update readme for default branch for PRs",
-          "timestamp": "2024-02-21T11:12:18+01:00",
-          "tree_id": "e84b3ff01cf651662c716ae7049068c3bebffa6b",
-          "url": "https://github.com/maidsafe/safe_network/commit/23d1122f64520a08e149da81ae8d47683564e3ea"
-        },
-        "date": 1708511262692,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 1.2590637657853365,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.8443265027939995,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 58.44135005379856,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11699,6 +11660,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 58.90411860054433,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "2b5759182091f16cd4f5d61700241988a7f63329",
+          "message": "fix(audit): dont overwrite beta tracking payments",
+          "timestamp": "2024-06-04T22:51:18+09:00",
+          "tree_id": "4ee93b24617f20884fdafd75a24855c537e670c6",
+          "url": "https://github.com/maidsafe/safe_network/commit/2b5759182091f16cd4f5d61700241988a7f63329"
+        },
+        "date": 1717509793628,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2242677466544432,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.6717706646734427,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 58.36127826182807,
             "unit": "MiB/s"
           }
         ]
