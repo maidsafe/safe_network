21bbbcfb1b103bb571083dfa4c328c6335d376c9
github-action-benchmark <github@users.noreply.github.com>
2024-06-04 21:33:52 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 7e1a730502e589ca37b6b1292c6eae2740fc459f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717536712257,
+  "lastUpdate": 1717536832775,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "anselmega@gmail.com",
-            "name": "grumbach",
-            "username": "grumbach"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "4e77d03b69ce36c023392f97930ad550f0109187",
-          "message": "feat: use the old serialisation as default, add some docs",
-          "timestamp": "2024-02-22T08:15:31Z",
-          "tree_id": "81c2d2ec319136d12adede68054e98bbd13c3fa4",
-          "url": "https://github.com/maidsafe/safe_network/commit/4e77d03b69ce36c023392f97930ad550f0109187"
-        },
-        "date": 1708592574157,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 51,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 50,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "grumbach",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "7e1a730502e589ca37b6b1292c6eae2740fc459f",
+          "message": "fix: distinguish DAG crawl waits",
+          "timestamp": "2024-06-05T06:19:56+09:00",
+          "tree_id": "594c592097f612f9c6499b40a8f63909f18ee318",
+          "url": "https://github.com/maidsafe/safe_network/commit/7e1a730502e589ca37b6b1292c6eae2740fc459f"
+        },
+        "date": 1717536831784,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 53,
             "unit": "MB"
           }
         ]
