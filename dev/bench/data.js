3e53ccfa317e26547edbded88d223511f5982022
github-action-benchmark <github@users.noreply.github.com>
2024-06-08 00:28:40 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 60422736e68c4f618cd2f056f7aaa43e459c55a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717806394556,
+  "lastUpdate": 1717806520923,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "maciej@robotix-lozinski.pl",
-            "name": "loziniak",
-            "username": "loziniak"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "fb309bf4977623196660261ccaf637e0993e05a3",
-          "message": "chore(api): make logging::Error public\n\nit's public in other crates, it makes error handling easier.",
-          "timestamp": "2024-03-11T01:15:43Z",
-          "tree_id": "0ceb828a58c2550e6b445b934f094cdc884f6c02",
-          "url": "https://github.com/maidsafe/safe_network/commit/fb309bf4977623196660261ccaf637e0993e05a3"
-        },
-        "date": 1710122383341,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 53,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "Roland Sherwin",
             "username": "RolandSherwin"
@@ -20396,6 +20367,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/maidsafe/safe_network/commit/12b0e690835e647817b2317f182659135320f40c"
         },
         "date": 1717798543175,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 53,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "60422736e68c4f618cd2f056f7aaa43e459c55a8",
+          "message": "fix(uploader): fix for mismatched ordering of instructions",
+          "timestamp": "2024-06-08T09:13:59+09:00",
+          "tree_id": "e96a511a8b4a51e023050e9c76c2100e25a64bdf",
+          "url": "https://github.com/maidsafe/safe_network/commit/60422736e68c4f618cd2f056f7aaa43e459c55a8"
+        },
+        "date": 1717806520573,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
