9c83e425b8cbdb06d142e3bc21724b87547ef953
github-action-benchmark <github@users.noreply.github.com>
2024-06-10 20:18:31 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 84156b3340dc65e015f1f1b0217713eaca3be7e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718050590988,
+  "lastUpdate": 1718050711028,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "a00d2537d9b7453fa4410a069a4d03d0f8969b3b",
-          "message": "ci: remove release schedule for now",
-          "timestamp": "2024-03-13T03:37:01+01:00",
-          "tree_id": "8f0b20430725903ff18d87854819084d3e8b5c09",
-          "url": "https://github.com/maidsafe/safe_network/commit/a00d2537d9b7453fa4410a069a4d03d0f8969b3b"
-        },
-        "date": 1710298204013,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 53,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "Roland Sherwin",
             "username": "RolandSherwin"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 51,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "distinct": true,
+          "id": "84156b3340dc65e015f1f1b0217713eaca3be7e3",
+          "message": "feat(auditor): recognize payment forwards with default key",
+          "timestamp": "2024-06-10T19:26:00Z",
+          "tree_id": "34258f2a9c7a38fe2541fe16976172a33f8a6afc",
+          "url": "https://github.com/maidsafe/safe_network/commit/84156b3340dc65e015f1f1b0217713eaca3be7e3"
+        },
+        "date": 1718050710406,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 54,
             "unit": "MB"
           }
         ]
