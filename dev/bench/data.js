a2e7e8a4246cb27670159bff5775a2e4ddcc32dc
github-action-benchmark <github@users.noreply.github.com>
2024-06-17 12:55:45 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 22ae1147d594906808903e280755ecea593bc6d5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718628943692,
+  "lastUpdate": 1718628945050,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "75967394ebaed6ff2963934f95bf6c7bab3f42dd",
-          "message": "fix(node): get_closest error not trigger bad_node report",
-          "timestamp": "2024-03-18T10:55:00Z",
-          "tree_id": "69d6e87ea96a3fe95d150c6d0c463159f0d467af",
-          "url": "https://github.com/maidsafe/safe_network/commit/75967394ebaed6ff2963934f95bf6c7bab3f42dd"
-        },
-        "date": 1710763057611,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 186,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 84,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "Roland Sherwin",
             "username": "RolandSherwin"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 86,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno Zeeman",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "22ae1147d594906808903e280755ecea593bc6d5",
+          "message": "refactor: fix manual_unwrap_or_default\n\nclippy lint in nightly",
+          "timestamp": "2024-06-17T12:04:26Z",
+          "tree_id": "af8dccc2edda4a7a768e547ebe525f7de641a5cf",
+          "url": "https://github.com/maidsafe/safe_network/commit/22ae1147d594906808903e280755ecea593bc6d5"
+        },
+        "date": 1718628944756,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 175,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 87,
             "unit": "MB"
           }
         ]
