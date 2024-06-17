b71c026b7b6148b84a5986b8fb293c22db4394cc
github-action-benchmark <github@users.noreply.github.com>
2024-06-17 09:25:50 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 7bfeffd0624fb4bfacc2d644df5643ae9b640ec4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718616347934,
+  "lastUpdate": 1718616350301,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -30612,45 +30612,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "e4646093c7393886bc71de14dc7bd5c774cd08bb",
-          "message": "feat: provide `faucet upgrade` command\n\nUses the generic service manager to provide a `faucet upgrade` command, which will be used during\nthe general upgrade process to either upgrade or downgrade the faucet.\n\nSome code was common between the node and faucet upgrades, so that was refactored to be shared\nbetween the two.\n\nAlso clarifies some of the language used on the command documentation.",
-          "timestamp": "2024-03-14T23:56:52Z",
-          "tree_id": "13b8597d102c3f071f352c50cdc2d0e010929320",
-          "url": "https://github.com/maidsafe/safe_network/commit/e4646093c7393886bc71de14dc7bd5c774cd08bb"
-        },
-        "date": 1710463444306,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "swarm_driver long handling times",
-            "value": 6333,
-            "unit": "hits"
-          },
-          {
-            "name": "swarm_driver long handling total_time",
-            "value": 36698,
-            "unit": "ms"
-          },
-          {
-            "name": "swarm_driver average long handling time",
-            "value": 5,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "maciej@robotix-lozinski.pl",
             "name": "loziniak",
             "username": "loziniak"
@@ -42300,6 +42261,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 33974,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 4,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno Zeeman",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno",
+            "username": "b-zee"
+          },
+          "distinct": true,
+          "id": "7bfeffd0624fb4bfacc2d644df5643ae9b640ec4",
+          "message": "style(protocol): ignore clone_on_ref_ptr",
+          "timestamp": "2024-06-17T08:27:18Z",
+          "tree_id": "623d6579ec2c195664dd591e79e927095beb72a7",
+          "url": "https://github.com/maidsafe/safe_network/commit/7bfeffd0624fb4bfacc2d644df5643ae9b640ec4"
+        },
+        "date": 1718616349533,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 7385,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 32871,
             "unit": "ms"
           },
           {
