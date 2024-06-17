d8449d99336f97b5f8ffb144ba06f21e3371bc78
github-action-benchmark <github@users.noreply.github.com>
2024-06-17 05:57:28 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 13a6d8aecba82298e14cde80ca36270e833675e2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718586426067,
+  "lastUpdate": 1718603848256,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "gapspace@engineer.com",
-            "name": "Jason Paul",
-            "username": "JasonPaulGithub"
-          },
-          "distinct": true,
-          "id": "fb9191c1d1b431e080890b4049a5e99b19308be1",
-          "message": "fix(test): make tweaks to wan nightly tests",
-          "timestamp": "2024-03-14T14:29:06Z",
-          "tree_id": "e63a2351c243c4ff09fbe4766a38095bcc38056e",
-          "url": "https://github.com/maidsafe/safe_network/commit/fb9191c1d1b431e080890b4049a5e99b19308be1"
-        },
-        "date": 1710429453222,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 1.2849923042337745,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.8174312524657188,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 58.09354473489316,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11699,6 +11660,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 57.91142437925475,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "13a6d8aecba82298e14cde80ca36270e833675e2",
+          "message": "fix(networking): use default Relay config from libp2p\n\nThis is much more conservative and should help relay load on machines",
+          "timestamp": "2024-06-17T05:13:04Z",
+          "tree_id": "5722bbcea829b74bcfd914c9b51f451808809921",
+          "url": "https://github.com/maidsafe/safe_network/commit/13a6d8aecba82298e14cde80ca36270e833675e2"
+        },
+        "date": 1718603842853,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.232528579912306,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.6765401863491836,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 59.319896325284695,
             "unit": "MiB/s"
           }
         ]
