6d85e8f8b2d92e1fe2e6844cdbf610306cef0fff
github-action-benchmark <github@users.noreply.github.com>
2024-06-17 12:54:35 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 5da8ca2118dce01c9d1539009c5f2123e1a2b889

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718628873144,
+  "lastUpdate": 1718628874967,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "75e125675b95107781135311479bc94ba4c56550",
-          "message": "feat: support a port range on the `add` command\n\nPreviously we would not allow a custom port to be specified if more than one service was being\nadded. However, without autonat, people still need to open ports manually on their router, so the\nnode services they add will need to match this port range. Without the ability to specify a range,\nthey'd need to add the services one by one.",
-          "timestamp": "2024-03-18T02:14:02Z",
-          "tree_id": "d29caf619ed61b6ab5a7b61fb21d65f10339c28a",
-          "url": "https://github.com/maidsafe/safe_network/commit/75e125675b95107781135311479bc94ba4c56550"
-        },
-        "date": 1710730805255,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 193,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 86,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -30598,6 +30564,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory usage w/ upload",
             "value": 188,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 86,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno Zeeman",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "5da8ca2118dce01c9d1539009c5f2123e1a2b889",
+          "message": "refactor: fix nonexisting feature\n\nThe `wasm32` is not a feature. It was probably supposed to be the target architecture.",
+          "timestamp": "2024-06-17T12:04:03Z",
+          "tree_id": "9dd60227f7abb3242d43ad574581d2922cf8ebd3",
+          "url": "https://github.com/maidsafe/safe_network/commit/5da8ca2118dce01c9d1539009c5f2123e1a2b889"
+        },
+        "date": 1718628874446,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 198,
             "unit": "MB"
           },
           {
