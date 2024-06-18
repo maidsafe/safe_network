c8afe1da6df34a0a3deb696fc8739f1cda8b7bc9
github-action-benchmark <github@users.noreply.github.com>
2024-06-18 06:36:05 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 5ff2218ad0aec5f2f33665e11fb545c4d7fea266

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718692563043,
+  "lastUpdate": 1718692565853,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -30612,45 +30612,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": false,
-          "id": "421b42ef26525f7520ed8846f5cb85fd03d23b27",
-          "message": "refactor: remove churn example from node manager",
-          "timestamp": "2024-03-19T00:31:49Z",
-          "tree_id": "4d47c5becec325f4f42c3150a8a12316ecafa631",
-          "url": "https://github.com/maidsafe/safe_network/commit/421b42ef26525f7520ed8846f5cb85fd03d23b27"
-        },
-        "date": 1710810980157,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "swarm_driver long handling times",
-            "value": 6396,
-            "unit": "hits"
-          },
-          {
-            "name": "swarm_driver long handling total_time",
-            "value": 37313,
-            "unit": "ms"
-          },
-          {
-            "name": "swarm_driver average long handling time",
-            "value": 5,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "gabrielviganotti@gmail.com",
             "name": "Gabriel Viganotti",
             "username": "bochaco"
@@ -42300,6 +42261,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 33782,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 4,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "5ff2218ad0aec5f2f33665e11fb545c4d7fea266",
+          "message": "chore(network): disable adding relay candidates for clients",
+          "timestamp": "2024-06-18T05:49:40Z",
+          "tree_id": "b72d180e7eb4c9b148da44bae42fc24c3187b2eb",
+          "url": "https://github.com/maidsafe/safe_network/commit/5ff2218ad0aec5f2f33665e11fb545c4d7fea266"
+        },
+        "date": 1718692564786,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 7240,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 31677,
             "unit": "ms"
           },
           {
