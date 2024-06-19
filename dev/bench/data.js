a9b4bae23e5ee147a7ef72dfa5552fc34820f76a
github-action-benchmark <github@users.noreply.github.com>
2024-06-19 07:34:28 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 380765a80844b5dd43788c5eb0aba1910db539e8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718776555486,
+  "lastUpdate": 1718782468549,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "chris.oneil@gmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "distinct": true,
-          "id": "329d326a0bb018d013572cb1ad71c8049e26f2f1",
-          "message": "chore: run `safenodemand` service as root\n\nCreating a user for running the `safenodemand` service doesn't make as much sense because the user\nmust have root access. We can leave it up to the user to create an account and set it up for\npasswordless sudo, then provide an optional `--user` argument. For now we don't have this, and just\ngo with root.",
-          "timestamp": "2024-03-19T20:55:22Z",
-          "tree_id": "fdab391fd92f016c55d0ab915dd795602eb8f4a8",
-          "url": "https://github.com/maidsafe/safe_network/commit/329d326a0bb018d013572cb1ad71c8049e26f2f1"
-        },
-        "date": 1710882540883,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 1.3129254269758266,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.8467536583204311,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 58.86358335310985,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11699,6 +11660,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 58.62196140825606,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "380765a80844b5dd43788c5eb0aba1910db539e8",
+          "message": "chore: update naming for clarity around replication",
+          "timestamp": "2024-06-19T06:49:38Z",
+          "tree_id": "a36f8b1541d8caae2d31b356a8b585bd4245c0d8",
+          "url": "https://github.com/maidsafe/safe_network/commit/380765a80844b5dd43788c5eb0aba1910db539e8"
+        },
+        "date": 1718782463005,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2163350188051807,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.6852612449927136,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 59.060964545681145,
             "unit": "MiB/s"
           }
         ]
