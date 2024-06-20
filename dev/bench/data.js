2254c313714e9b312fdaef82015d282b6d723602
github-action-benchmark <github@users.noreply.github.com>
2024-06-20 08:54:25 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 02310626666d28768cf3546b41a88c87340d5dbc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718873663035,
+  "lastUpdate": 1718873665076,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -30612,45 +30612,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "gabrielviganotti@gmail.com",
-            "name": "Gabriel Viganotti",
-            "username": "bochaco"
-          },
-          "committer": {
-            "email": "gabrielviganotti@gmail.com",
-            "name": "bochaco",
-            "username": "bochaco"
-          },
-          "distinct": true,
-          "id": "56a70313a4a53f8ad07de1e1f76f14069021465d",
-          "message": "feat(cli): have CLI folders cmds to act on current directory by default",
-          "timestamp": "2024-03-20T17:22:17Z",
-          "tree_id": "da251f5611495bcfedc0e963b2d9471f3cbbf9ca",
-          "url": "https://github.com/maidsafe/safe_network/commit/56a70313a4a53f8ad07de1e1f76f14069021465d"
-        },
-        "date": 1710958443966,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "swarm_driver long handling times",
-            "value": 7201,
-            "unit": "hits"
-          },
-          {
-            "name": "swarm_driver long handling total_time",
-            "value": 41088,
-            "unit": "ms"
-          },
-          {
-            "name": "swarm_driver average long handling time",
-            "value": 5,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -42300,6 +42261,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 46623,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 4,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno Zeeman",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "02310626666d28768cf3546b41a88c87340d5dbc",
+          "message": "style(auditor): add workspace lints to auditor",
+          "timestamp": "2024-06-20T08:00:15Z",
+          "tree_id": "194b44e8fd03600b26dc5c24291cc92f020c9cff",
+          "url": "https://github.com/maidsafe/safe_network/commit/02310626666d28768cf3546b41a88c87340d5dbc"
+        },
+        "date": 1718873664423,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 8069,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 37262,
             "unit": "ms"
           },
           {
