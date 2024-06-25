d610f922c8797016a7bde3e83fdabd4661975b0d
github-action-benchmark <github@users.noreply.github.com>
2024-06-25 13:05:06 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 489df9f63605b9c362cf42e2c87fdbed9553fc6d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719320585916,
+  "lastUpdate": 1719320706901,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "rmctiernan@protonmail.com",
-            "name": "RMcTn",
-            "username": "RMcTn"
-          },
-          "committer": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "distinct": true,
-          "id": "f9acc4ed69e64ca79a005836428b824563130c66",
-          "message": "docs: fix wrong command in usage example",
-          "timestamp": "2024-03-24T12:17:47Z",
-          "tree_id": "1698379e4d1b8887410827a87c3e5b34bf964d30",
-          "url": "https://github.com/maidsafe/safe_network/commit/f9acc4ed69e64ca79a005836428b824563130c66"
-        },
-        "date": 1711285837936,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 53,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "Roland Sherwin",
             "username": "RolandSherwin"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 54,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": false,
+          "id": "489df9f63605b9c362cf42e2c87fdbed9553fc6d",
+          "message": "feat: add script to compare two dags user lists",
+          "timestamp": "2024-06-25T12:18:25Z",
+          "tree_id": "efb3bb8c8e725b335f4386595cc8b2796f0399fa",
+          "url": "https://github.com/maidsafe/safe_network/commit/489df9f63605b9c362cf42e2c87fdbed9553fc6d"
+        },
+        "date": 1719320706361,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 51,
             "unit": "MB"
           }
         ]
