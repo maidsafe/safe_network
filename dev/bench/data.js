f0ba6e90a84f761ed5697631f8ae46c05b9e2352
github-action-benchmark <github@users.noreply.github.com>
2024-06-25 07:01:10 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 8de2dbd52a43233276983b4281a48bad7b5ac5ed

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719298747903,
+  "lastUpdate": 1719298870701,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "859fa367839ec88a59692aa9e0ea245270379796",
-          "message": "fix: attach binaries to node manager release\n\nThe crate name was specified as `sn_node_manager` rather than `sn-node-manager`.\n\nAlso disable the `sn_service_management` crate from being released on Github. At the same time I\narranged the crates in the config file alphabetically, just because it makes them a bit easier to\nfind.",
-          "timestamp": "2024-03-22T20:45:03Z",
-          "tree_id": "f755a9c36ecd60467aa7f1fccde5d934267ee790",
-          "url": "https://github.com/maidsafe/safe_network/commit/859fa367839ec88a59692aa9e0ea245270379796"
-        },
-        "date": 1711143531689,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 57,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "gabrielviganotti@gmail.com",
             "name": "Gabriel Viganotti",
             "username": "bochaco"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 54,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman@live.nl",
+            "name": "Benno Zeeman",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "8de2dbd52a43233276983b4281a48bad7b5ac5ed",
+          "message": "docs(client): remove api calls chapter\n\nPrefer the docs that are generated with `///`. (docs.rs)\n\nCloses #751",
+          "timestamp": "2024-06-25T06:14:57Z",
+          "tree_id": "633dcd8778dbaf6c4eec06bc678dda0faec181b7",
+          "url": "https://github.com/maidsafe/safe_network/commit/8de2dbd52a43233276983b4281a48bad7b5ac5ed"
+        },
+        "date": 1719298870162,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 50,
             "unit": "MB"
           }
         ]
