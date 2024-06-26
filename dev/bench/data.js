cd66b968a47d4e2cdcf71c176d823149e29e61b3
github-action-benchmark <github@users.noreply.github.com>
2024-06-26 15:08:03 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for bf6695aeb284bb28f3a6ea26bb1800f0dd4fb44c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719414481706,
+  "lastUpdate": 1719414483626,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -30612,45 +30612,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "9a32e28ae10636842c374187155b6d332e3b13f0",
-          "message": "chore(node): optimization to reduce logging",
-          "timestamp": "2024-03-25T12:09:53Z",
-          "tree_id": "0d06f968f7da63f6b058e9de32d4a1ae922a09fd",
-          "url": "https://github.com/maidsafe/safe_network/commit/9a32e28ae10636842c374187155b6d332e3b13f0"
-        },
-        "date": 1711371539278,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "swarm_driver long handling times",
-            "value": 6927,
-            "unit": "hits"
-          },
-          {
-            "name": "swarm_driver long handling total_time",
-            "value": 38953,
-            "unit": "ms"
-          },
-          {
-            "name": "swarm_driver average long handling time",
-            "value": 5,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -42305,6 +42266,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver average long handling time",
             "value": 5,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "Roland Sherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "distinct": false,
+          "id": "bf6695aeb284bb28f3a6ea26bb1800f0dd4fb44c",
+          "message": "fix(launchpad): make sure the correct primary path is used\n- when dealing with unix, every mount point would start with '/', so\ninstead find the point that ends with '/'. This would be our primary\npartition.",
+          "timestamp": "2024-06-26T14:10:11Z",
+          "tree_id": "936529995a1855c53531adc957ed6e547ef62d16",
+          "url": "https://github.com/maidsafe/safe_network/commit/bf6695aeb284bb28f3a6ea26bb1800f0dd4fb44c"
+        },
+        "date": 1719414483271,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 6252,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 29455,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 4,
             "unit": "ms"
           }
         ]
