b39dd6a23eceee2dc0e7edca8ad710fcd912ce73
Warm Beer <mickvd99@gmail.com>
2024-07-08 15:57:40 +0200

    chore(wallet): added more comments

--- sn_transfers/src/wallet/encryption.rs
+++ sn_transfers/src/wallet/encryption.rs
@@ -20,7 +20,7 @@ use std::num::NonZeroU32;
 use std::path::Path;
 
 /// Number of iterations for pbkdf2.
-const ITERATIONS: u32 = 100_000; // should always be > 0.
+const ITERATIONS: u32 = 100_000; // Should always be > 0
 /// Filename for the encrypted secret key.
 const ENCRYPTED_MAIN_SECRET_KEY_FILENAME: &str = "main_secret_key.encrypted";
 
@@ -135,14 +135,21 @@ pub(crate) fn encrypt_secret_key(
     secret_key: &MainSecretKey,
     password: &str,
 ) -> Result<EncryptedSecretKey> {
+    // Generate a random salt
+    // Salt is used to ensure unique derived keys even for identical passwords
     let mut salt = [0u8; 8];
     rand::thread_rng().fill(&mut salt);
 
+    // Generate a random nonce
+    // Nonce is used to ensure unique encryption outputs even for identical inputs
     let mut nonce = [0u8; 12];
     rand::thread_rng().fill(&mut nonce);
 
     let mut key = [0; 32];
 
+    // Derive a key from the password using PBKDF2 with HMAC<Sha512>
+    // PBKDF2 is used for key derivation to mitigate brute-force attacks by making key derivation computationally expensive
+    // HMAC<Sha512> is used as the pseudorandom function for its security properties
     ring::pbkdf2::derive(
         ring::pbkdf2::PBKDF2_HMAC_SHA512,
         NonZeroU32::new(ITERATIONS).unwrap(),
@@ -151,18 +158,25 @@ pub(crate) fn encrypt_secret_key(
         &mut key,
     );
 
+    // Create an unbound key using CHACHA20_POLY1305 algorithm
+    // CHACHA20_POLY1305 is a fast and secure AEAD (Authenticated Encryption with Associated Data) algorithm
     let unbound_key = ring::aead::UnboundKey::new(&ring::aead::CHACHA20_POLY1305, &key)
         .map_err(|_| Error::FailedToEncryptKey(String::from("Could not create unbound key.")))?;
 
+    // Create a sealing key with the unbound key and nonce
     let mut sealing_key = ring::aead::SealingKey::new(unbound_key, NonceSeq(nonce));
     let aad = ring::aead::Aad::from(&[]);
+
+    // Convert the secret key to bytes
     let secret_key_bytes = secret_key.to_bytes();
     let mut encrypted_secret_key = secret_key_bytes;
 
+    // seal_in_place_append_tag encrypts the data and appends an authentication tag to ensure data integrity
     sealing_key
         .seal_in_place_append_tag(aad, &mut encrypted_secret_key)
         .map_err(|_| Error::FailedToEncryptKey(String::from("Could not seal sealing key.")))?;
 
+    // Return the encrypted secret key along with salt and nonce encoded as hex strings
     Ok(EncryptedSecretKey {
         encrypted_secret_key: encode(encrypted_secret_key),
         salt: encode(salt),
