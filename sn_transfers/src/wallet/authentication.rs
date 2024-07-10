d2c537be0df0b00ad754bf108861bd641d4b30c8
Warm Beer <mickvd99@gmail.com>
2024-07-10 11:42:56 +0200

    chore(wallet): add wallet authentication function to CLI

--- sn_transfers/src/wallet/authentication.rs
+++ sn_transfers/src/wallet/authentication.rs
@@ -4,7 +4,7 @@ use chrono::{DateTime, Duration, Utc};
 use secrecy::{ExposeSecret, Secret};
 use std::path::PathBuf;
 
-/// Time before the user has to provide the password again
+/// Time (in seconds) before the user has to provide the password again for an encrypted wallet
 const PASSWORD_EXPIRATION_TIME_SECS: i64 = 120;
 
 /// Manager that makes it easier to interact with encrypted wallets
@@ -28,40 +28,52 @@ impl AuthenticationManager {
         }
     }
 
-    pub fn get_password(&self) -> Result<String> {
-        if let (Some(password), Some(expiration_time)) = (&self.password, self.password_expires_at)
-        {
-            if Utc::now() <= expiration_time {
-                Ok(password.expose_secret().to_owned())
-            } else {
-                Err(Error::HotWalletPasswordExpired)
-            }
-        } else {
-            Err(Error::HotWalletPasswordRequired)
-        }
+    pub fn set_password(&mut self, password: String) -> Result<()> {
+        self.verify_password(&password)?;
+        self.password = Some(Secret::new(password));
+        self.reset_password_expiration_time();
+        Ok(())
     }
 
-    pub fn set_password(&mut self, password: String) -> Result<()> {
+    fn verify_password(&self, password: &str) -> Result<()> {
         let encrypted_secret_key = EncryptedSecretKey::from_file(self.wallet_dir.as_path())?;
+        // Check if password is correct by trying to decrypt
+        encrypted_secret_key.decrypt(password)?;
+        Ok(())
+    }
 
-        // Check if password is correct
-        encrypted_secret_key.decrypt(&password)?;
-
-        self.password = Some(Secret::new(password));
+    fn reset_password_expiration_time(&mut self) {
         self.password_expires_at =
             Some(Utc::now() + Duration::seconds(PASSWORD_EXPIRATION_TIME_SECS));
-
-        Ok(())
     }
 
     /// Returns password if wallet is encrypted.
     /// Is None if wallet is not encrypted.
     /// Is Some if wallet is encrypted and password is available.
     /// Fails if wallet is encrypted, but no password available. In that case, the password needs to be set using `set_password()`.
-    pub fn authenticate(&self) -> Result<Option<String>> {
+    pub fn authenticate(&mut self) -> Result<Option<String>> {
+        // If wallet is encrypted, require a valid password
         if EncryptedSecretKey::file_exists(self.wallet_dir.as_path()) {
-            self.get_password().map(Some)
+            // Check if a password is set
+            if let (Some(password), Some(expiration_time)) =
+                (&self.password.to_owned(), self.password_expires_at)
+            {
+                // Check if password hasn't expired
+                if Utc::now() <= expiration_time {
+                    // Renew password expiration time after authenticating
+                    self.reset_password_expiration_time();
+                    Ok(Some(password.expose_secret().to_owned()))
+                } else {
+                    // Password is no longer active.
+                    // Password needs to be set again using `set_password()`
+                    Err(Error::WalletPasswordExpired)
+                }
+            } else {
+                // Password needs to be set using `set_password()`
+                Err(Error::WalletPasswordRequired)
+            }
         } else {
+            // Wallet is not encrypted
             Ok(None)
         }
     }
