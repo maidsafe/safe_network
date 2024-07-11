67ab2a1aa4b235a970166466a891e0f28f7b017f
github-action-benchmark <github@users.noreply.github.com>
2024-07-11 06:08:26 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 42e9d6c62c9c9836644e3cbf3981eca2710caeb1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720677984634,
+  "lastUpdate": 1720678106360,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11718,35 +11718,6 @@ window.BENCHMARK_DATA = {
             "username": "RolandSherwin"
           },
           "distinct": true,
-          "id": "c49ff0e0561b8ee607c3b6ad457f488bd81338ae",
-          "message": "fix(uploader): clarify the use of root and wallet dirs",
-          "timestamp": "2024-03-27T21:55:16Z",
-          "tree_id": "2e361fb570c0a61a5868571eff4d2c9aeede1b9a",
-          "url": "https://github.com/maidsafe/safe_network/commit/c49ff0e0561b8ee607c3b6ad457f488bd81338ae"
-        },
-        "date": 1711578999782,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 49,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
           "id": "fa158d5ea0112b5f2884913ca8914b97ae8ff239",
           "message": "fix(cli): read from cache during initial chunking process",
           "timestamp": "2024-03-28T10:03:51Z",
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 51,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anselmega@gmail.com",
+            "name": "grumbach",
+            "username": "grumbach"
+          },
+          "committer": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "distinct": true,
+          "id": "42e9d6c62c9c9836644e3cbf3981eca2710caeb1",
+          "message": "fix: double spend ignored when parent is double spent",
+          "timestamp": "2024-07-11T05:17:12Z",
+          "tree_id": "56fb0bd0049d386f8e35bdc64f4fed83465b1706",
+          "url": "https://github.com/maidsafe/safe_network/commit/42e9d6c62c9c9836644e3cbf3981eca2710caeb1"
+        },
+        "date": 1720678105859,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 58,
             "unit": "MB"
           }
         ]
