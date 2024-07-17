b97c134de44470449fa9e2bb31e547b6ef0077e3
github-action-benchmark <github@users.noreply.github.com>
2024-07-17 01:06:23 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 7a16c14a31b0e4847e1a24aad03455c0050e9ace

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721178381343,
+  "lastUpdate": 1721178383423,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "cbc0e73307ad76683e3841d2c1229e27aa41be29",
-          "message": "fix(client): do not check quote expiry from the client",
-          "timestamp": "2024-03-31T22:08:49Z",
-          "tree_id": "92f7e8eb069d9739f05c6afbdc3e1bca9cad240f",
-          "url": "https://github.com/maidsafe/safe_network/commit/cbc0e73307ad76683e3841d2c1229e27aa41be29"
-        },
-        "date": 1711926052852,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 190,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 84,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 82,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "qima",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "7a16c14a31b0e4847e1a24aad03455c0050e9ace",
+          "message": "feat(auditor): dump fetched spends to local disk",
+          "timestamp": "2024-07-17T00:15:21Z",
+          "tree_id": "df587f4ddf412d166f582d82191fbfd6965eee2b",
+          "url": "https://github.com/maidsafe/safe_network/commit/7a16c14a31b0e4847e1a24aad03455c0050e9ace"
+        },
+        "date": 1721178382685,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 200,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 83,
             "unit": "MB"
           }
         ]
