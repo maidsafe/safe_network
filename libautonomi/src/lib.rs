c72c28fd74f474335ffe9d8c398a55c29f92544c
Benno Zeeman <bzeeman@live.nl>
2024-07-19 10:15:06 +0200

    feat(autonomi): client API init

---
+++ libautonomi/src/lib.rs
@@ -0,0 +1,202 @@
+pub use client::Client;
+/// (Re-export for convenience.)
+pub use libp2p::{multiaddr, Multiaddr};
+
+mod self_encryption;
+
+mod client {
+    use std::{collections::HashSet, time::Duration};
+
+    use bytes::Bytes;
+    use libp2p::{identity::Keypair, Multiaddr};
+    use sn_client::networking::{
+        multiaddr_is_global, version::IDENTIFY_PROTOCOL_STR, Network, NetworkBuilder, NetworkEvent,
+        CLOSE_GROUP_SIZE,
+    };
+    use sn_protocol::NetworkAddress;
+    use tokio::{sync::mpsc::Receiver, time::interval};
+
+    use crate::self_encryption::encrypt;
+
+    #[derive(Debug, thiserror::Error)]
+    #[error(transparent)]
+    pub struct PutError(#[from] crate::self_encryption::Error);
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum ConnectError {
+        #[error("Could not connect to peers due to incompatible protocol: {0:?}")]
+        TimedOutWithIncompatibleProtocol(HashSet<String>, String),
+        #[error("Could not connect to enough peers in time.")]
+        TimedOut,
+    }
+
+    #[derive(Clone)]
+    pub struct Client {
+        network: Network,
+    }
+
+    // /// Type of events broadcasted by the client to the public API.
+    // #[derive(Clone, Debug)]
+    // pub enum Event {
+    //     /// A peer has been added to the Routing table.
+    //     /// Also contains the max number of peers to connect to before we receive ClientEvent::ConnectedToNetwork
+    //     PeerAdded { max_peers_to_connect: usize },
+    //     /// We've encountered a Peer with an unsupported protocol.
+    //     PeerWithUnsupportedProtocol {
+    //         our_protocol: String,
+    //         their_protocol: String,
+    //     },
+    //     /// The client has been connected to the network
+    //     ConnectedToNetwork,
+    //     /// No network activity has been received for a given duration
+    //     /// we should error out
+    //     InactiveClient(tokio::time::Duration),
+    // }
+
+    impl Client {
+        /// ```no_run
+        /// # use libautonomi::Client;
+        /// let client = Client::connect(&["/ip4/127.0.0.1/udp/12000/quic".parse().unwrap()]);
+        /// ```
+        pub async fn connect(peers: &[Multiaddr]) -> Result<Self, ConnectError> {
+            // Any global address makes the client non-local
+            let local = !peers.iter().any(multiaddr_is_global);
+
+            let (network, event_receiver) = build_client_and_run_swarm(local);
+
+            // Spawn task to dial to the given peers
+            let network_clone = network.clone();
+            let peers = peers.to_vec();
+            let _handle = tokio::spawn(async move {
+                for addr in peers {
+                    if let Err(err) = network_clone.dial(addr.clone()).await {
+                        eprintln!("addr={addr} Failed to dial: {err:?}");
+                    };
+                }
+            });
+
+            let (sender, receiver) = tokio::sync::oneshot::channel();
+            tokio::spawn(handle_event_receiver(event_receiver, sender));
+
+            receiver.await.expect("sender should not close")?;
+
+            Ok(Self { network })
+        }
+
+        pub async fn put(&self, data: Bytes) -> Result<(), PutError> {
+            let (map, _chunks) = encrypt(data)?;
+
+            let addr = NetworkAddress::from_chunk_address(*map.address());
+
+            let cost = self
+                .network
+                .get_store_costs_from_network(addr, vec![])
+                .await
+                .expect("get store cost");
+
+            tracing::info!("cost: {cost:?}");
+
+            Ok(())
+        }
+    }
+
+    fn build_client_and_run_swarm(local: bool) -> (Network, Receiver<NetworkEvent>) {
+        // TODO: `root_dir` is only used for nodes. `NetworkBuilder` should not require it.
+        let root_dir = std::env::temp_dir();
+        let network_builder = NetworkBuilder::new(Keypair::generate_ed25519(), local, root_dir);
+
+        // TODO: Re-export `Receiver<T>` from `sn_networking`. Else users need to keep their `tokio` dependency in sync.
+        // TODO: Think about handling the mDNS error here.
+        let (network, event_receiver, swarm_driver) =
+            network_builder.build_client().expect("mdns to succeed");
+
+        let _swarm_driver = tokio::spawn(swarm_driver.run());
+
+        (network, event_receiver)
+    }
+
+    async fn handle_event_receiver(
+        mut event_receiver: Receiver<NetworkEvent>,
+        sender: tokio::sync::oneshot::Sender<Result<(), ConnectError>>,
+    ) {
+        // We switch this to `None` when we've sent the oneshot 'connect' result.
+        let mut sender = Some(sender);
+        let mut unsupported_protocols = vec![];
+
+        let mut timeout_timer = interval(Duration::from_secs(20));
+        timeout_timer.tick().await;
+
+        loop {
+            tokio::select! {
+                _ = timeout_timer.tick() =>  {
+                    if let Some(sender) = sender.take() {
+                        if unsupported_protocols.len() > 1 {
+                            let protocols: HashSet<String> =
+                                unsupported_protocols.iter().cloned().collect();
+                            sender
+                                .send(Err(ConnectError::TimedOutWithIncompatibleProtocol(
+                                    protocols,
+                                    IDENTIFY_PROTOCOL_STR.to_string(),
+                                )))
+                                .expect("receiver should not close");
+                        } else {
+                            sender
+                                .send(Err(ConnectError::TimedOut))
+                                .expect("receiver should not close");
+                        }
+                    }
+                }
+                event = event_receiver.recv() => {
+                    let event = event.expect("receiver should not close");
+                    tracing::trace!("Handling event: {event:?}");
+                    match event {
+                        NetworkEvent::PeerAdded(_peer_id, peers_len) => {
+                            tracing::debug!("Peer added: {peers_len} in routing table");
+
+                            if peers_len >= CLOSE_GROUP_SIZE {
+                                if let Some(sender) = sender.take() {
+                                    sender.send(Ok(())).expect("receiver should not close");
+                                }
+                            }
+                        }
+                        NetworkEvent::PeerWithUnsupportedProtocol { their_protocol, .. } => {
+                            tracing::warn!(their_protocol, "Peer with unsupported protocol");
+
+                            if sender.is_some() {
+                                unsupported_protocols.push(their_protocol);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        // TODO: Handle closing of network events sender
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_client_new() {
+            let _ = tracing_subscriber::fmt()
+                .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+                .try_init();
+
+            let peers: Vec<Multiaddr> = vec!["/ip4/127.0.0.1/udp/46910/quic-v1"]
+                .into_iter()
+                .map(|addr| addr.parse().expect("valid multiaddr"))
+                .collect();
+
+            let client = Client::connect(&peers).await.unwrap();
+
+            client.put(b"Hello, world!".to_vec().into()).await.unwrap();
+
+            // while let Some(event) = client.event_receiver.recv().await {
+            //     println!("Received event: {event:?}");
+            // }
+        }
+    }
+}
---
+++ libautonomi/src/self_encryption.rs
@@ -0,0 +1,88 @@
+// Copyright 2024 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
+use bytes::{BufMut, Bytes, BytesMut};
+use self_encryption::{DataMap, MAX_CHUNK_SIZE};
+use serde::{Deserialize, Serialize};
+use sn_protocol::storage::Chunk;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Encoding(#[from] rmp_serde::encode::Error),
+    #[error(transparent)]
+    SelfEncryption(#[from] self_encryption::Error),
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) enum DataMapLevel {
+    // Holds the data map to the source data.
+    First(DataMap),
+    // Holds the data map of an _additional_ level of chunks
+    // resulting from chunking up a previous level data map.
+    // This happens when that previous level data map was too big to fit in a chunk itself.
+    Additional(DataMap),
+}
+
+pub(crate) fn encrypt(data: Bytes) -> Result<(Chunk, Vec<Chunk>), Error> {
+    let (data_map, chunks) = self_encryption::encrypt(data)?;
+    let (data_map_chunk, additional_chunks) = pack_data_map(data_map)?;
+
+    // Transform `EncryptedChunk` into `Chunk`
+    let chunks: Vec<Chunk> = chunks
+        .into_iter()
+        .map(|c| Chunk::new(c.content))
+        .chain(additional_chunks)
+        .collect();
+
+    Ok((data_map_chunk, chunks))
+}
+
+// Produces a chunk out of the first `DataMap`, which is validated for its size.
+// If the chunk is too big, it is self-encrypted and the resulting (additional level) `DataMap` is put into a chunk.
+// The above step is repeated as many times as required until the chunk size is valid.
+// In other words: If the chunk content is too big, it will be
+// self encrypted into additional chunks, and now we have a new `DataMap`
+// which points to all of those additional chunks.. and so on.
+fn pack_data_map(data_map: DataMap) -> Result<(Chunk, Vec<Chunk>), Error> {
+    let mut chunks = vec![];
+    let mut chunk_content = wrap_data_map(&DataMapLevel::First(data_map))?;
+
+    let (data_map_chunk, additional_chunks) = loop {
+        let chunk = Chunk::new(chunk_content);
+        // If datamap chunk is less than `MAX_CHUNK_SIZE` return it so it can be directly sent to the network.
+        if MAX_CHUNK_SIZE >= chunk.serialised_size() {
+            chunks.reverse();
+            // Returns the last datamap, and all the chunks produced.
+            break (chunk, chunks);
+        } else {
+            let mut bytes = BytesMut::with_capacity(MAX_CHUNK_SIZE).writer();
+            let mut serialiser = rmp_serde::Serializer::new(&mut bytes);
+            chunk.serialize(&mut serialiser)?;
+            let serialized_chunk = bytes.into_inner().freeze();
+
+            let (data_map, next_encrypted_chunks) = self_encryption::encrypt(serialized_chunk)?;
+            chunks = next_encrypted_chunks
+                .iter()
+                .map(|c| Chunk::new(c.content.clone())) // no need to encrypt what is self-encrypted
+                .chain(chunks)
+                .collect();
+            chunk_content = wrap_data_map(&DataMapLevel::Additional(data_map))?;
+        }
+    };
+
+    Ok((data_map_chunk, additional_chunks))
+}
+
+fn wrap_data_map(data_map: &DataMapLevel) -> Result<Bytes, rmp_serde::encode::Error> {
+    // we use an initial/starting size of 300 bytes as that's roughly the current size of a DataMapLevel instance.
+    let mut bytes = BytesMut::with_capacity(300).writer();
+    let mut serialiser = rmp_serde::Serializer::new(&mut bytes);
+    data_map.serialize(&mut serialiser)?;
+    Ok(bytes.into_inner().freeze())
+}
