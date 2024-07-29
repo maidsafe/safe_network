c89f143040afc7fa0cc8905ecd45c05ce0fce1af
github-action-benchmark <github@users.noreply.github.com>
2024-07-29 14:06:18 +0000

    add Client memory (customSmallerIsBetter) benchmark result for b5bb768f8251d7193837fa24904ab054e57eee19

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722261976495,
+  "lastUpdate": 1722261978754,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20415,40 +20415,6 @@ window.BENCHMARK_DATA = {
             "username": "RolandSherwin"
           },
           "committer": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "distinct": true,
-          "id": "06fc9839a9969ea8e7909b308983f3fbb2132e05",
-          "message": "chore(deps): bump dependencies",
-          "timestamp": "2024-04-04T20:46:43Z",
-          "tree_id": "ec6035708cb84babda6799b5cd976e7f2e68c70d",
-          "url": "https://github.com/maidsafe/safe_network/commit/06fc9839a9969ea8e7909b308983f3fbb2132e05"
-        },
-        "date": 1712266461986,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 185,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 82,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
             "email": "joshuef@gmail.com",
             "name": "joshuef",
             "username": "joshuef"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 88,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b5bb768f8251d7193837fa24904ab054e57eee19",
+          "message": "Merge pull request #2005 from maqi/debug_resend_spend\n\nchore(client): debug resend spend",
+          "timestamp": "2024-07-29T13:13:21Z",
+          "tree_id": "149db33fc0a4fbea939698608c67abf8360c2853",
+          "url": "https://github.com/maidsafe/safe_network/commit/b5bb768f8251d7193837fa24904ab054e57eee19"
+        },
+        "date": 1722261978240,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 206,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 86,
             "unit": "MB"
           }
         ]
