c4fe44b5381b3c986c4a6b855c8eb8ddb235b16a
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 14:30:18 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 5e617460b63fbe85d3a2c6ebc3a1e3ecbd482a70

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722868104556,
+  "lastUpdate": 1722868218176,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11713,35 +11713,6 @@ window.BENCHMARK_DATA = {
             "username": "RolandSherwin"
           },
           "committer": {
-            "email": "gabrielviganotti@gmail.com",
-            "name": "bochaco",
-            "username": "bochaco"
-          },
-          "distinct": true,
-          "id": "1d4761a731f2c8b440c69284c81a5e9e94aae77d",
-          "message": "feat(cli): implement FilesUploadStatusNotifier trait for lib code\n\nThis would allow users of this struct to override the default stdout\nprinting and instead provide thier own custom impl",
-          "timestamp": "2024-04-09T12:43:51Z",
-          "tree_id": "b47e8dcc662f27c88e3871759d0cd3a0718d1e02",
-          "url": "https://github.com/maidsafe/safe_network/commit/1d4761a731f2c8b440c69284c81a5e9e94aae77d"
-        },
-        "date": 1712670550217,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 58,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
             "email": "RolandSherwin@protonmail.com",
             "name": "RolandSherwin",
             "username": "RolandSherwin"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 150,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mickvd99@gmail.com",
+            "name": "Mick van Dijke",
+            "username": "mickvandijke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e617460b63fbe85d3a2c6ebc3a1e3ecbd482a70",
+          "message": "Merge pull request #2003 from mickvandijke/1999-node-should-panic-on-upnp-failure\n\nchore(node): [#1999] panic on upnp event `GatewayNotFound`",
+          "timestamp": "2024-08-05T13:28:37Z",
+          "tree_id": "ddb8e7412a303a051768821ea22727b558194539",
+          "url": "https://github.com/maidsafe/safe_network/commit/5e617460b63fbe85d3a2c6ebc3a1e3ecbd482a70"
+        },
+        "date": 1722868217844,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 135,
             "unit": "MB"
           }
         ]
