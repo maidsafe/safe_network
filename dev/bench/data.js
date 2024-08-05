747921968fec63ab295385f34e84d912074fdbc8
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 15:13:21 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 5200a00225d9965d09ce83c5e284b4b8c92e6bbf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722870685804,
+  "lastUpdate": 1722870801686,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "maqi",
-            "username": "maqi"
-          },
-          "distinct": true,
-          "id": "17d292b2c2a2700a38b2a68ed69833c8ac81985a",
-          "message": "feat(networking): shift to use ilog2 bucket distance for close data calcs\n\nilog2 is about magnitude rather than specifics as distance is already being estimated\nwe can use this to easily render a buffer zone of data we replicate\nvs a close bucket which we deem to be our responsibility",
-          "timestamp": "2024-04-09T15:15:02Z",
-          "tree_id": "10a720c91547f595bbeafb4754b815a84d54805e",
-          "url": "https://github.com/maidsafe/safe_network/commit/17d292b2c2a2700a38b2a68ed69833c8ac81985a"
-        },
-        "date": 1712678332446,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 54,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "chriso83@protonmail.com",
             "name": "Chris O'Neil",
             "username": "jacderida"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 133,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5200a00225d9965d09ce83c5e284b4b8c92e6bbf",
+          "message": "Merge pull request #2023 from maqi/upload_un-recoverable_error_during_make_payment\n\nfix(node): terminate make_payment process during unrecoverable error",
+          "timestamp": "2024-08-05T14:17:08Z",
+          "tree_id": "597906977fa9ce06a7d204e8232a62af90e0ad62",
+          "url": "https://github.com/maidsafe/safe_network/commit/5200a00225d9965d09ce83c5e284b4b8c92e6bbf"
+        },
+        "date": 1722870801426,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 143,
             "unit": "MB"
           }
         ]
