ce0b7e5c44796ad8031cf089401a4ae06663d9ac
github-action-benchmark <github@users.noreply.github.com>
2024-08-06 10:24:24 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 52b1faaa2400bdb0467845d5fa158363b16c52c2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722939748092,
+  "lastUpdate": 1722939864389,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "distinct": true,
-          "id": "db8098cac9219ee7748cf5d07daeb9d2223c77f1",
-          "message": "fix: incorrect release type reference\n\nAlready tried to fix this bug with the faucet upgrade process downloading the wrong, but somehow\nstill managed to miss another hard-coded `ReleaseType` reference.",
-          "timestamp": "2024-04-09T19:39:26Z",
-          "tree_id": "515672dce86fbca97c24f6737a086068f854a4fd",
-          "url": "https://github.com/maidsafe/safe_network/commit/db8098cac9219ee7748cf5d07daeb9d2223c77f1"
-        },
-        "date": 1712694654896,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 47,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 143,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman+github@proton.me",
+            "name": "Benno",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "52b1faaa2400bdb0467845d5fa158363b16c52c2",
+          "message": "Merge pull request #2024 from grumbach/update_readme_transfers\n\ndocs: update readme about transfers",
+          "timestamp": "2024-08-06T09:24:16Z",
+          "tree_id": "388f252176647b8ac664fa36ff0394295435bcfe",
+          "url": "https://github.com/maidsafe/safe_network/commit/52b1faaa2400bdb0467845d5fa158363b16c52c2"
+        },
+        "date": 1722939863583,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 175,
             "unit": "MB"
           }
         ]
