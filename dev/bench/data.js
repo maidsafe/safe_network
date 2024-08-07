1e9d9600b5b3b47516ba46962a3ef2d45864dc5c
github-action-benchmark <github@users.noreply.github.com>
2024-08-07 18:04:20 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 9f5d728cfc19eae3ece75e3a24474cb3dcb450dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723052253003,
+  "lastUpdate": 1723053860877,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": false,
-          "id": "a0e98ec1a58b5d8b7c95314f6c9f5f8d5530d86b",
-          "message": "feat(node): notify peer it is now considered as BAD",
-          "timestamp": "2024-04-10T12:13:57Z",
-          "tree_id": "b1ab4c760639806716b085d08c92aa7f0471985c",
-          "url": "https://github.com/maidsafe/safe_network/commit/a0e98ec1a58b5d8b7c95314f6c9f5f8d5530d86b"
-        },
-        "date": 1712754135597,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 1.1988732699840456,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.6278685929353456,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 56.789997577858024,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11699,6 +11660,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 61.72341752563034,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9f5d728cfc19eae3ece75e3a24474cb3dcb450dc",
+          "message": "Merge pull request #2028 from RolandSherwin/move_metrics\n\nrefactor(network): remove some unused network events",
+          "timestamp": "2024-08-07T17:12:18Z",
+          "tree_id": "abcae2459010667eddf8a0a21dae476aa50332b8",
+          "url": "https://github.com/maidsafe/safe_network/commit/9f5d728cfc19eae3ece75e3a24474cb3dcb450dc"
+        },
+        "date": 1723053854433,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2718441973774801,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.7012046205361127,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 60.602700519457116,
             "unit": "MiB/s"
           }
         ]
