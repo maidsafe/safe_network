0f6d7eafc8c9b064d85836efb5fc1e2efde3b751
Benno Zeeman <bzeeman@live.nl>
2024-08-15 17:37:14 +0200

    feat: impl storing chunks on put

--- libautonomi/src/secrets.rs
+++ libautonomi/src/secrets.rs
@@ -2,12 +2,12 @@ use sn_client::acc_packet::user_secret::account_wallet_secret_key;
 use sn_client::transfers::MainSecretKey;
 
 pub fn generate_mnemonic() -> eyre::Result<bip39::Mnemonic> {
-    sn_client::acc_packet::user_secret::random_eip2333_mnemonic().into()
+    Ok(sn_client::acc_packet::user_secret::random_eip2333_mnemonic()?)
 }
 
 pub fn main_sk_from_mnemonic(
     mnemonic: bip39::Mnemonic,
     derivation_passphrase: &str,
 ) -> eyre::Result<MainSecretKey> {
-    account_wallet_secret_key(mnemonic, derivation_passphrase).into()
+    Ok(account_wallet_secret_key(mnemonic, derivation_passphrase)?)
 }
