9847375ff02958a5b7a7e663e84f0aab697dc332
Benno Zeeman <bzeeman@live.nl>
2024-08-15 14:13:34 +0200

    feat: pay for put

---
+++ libautonomi/src/client_wallet.rs
@@ -0,0 +1,164 @@
+use std::collections::{BTreeSet, HashSet};
+
+use libp2p::{
+    futures::future::join_all,
+    kad::{Quorum, Record},
+};
+use sn_client::{
+    networking::{
+        GetRecordCfg, GetRecordError, Network, NetworkError, PutRecordCfg, VerificationKind,
+    },
+    transfers::{HotWallet, SignedSpend, UniquePubkey},
+};
+use sn_protocol::{
+    storage::{try_serialize_record, RecordKind, RetryStrategy, SpendAddress},
+    NetworkAddress, PrettyPrintRecordKey,
+};
+
+use crate::{Client, VERIFY_STORE};
+
+#[derive(Debug, thiserror::Error)]
+pub enum SendSpendsError {
+    /// The cashnotes that were attempted to be spent have already been spent to another address
+    #[error("Double spend attempted with cashnotes: {0:?}")]
+    DoubleSpendAttemptedForCashNotes(BTreeSet<UniquePubkey>),
+    /// A general error when a transfer fails
+    #[error("Failed to send tokens due to {0}")]
+    CouldNotSendMoney(String),
+}
+
+impl Client {
+    /// Send spend requests to the network.
+    pub async fn send_spends(
+        &self,
+        spend_requests: impl Iterator<Item = &SignedSpend>,
+    ) -> Result<(), SendSpendsError> {
+        let mut tasks = Vec::new();
+
+        // send spends to the network in parralel
+        for spend_request in spend_requests {
+            tracing::debug!(
+                "sending spend request to the network: {:?}: {spend_request:#?}",
+                spend_request.unique_pubkey()
+            );
+
+            let the_task = async move {
+                let cash_note_key = spend_request.unique_pubkey();
+                let result = store_spend(self.network.clone(), spend_request.clone()).await;
+
+                (cash_note_key, result)
+            };
+            tasks.push(the_task);
+        }
+
+        // wait for all the tasks to complete and gather the errors
+        let mut errors = Vec::new();
+        let mut double_spent_keys = BTreeSet::new();
+        for (spend_key, spend_attempt_result) in join_all(tasks).await {
+            match spend_attempt_result {
+                Err(sn_client::networking::NetworkError::GetRecordError(
+                    GetRecordError::RecordDoesNotMatch(_),
+                ))
+                | Err(sn_client::networking::NetworkError::GetRecordError(
+                    GetRecordError::SplitRecord { .. },
+                )) => {
+                    tracing::warn!(
+                        "Double spend detected while trying to spend: {:?}",
+                        spend_key
+                    );
+                    double_spent_keys.insert(*spend_key);
+                }
+                Err(e) => {
+                    tracing::warn!(
+                        "Spend request errored out when sent to the network {spend_key:?}: {e}"
+                    );
+                    errors.push((spend_key, e));
+                }
+                Ok(()) => {
+                    tracing::trace!(
+                        "Spend request was successfully sent to the network: {spend_key:?}"
+                    );
+                }
+            }
+        }
+
+        // report errors accordingly
+        // double spend errors in priority as they should be dealt with by the wallet
+        if !double_spent_keys.is_empty() {
+            return Err(SendSpendsError::DoubleSpendAttemptedForCashNotes(
+                double_spent_keys,
+            ));
+        }
+        if !errors.is_empty() {
+            let mut err_report = "Failed to send spend requests to the network:".to_string();
+            for (spend_key, e) in &errors {
+                tracing::warn!("Failed to send spend request to the network: {spend_key:?}: {e}");
+                err_report.push_str(&format!("{spend_key:?}: {e}"));
+            }
+            return Err(SendSpendsError::CouldNotSendMoney(err_report));
+        }
+
+        Ok(())
+    }
+
+    /// Resend failed transactions. This can optionally verify the store has been successful.
+    /// This will attempt to GET the cash_note from the network.
+    pub(super) async fn resend_pending_transactions(&mut self, wallet: &mut HotWallet) {
+        if wallet.unconfirmed_spend_requests().is_empty() {
+            return;
+        }
+
+        if self
+            .send_spends(wallet.unconfirmed_spend_requests().iter())
+            .await
+            .is_ok()
+        {
+            wallet.clear_confirmed_spend_requests();
+        }
+    }
+}
+
+/// Send a `SpendCashNote` request to the network. Protected method.
+async fn store_spend(network: Network, spend: SignedSpend) -> Result<(), NetworkError> {
+    let unique_pubkey = *spend.unique_pubkey();
+    let cash_note_addr = SpendAddress::from_unique_pubkey(&unique_pubkey);
+    let network_address = NetworkAddress::from_spend_address(cash_note_addr);
+
+    let key = network_address.to_record_key();
+    let pretty_key = PrettyPrintRecordKey::from(&key);
+    tracing::trace!("Sending spend {unique_pubkey:?} to the network via put_record, with addr of {cash_note_addr:?} - {pretty_key:?}");
+    let record_kind = RecordKind::Spend;
+    let record = Record {
+        key,
+        value: try_serialize_record(&[spend], record_kind)?.to_vec(),
+        publisher: None,
+        expires: None,
+    };
+
+    let (record_to_verify, expected_holders) = if VERIFY_STORE {
+        let expected_holders: HashSet<_> = network
+            .get_closest_peers(&network_address, true)
+            .await?
+            .iter()
+            .cloned()
+            .collect();
+        (Some(record.clone()), expected_holders)
+    } else {
+        (None, Default::default())
+    };
+
+    // When there is retry on Put side, no need to have a retry on Get
+    let verification_cfg = GetRecordCfg {
+        get_quorum: Quorum::Majority,
+        retry_strategy: None,
+        target_record: record_to_verify,
+        expected_holders,
+    };
+    let put_cfg = PutRecordCfg {
+        put_quorum: Quorum::Majority,
+        retry_strategy: Some(RetryStrategy::Persistent),
+        use_put_record_to: None,
+        verification: Some((VerificationKind::Network, verification_cfg)),
+    };
+    Ok(network.put_record(record, &put_cfg).await?)
+}
