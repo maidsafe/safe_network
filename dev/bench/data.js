2baba1a714e28fc6c3070db0bf14918cb2745f59
github-action-benchmark <github@users.noreply.github.com>
2024-08-19 13:24:21 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 3e04c8531d08c8ebd5231bf52c1ee8f9d1a4a1e8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724073859566,
+  "lastUpdate": 1724073861238,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -30622,45 +30622,6 @@ window.BENCHMARK_DATA = {
             "username": "joshuef"
           },
           "distinct": true,
-          "id": "9ad4b38116981e9c9410f0fc23dcc163fd29ee58",
-          "message": "ci: dont require await for past release in bumper",
-          "timestamp": "2024-04-11T14:20:18+09:00",
-          "tree_id": "3d8511e7029d30d8146b8c07604c4090e2fdfd71",
-          "url": "https://github.com/maidsafe/safe_network/commit/9ad4b38116981e9c9410f0fc23dcc163fd29ee58"
-        },
-        "date": 1712813628925,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "swarm_driver long handling times",
-            "value": 7369,
-            "unit": "hits"
-          },
-          {
-            "name": "swarm_driver long handling total_time",
-            "value": 34750,
-            "unit": "ms"
-          },
-          {
-            "name": "swarm_driver average long handling time",
-            "value": 4,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
           "id": "39feb2eec29b46233b63a86f81c1e1220c750b83",
           "message": "ci: specify suffix for release bumps",
           "timestamp": "2024-04-11T14:27:39+09:00",
@@ -42300,6 +42261,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 26646,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 5,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chris.oneil@gmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3e04c8531d08c8ebd5231bf52c1ee8f9d1a4a1e8",
+          "message": "Merge pull request #2046 from jacderida/chore-new_pr_script\n\nchore: script to list merged pull requests",
+          "timestamp": "2024-08-19T12:17:15Z",
+          "tree_id": "1bc5fb9a91857a89bf59abfd6da6546625f44d77",
+          "url": "https://github.com/maidsafe/safe_network/commit/3e04c8531d08c8ebd5231bf52c1ee8f9d1a4a1e8"
+        },
+        "date": 1724073860910,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 4783,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 26446,
             "unit": "ms"
           },
           {
