04844b9d85477b21ad7c6b12c0668d1fa180b80c
github-action-benchmark <github@users.noreply.github.com>
2024-08-19 10:46:27 +0000

    add Client memory (customSmallerIsBetter) benchmark result for d95cc9bf80646da484a887e329e6860f3cf676df

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724064385263,
+  "lastUpdate": 1724064387323,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20420,40 +20420,6 @@ window.BENCHMARK_DATA = {
             "username": "joshuef"
           },
           "distinct": true,
-          "id": "c96cc5b174c6ef88f6593bb480c33e4f6fddf007",
-          "message": "feat(resources): update bump script to strip suffix-version if no changes found",
-          "timestamp": "2024-04-11T12:44:36+09:00",
-          "tree_id": "8f9fde5c2cd9d1f424c50fb8c41241ee5749c642",
-          "url": "https://github.com/maidsafe/safe_network/commit/c96cc5b174c6ef88f6593bb480c33e4f6fddf007"
-        },
-        "date": 1712807891687,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 180,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 83,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
           "id": "37ab87b2a410bab7b45e8a5562bc7c7605f7e5e6",
           "message": "ci: remove schedule version bumper from workflow",
           "timestamp": "2024-04-11T13:57:05+09:00",
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 81,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "RolandSherwin@protonmail.com",
+            "name": "RolandSherwin",
+            "username": "RolandSherwin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d95cc9bf80646da484a887e329e6860f3cf676df",
+          "message": "Merge pull request #2041 from mazzi/feat_new_layout\n\nfeat(launchpad): new layout sections and features",
+          "timestamp": "2024-08-19T09:47:41Z",
+          "tree_id": "f231275c58acc8f1063f62ababa41afc38680b14",
+          "url": "https://github.com/maidsafe/safe_network/commit/d95cc9bf80646da484a887e329e6860f3cf676df"
+        },
+        "date": 1724064386545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 191,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 83,
             "unit": "MB"
           }
         ]
