d37e8c3b2e2c20bc3e6c1fe770fe953280d4b17c
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 01:48:26 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 3f0e1887b68cfdf64d3ef5f6f86195c638b65732

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724723185348,
+  "lastUpdate": 1724723306493,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": false,
-          "id": "59cec7b7e0167557298a5cde0a2bb1138ae408d2",
-          "message": "fix: justfile conditional error",
-          "timestamp": "2024-04-15T07:48:00Z",
-          "tree_id": "96b285dbc1d8d5d6e3f4534f72eed5e7bae57b18",
-          "url": "https://github.com/maidsafe/safe_network/commit/59cec7b7e0167557298a5cde0a2bb1138ae408d2"
-        },
-        "date": 1713169998535,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 49,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "qi.ma@maidsafe.net",
             "name": "qima",
             "username": "maqi"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 151,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f0e1887b68cfdf64d3ef5f6f86195c638b65732",
+          "message": "Merge pull request #2061 from maqi/register_replicate_on_edit\n\nfix(client): fetch register accurately and merge results",
+          "timestamp": "2024-08-27T01:00:08Z",
+          "tree_id": "3b2f5b98da4263bbeef36ba03f0f84c07ebdc1df",
+          "url": "https://github.com/maidsafe/safe_network/commit/3f0e1887b68cfdf64d3ef5f6f86195c638b65732"
+        },
+        "date": 1724723305961,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 153,
             "unit": "MB"
           }
         ]
