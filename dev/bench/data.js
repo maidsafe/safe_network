d7c7b08a6c7af4e3ce3f83139713cf185a00d400
github-action-benchmark <github@users.noreply.github.com>
2024-08-29 09:19:37 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 3347c6988b588cf686ffcaaab41960ef96bb8334

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724923174574,
+  "lastUpdate": 1724923177206,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "gapspace@engineer.com",
-            "name": "Jason Paul",
-            "username": "JasonPaulGithub"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "445da434b5854ee99f946de5fe6307b0eac3de56",
-          "message": "chore: remove deprecated wallet deposit cmd",
-          "timestamp": "2024-04-16T11:21:36Z",
-          "tree_id": "db89cd67d27e5164e294cea38a48221b95555adc",
-          "url": "https://github.com/maidsafe/safe_network/commit/445da434b5854ee99f946de5fe6307b0eac3de56"
-        },
-        "date": 1713269718303,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 188,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 84,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "Roland Sherwin",
             "username": "RolandSherwin"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 84,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3347c6988b588cf686ffcaaab41960ef96bb8334",
+          "message": "Merge pull request #2071 from maqi/auditor_lost_tracking_investigate\n\nchore(auditor): trival auditor code cleanup",
+          "timestamp": "2024-08-29T08:23:49Z",
+          "tree_id": "221e1d68fbbb6a15516f50d0d6902524ba5cacbf",
+          "url": "https://github.com/maidsafe/safe_network/commit/3347c6988b588cf686ffcaaab41960ef96bb8334"
+        },
+        "date": 1724923176132,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 189,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 83,
             "unit": "MB"
           }
         ]
