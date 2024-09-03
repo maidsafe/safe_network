55f6d46ab5073241886723bad06e8e76c353a50e
github-action-benchmark <github@users.noreply.github.com>
2024-09-03 01:09:27 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 8e111568db1555cc91d47781ef9827e018f135b8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725325765745,
+  "lastUpdate": 1725325767773,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "0fe22e81c038a24123a42e869052894bad48a826",
-          "message": "chore(node): do not reset farthest_acceptance_distance",
-          "timestamp": "2024-04-18T09:00:02Z",
-          "tree_id": "43e39963e5d0634edeee17f4dad19256d089d420",
-          "url": "https://github.com/maidsafe/safe_network/commit/0fe22e81c038a24123a42e869052894bad48a826"
-        },
-        "date": 1713434800329,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 184,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 82,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "anselmega@gmail.com",
             "name": "grumbach",
             "username": "grumbach"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 82,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8e111568db1555cc91d47781ef9827e018f135b8",
+          "message": "Merge pull request #2076 from mazzi/feat_connection_mode\n\nfeat(launchpad): connection mode and fixes",
+          "timestamp": "2024-09-03T00:21:03Z",
+          "tree_id": "aa895917611a7aee0a84f6977907438ef4fe0e4b",
+          "url": "https://github.com/maidsafe/safe_network/commit/8e111568db1555cc91d47781ef9827e018f135b8"
+        },
+        "date": 1725325766974,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 185,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 84,
             "unit": "MB"
           }
         ]
