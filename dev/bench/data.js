6181b51c6dfb522348f2b06524470ad82aca18ce
github-action-benchmark <github@users.noreply.github.com>
2024-09-05 15:55:44 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 82bd5eeb56e3213b726f9d8388e881e10154c34f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725551742997,
+  "lastUpdate": 1725551744338,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -20410,40 +20410,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "qima",
-            "username": "maqi"
-          },
-          "committer": {
-            "email": "qi.ma@maidsafe.net",
-            "name": "maqi",
-            "username": "maqi"
-          },
-          "distinct": false,
-          "id": "c3b91c565fa8f9a879cfb2b5a84fdf1cd109e07e",
-          "message": "chore(node): lower some log levels to reduce log size",
-          "timestamp": "2024-04-22T17:16:18Z",
-          "tree_id": "716782d7dee0e3a559e53274df7caf4130a9235b",
-          "url": "https://github.com/maidsafe/safe_network/commit/c3b91c565fa8f9a879cfb2b5a84fdf1cd109e07e"
-        },
-        "date": 1713810287727,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory usage w/ upload",
-            "value": 198,
-            "unit": "MB"
-          },
-          {
-            "name": "Average memory usage w/ upload",
-            "value": 83,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "joshuef@gmail.com",
             "name": "Josh Wilson",
             "username": "joshuef"
@@ -30603,6 +30569,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average memory usage w/ upload",
             "value": 85,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chris.oneil@gmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82bd5eeb56e3213b726f9d8388e881e10154c34f",
+          "message": "Merge pull request #2091 from maqi/log_spend_size\n\nchore(client): log spend size",
+          "timestamp": "2024-09-05T15:00:22Z",
+          "tree_id": "6f0452fbd90b4ab275321149233d3fb2a1871138",
+          "url": "https://github.com/maidsafe/safe_network/commit/82bd5eeb56e3213b726f9d8388e881e10154c34f"
+        },
+        "date": 1725551744016,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 204,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 83,
             "unit": "MB"
           }
         ]
