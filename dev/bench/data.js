3bc8e1dce8b5b40131a0f76391349707dfef52f1
github-action-benchmark <github@users.noreply.github.com>
2024-09-06 14:51:22 +0000

    add Node memory (customSmallerIsBetter) benchmark result for ecf4c223b285a0934229f60fb0e0dc86c8b9bb84

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725634163935,
+  "lastUpdate": 1725634282079,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "joshuef@gmail.com",
-            "name": "Josh Wilson",
-            "username": "joshuef"
-          },
-          "committer": {
-            "email": "joshuef@gmail.com",
-            "name": "joshuef",
-            "username": "joshuef"
-          },
-          "distinct": true,
-          "id": "2d350877ddd5244b3b23b2b0d2878025e4756795",
-          "message": "ci: increase token distribution build times by 5 mins",
-          "timestamp": "2024-04-24T02:11:07Z",
-          "tree_id": "a1f09bb1d45b6eb30eaff31f575220818479ce27",
-          "url": "https://github.com/maidsafe/safe_network/commit/2d350877ddd5244b3b23b2b0d2878025e4756795"
-        },
-        "date": 1713927402949,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 49,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "RolandSherwin@protonmail.com",
             "name": "Roland Sherwin",
             "username": "RolandSherwin"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 158,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman+github@proton.me",
+            "name": "Benno",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ecf4c223b285a0934229f60fb0e0dc86c8b9bb84",
+          "message": "Merge pull request #2095 from b-zee/refactor-clippy\n\nrefactor(cli/faucet): clippy suggestions",
+          "timestamp": "2024-09-06T14:01:00Z",
+          "tree_id": "05cb86e7d94bb3c5c69fbe87b48a61da1a088494",
+          "url": "https://github.com/maidsafe/safe_network/commit/ecf4c223b285a0934229f60fb0e0dc86c8b9bb84"
+        },
+        "date": 1725634281772,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 144,
             "unit": "MB"
           }
         ]
