32ea0618773f8dd07eb7fb671a7e3f6e14acb9aa
Benno Zeeman <bzeeman@live.nl>
2024-09-09 10:08:02 +0200

    chore(global): unused rustdoc lints

--- sn_faucet/src/main.rs
+++ sn_faucet/src/main.rs
@@ -159,7 +159,6 @@ struct Opt {
     ///  - Linux: $HOME/.local/share/safe/client/logs
     ///  - macOS: $HOME/Library/Application Support/safe/client/logs
     ///  - Windows: C:\Users\<username>\AppData\Roaming\safe\client\logs
-    #[expect(rustdoc::invalid_html_tags)]
     #[clap(long, value_parser = parse_log_output, verbatim_doc_comment, default_value = "data-dir")]
     pub log_output_dest: LogOutputDest,
 
