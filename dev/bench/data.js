dbe49e0eccc719cca12eb3fe2f86161f6d7e9e61
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 14:30:58 +0000

    add Node memory (customSmallerIsBetter) benchmark result for abf5cd8c4680939ca216761ca55d2b08415c50a7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725978537624,
+  "lastUpdate": 1725978658285,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -11708,35 +11708,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "Roland Sherwin",
-            "username": "RolandSherwin"
-          },
-          "committer": {
-            "email": "RolandSherwin@protonmail.com",
-            "name": "RolandSherwin",
-            "username": "RolandSherwin"
-          },
-          "distinct": true,
-          "id": "ac77ad7951f11412d5d3335654d5e846c990d271",
-          "message": "chore(network): remove nat detection via incoming connections check",
-          "timestamp": "2024-04-25T12:23:56Z",
-          "tree_id": "6a5b19608a2f2fafada1cda0c99a30d32b7c9a42",
-          "url": "https://github.com/maidsafe/safe_network/commit/ac77ad7951f11412d5d3335654d5e846c990d271"
-        },
-        "date": 1714051039787,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Peak memory w/ `safe` benchmarks",
-            "value": 52,
-            "unit": "MB"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "chriso83@protonmail.com",
             "name": "Chris O'Neil",
             "username": "jacderida"
@@ -20401,6 +20372,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 140,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bzeeman+github@proton.me",
+            "name": "Benno",
+            "username": "b-zee"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "abf5cd8c4680939ca216761ca55d2b08415c50a7",
+          "message": "Merge pull request #2098 from b-zee/chore-expect-lint\n\nchore(global): replace allow with expect lint",
+          "timestamp": "2024-09-10T13:43:45Z",
+          "tree_id": "99ba7359258163b44f56b36e7d84a08d96dc9436",
+          "url": "https://github.com/maidsafe/safe_network/commit/abf5cd8c4680939ca216761ca55d2b08415c50a7"
+        },
+        "date": 1725978657490,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 132,
             "unit": "MB"
           }
         ]
