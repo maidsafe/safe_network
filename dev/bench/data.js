16facdb2e53604dc8d6e2a77a2283187c3cf1c49
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 13:13:27 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for b26bbe3009d4c1ed066a3a4a211f3251725a375b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,47 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726104309835,
+  "lastUpdate": 1726146807038,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "`safe files` benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "chriso83@protonmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "committer": {
-            "email": "chris.oneil@gmail.com",
-            "name": "Chris O'Neil",
-            "username": "jacderida"
-          },
-          "distinct": true,
-          "id": "ff83510da343c9763bdba5132fcdbc822e4b704e",
-          "message": "ci: add `node-launchpad` binary to release process\n\nThe new TUI for the node manager is a new, separate binary that is being added in to the release\nprocess. It will have a Github release.\n\nFor some reason it seems to have became necessary to explicitly call `rustup target` during the\nartifacts build process. Without doing it, I was getting build errors on macOS.",
-          "timestamp": "2024-05-01T20:22:23+01:00",
-          "tree_id": "1a308fde9158d5b7e52e4597c161ea2295b59311",
-          "url": "https://github.com/maidsafe/safe_network/commit/ff83510da343c9763bdba5132fcdbc822e4b704e"
-        },
-        "date": 1714592085131,
-        "tool": "customBiggerIsBetter",
-        "benches": [
-          {
-            "name": "safe files upload 1mb",
-            "value": 1.1930696326918593,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files upload 10mb",
-            "value": 1.6336975691660944,
-            "unit": "MiB/s"
-          },
-          {
-            "name": "safe files download",
-            "value": 57.00395689596022,
-            "unit": "MiB/s"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -11699,6 +11660,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "safe files download",
             "value": 61.69454352189301,
+            "unit": "MiB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "distinct": true,
+          "id": "b26bbe3009d4c1ed066a3a4a211f3251725a375b",
+          "message": "Merge branch 'hotfix-2024.09.1.4'",
+          "timestamp": "2024-09-12T13:54:12+01:00",
+          "tree_id": "89135b59a79834d4fd151105b249afb14f221ba0",
+          "url": "https://github.com/maidsafe/safe_network/commit/b26bbe3009d4c1ed066a3a4a211f3251725a375b"
+        },
+        "date": 1726146800354,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "safe files upload 1mb",
+            "value": 1.2560555443422208,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files upload 10mb",
+            "value": 1.7106573403404588,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "safe files download",
+            "value": 60.26992915098607,
             "unit": "MiB/s"
           }
         ]
