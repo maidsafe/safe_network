1d8d2249d9aab5a07c4b5a61b34fac5b4d79e869
Warm Beer <mickvd99@gmail.com>
2024-10-02 10:19:17 +0200

    refactor(autonomi): wasm test compile-time env variables

--- evmlib/src/utils.rs
+++ evmlib/src/utils.rs
@@ -4,6 +4,10 @@ use rand::Rng;
 use std::env;
 use std::env::VarError;
 
+const ENV_RPC_URL: &str = "RPC_URL";
+const ENV_PAYMENT_TOKEN_ADDRESS: &str = "PAYMENT_TOKEN_ADDRESS";
+const ENV_CHUNK_PAYMENTS_ADDRESS: &str = "CHUNK_PAYMENTS_ADDRESS";
+
 /// Generate a random Address.
 pub fn dummy_address() -> Address {
     Address::new(rand::rngs::OsRng.gen())
@@ -16,7 +20,11 @@ pub fn dummy_hash() -> Hash {
 
 /// Get the `Network` from environment variables
 pub fn evm_network_from_env() -> Result<Network, VarError> {
-    const EVM_VARS: [&str; 3] = ["RPC_URL", "PAYMENT_TOKEN_ADDRESS", "CHUNK_PAYMENTS_ADDRESS"];
+    const EVM_VARS: [&str; 3] = [
+        ENV_RPC_URL,
+        ENV_PAYMENT_TOKEN_ADDRESS,
+        ENV_CHUNK_PAYMENTS_ADDRESS,
+    ];
     let custom_vars_exist = EVM_VARS.iter().all(|var| env::var(var).is_ok());
 
     if custom_vars_exist {
@@ -28,4 +36,19 @@ pub fn evm_network_from_env() -> Result<Network, VarError> {
     } else {
         Ok(Network::ArbitrumOne)
     }
+}
+
+/// Get the EVM `Network` using env variables at compile time.
+pub fn evm_network_from_env_compile_time() -> Network {
+    let rpc_url = option_env!("RPC_URL").expect("RPC_URL not set");
+    let payment_token_address =
+        option_env!("PAYMENT_TOKEN_ADDRESS").expect("PAYMENT_TOKEN_ADDRESS not set");
+    let chunk_payments_address =
+        option_env!("CHUNK_PAYMENTS_ADDRESS").expect("CHUNK_PAYMENTS_ADDRESS not set");
+
+    Network::Custom(CustomNetwork::new(
+        rpc_url,
+        payment_token_address,
+        chunk_payments_address,
+    ))
 }
