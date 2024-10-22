0d5e244d6c33224535918121e1246e6d046ef146
Chris O'Neil <chriso83@protonmail.com>
2024-10-22 16:13:51 +0100

    chore: include release cycle info in crate
    For some reason the `sn_build_info` crate started to manifest an error that it couldn't find the
    `release-cycle-info` file. The solution was to embed the information in the crate. This is less than
    ideal, because it now needs to be updated in two places. However, we could have the other parts of
    the release process read the Rust file. We can do that later.

--- sn_build_info/build.rs
+++ sn_build_info/build.rs
@@ -5,62 +5,24 @@
 // under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
-use std::fs;
-use std::path::Path;
 use vergen::EmitBuilder;
+
+mod release_info {
+    include!("src/release_info.rs");
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     EmitBuilder::builder()
         .build_date()
-        // Emit the short SHA-1 hash of the current commit
         .git_sha(true)
-        // Emit the current branch name
         .git_branch()
-        // Emit the annotated tag of the current commit, or fall back to abbreviated commit object.
         .git_describe(true, false, None)
         .emit()?;
 
-    let release_info_path = Path::new("../release-cycle-info");
-    let contents =
-        fs::read_to_string(release_info_path).expect("Failed to read release-cycle-info");
-
-    let mut year = String::new();
-    let mut month = String::new();
-    let mut cycle = String::new();
-    let mut counter = String::new();
-
-    for line in contents.lines() {
-        if line.starts_with("release-year:") {
-            year = line
-                .split(':')
-                .nth(1)
-                .map(|s| s.trim().to_string())
-                .unwrap_or_default();
-        } else if line.starts_with("release-month:") {
-            month = line
-                .split(':')
-                .nth(1)
-                .map(|s| s.trim().to_string())
-                .unwrap_or_default();
-        } else if line.starts_with("release-cycle:") {
-            cycle = line
-                .split(':')
-                .nth(1)
-                .map(|s| s.trim().to_string())
-                .unwrap_or_default();
-        } else if line.starts_with("release-cycle-counter:") {
-            counter = line
-                .split(':')
-                .nth(1)
-                .map(|s| s.trim().to_string())
-                .unwrap_or_default();
-        }
-    }
-
-    println!("cargo:rustc-env=RELEASE_YEAR={year}");
-    println!("cargo:rustc-env=RELEASE_MONTH={month}");
-    println!("cargo:rustc-env=RELEASE_CYCLE={cycle}");
-    println!("cargo:rustc-env=RELEASE_CYCLE_COUNTER={counter}");
+    println!("cargo:rustc-env=RELEASE_YEAR={}", release_info::RELEASE_YEAR);
+    println!("cargo:rustc-env=RELEASE_MONTH={}", release_info::RELEASE_MONTH);
+    println!("cargo:rustc-env=RELEASE_CYCLE={}", release_info::RELEASE_CYCLE);
+    println!("cargo:rustc-env=RELEASE_CYCLE_COUNTER={}", release_info::RELEASE_CYCLE_COUNTER);
 
     Ok(())
 }
