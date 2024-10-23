b7357109dcb89994de8f8dfcc83cf324974ee303
qima <qi.ma@maidsafe.net>
2024-10-23 18:30:10 +0800

    Revert "feat(autonomi): download either a file or directory based on data"
    This reverts commit 04ec14e1718193bcc76b5c07b902161b5210d4a6.

--- autonomi-cli/src/commands/file.rs
+++ autonomi-cli/src/commands/file.rs
@@ -8,11 +8,9 @@
 
 use crate::utils::collect_upload_summary;
 use autonomi::client::address::addr_to_str;
-use autonomi::client::address::str_to_addr;
 use autonomi::Multiaddr;
 use color_eyre::eyre::Context;
 use color_eyre::eyre::Result;
-use std::path::Path;
 use std::path::PathBuf;
 
 pub async fn cost(file: &str, peers: Vec<Multiaddr>) -> Result<()> {
@@ -28,35 +26,22 @@ pub async fn cost(file: &str, peers: Vec<Multiaddr>) -> Result<()> {
     println!("Total cost: {cost}");
     Ok(())
 }
-pub async fn upload(path: &str, peers: Vec<Multiaddr>) -> Result<()> {
+pub async fn upload(file: &str, peers: Vec<Multiaddr>) -> Result<()> {
     let wallet = crate::keys::load_evm_wallet()?;
     let mut client = crate::actions::connect_to_network(peers).await?;
     let event_receiver = client.enable_client_events();
     let (upload_summary_thread, upload_completed_tx) = collect_upload_summary(event_receiver);
 
-    let path = PathBuf::from(path);
+    println!("Uploading data to network...");
 
-    let xor_name = if path.is_dir() {
-        println!("Uploading directory: {path:?}");
-        info!("Uploading directory: {path:?}");
-        client
-            .dir_upload(&path, &wallet)
-            .await
-            .wrap_err("Failed to upload directory")?
-    } else {
-        println!("Uploading file: {path:?}");
-        info!("Uploading file: {path:?}");
-        client
-            .file_upload(&path, &wallet)
-            .await
-            .wrap_err("Failed to upload file")?
-    };
-
+    let xor_name = client
+        .dir_upload(PathBuf::from(file), &wallet)
+        .await
+        .wrap_err("Failed to upload file")?;
     let addr = addr_to_str(xor_name);
 
-    println!("Successfully uploaded: {path:?}");
+    println!("Successfully uploaded: {file}");
     println!("At address: {addr}");
-    info!("Successfully uploaded: {path:?} at address: {addr}");
     if let Ok(()) = upload_completed_tx.send(()) {
         let summary = upload_summary_thread.await?;
         if summary.record_count == 0 {
@@ -65,18 +50,13 @@ pub async fn upload(path: &str, peers: Vec<Multiaddr>) -> Result<()> {
             println!("Number of chunks uploaded: {}", summary.record_count);
             println!("Total cost: {} AttoTokens", summary.tokens_spent);
         }
-        info!("Summary for upload of data {path:?} at {addr:?}: {summary:?}");
     }
 
     Ok(())
 }
-pub async fn download(addr: &str, dest_path: &Path, peers: Vec<Multiaddr>) -> Result<()> {
-    let client = crate::actions::connect_to_network(peers).await?;
-    let address = str_to_addr(addr).wrap_err("Failed to parse data address")?;
-
-    client.download_file_or_dir(address, dest_path).await?;
-
-    Ok(())
+pub async fn download(addr: &str, dest_path: &str, peers: Vec<Multiaddr>) -> Result<()> {
+    let mut client = crate::actions::connect_to_network(peers).await?;
+    crate::actions::download(addr, dest_path, &mut client).await
 }
 
 pub fn list(_peers: Vec<Multiaddr>) -> Result<()> {
