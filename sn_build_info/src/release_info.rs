15bf53f8b08445849eeca873c5664f17b61a3d47
Chris O'Neil <chriso83@protonmail.com>
2024-11-02 23:27:01 +0000

    chore(release): release candidate 2024.10.4.4
    ==================
      Crate Versions
    ==================
    autonomi: 0.2.3-rc.2
    autonomi-cli: 0.1.4-rc.2
    evmlib: 0.1.3-rc.2
    evm_testnet: 0.1.3-rc.2
    sn_build_info: 0.1.18-rc.2
    sn_evm: 0.1.3-rc.2
    sn_logging: 0.2.39-rc.2
    sn_metrics: 0.1.19-rc.2
    nat-detection: 0.2.10-rc.2
    sn_networking: 0.19.2-rc.2
    sn_node: 0.112.3-rc.2
    node-launchpad: 0.4.3-rc.2
    sn_node_manager: 0.11.2-rc.2
    sn_node_rpc_client: 0.6.34-rc.2
    sn_peers_acquisition: 0.5.6-rc.2
    sn_protocol: 0.17.14-rc.2
    sn_registers: 0.4.2-rc.2
    sn_service_management: 0.4.2-rc.2
    sn_transfers: 0.20.2-rc.2
    test_utils: 0.4.10-rc.2
    token_supplies: 0.1.57-rc.2
    ===================
      Binary Versions
    ===================
    nat-detection: 0.2.10-rc.2
    node-launchpad: 0.4.3-rc.2
    autonomi: 0.1.4-rc.2
    safenode: 0.112.3-rc.2
    safenode-manager: 0.11.2-rc.2
    safenode_rpc_client: 0.6.34-rc.2
    safenodemand: 0.11.2-rc.2

--- sn_build_info/src/release_info.rs
+++ sn_build_info/src/release_info.rs
@@ -1,4 +1,4 @@
 pub const RELEASE_YEAR: &str = "2024";
 pub const RELEASE_MONTH: &str = "10";
 pub const RELEASE_CYCLE: &str = "4";
-pub const RELEASE_CYCLE_COUNTER: &str = "3";
+pub const RELEASE_CYCLE_COUNTER: &str = "4";
