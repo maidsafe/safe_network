ef9822d2d04d963639e2350df44176171a6bfcf0
Warm Beer <mickvd99@gmail.com>
2024-12-11 11:49:02 +0100

    chore: introduce rate limiter and retry strategy for `get_market_price`

---
+++ autonomi/src/client/rate_limiter.rs
@@ -0,0 +1,28 @@
+use ant_networking::target_arch::{sleep, Duration, Instant};
+
+pub struct RateLimiter {
+    last_request_time: Option<Instant>,
+}
+
+impl RateLimiter {
+    pub fn new() -> Self {
+        Self {
+            last_request_time: None,
+        }
+    }
+
+    pub async fn wait_interval_since_last_request(&mut self, interval_in_ms: u64) {
+        if let Some(last_request_time) = self.last_request_time {
+            let elapsed_time = last_request_time.elapsed();
+
+            let interval = Duration::from_millis(interval_in_ms);
+
+            if elapsed_time < interval {
+                println!("Waiting for: {:?}", interval - elapsed_time);
+                sleep(interval - elapsed_time).await;
+            }
+        }
+
+        self.last_request_time = Some(Instant::now());
+    }
+}
