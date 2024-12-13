f64a20872c237616ae551d18d631d7936af6f1e2
Chris O'Neil <chriso83@protonmail.com>
2024-12-13 16:06:16 +0000

    chore(release): release candidate 2024.12.1.2
    ==================
      Crate Versions
    ==================
    ant-bootstrap: 0.1.0-rc.2
    ant-build-info: 0.1.20-rc.2
    ant-cli: 0.3.0-rc.2
    ant-evm: 0.1.5-rc.2
    ant-logging: 0.2.41-rc.2
    ant-metrics: 0.1.21-rc.2
    ant-networking: 0.3.0-rc.2
    ant-node: 0.3.0-rc.2
    ant-node-manager: 0.11.4-rc.2
    ant-node-rpc-client: 0.6.37-rc.2
    ant-protocol: 0.3.0-rc.2
    ant-registers: 0.4.4-rc.2
    ant-service-management: 0.4.4-rc.2
    ant-token-supplies: 0.1.59-rc.2
    autonomi: 0.3.0-rc.2
    evmlib: 0.1.5-rc.2
    evm-testnet: 0.1.5-rc.2
    nat-detection: 0.2.12-rc.2
    node-launchpad: 0.5.0-rc.2
    test-utils: 0.4.12-rc.2
    ===================
      Binary Versions
    ===================
    ant: 0.3.0-rc.2
    antctl: 0.11.4-rc.2
    antctld: 0.11.4-rc.2
    antnode: 0.3.0-rc.2
    antnode_rpc_client: 0.6.37-rc.2
    nat-detection: 0.2.12-rc.2
    node-launchpad: 0.5.0-rc.2

--- ant-build-info/src/release_info.rs
+++ ant-build-info/src/release_info.rs
@@ -1,4 +1,4 @@
 pub const RELEASE_YEAR: &str = "2024";
 pub const RELEASE_MONTH: &str = "12";
 pub const RELEASE_CYCLE: &str = "1";
-pub const RELEASE_CYCLE_COUNTER: &str = "1";
+pub const RELEASE_CYCLE_COUNTER: &str = "2";
